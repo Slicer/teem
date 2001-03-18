d26d8a685cad0132f6b1d1b7813ff201f95e9775
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2001-03-18 23:40:02 +0000

    added #ifdef's to deal with non-presence of C9X-conformant definitions of max and min values for long long int and unsigned long long int.
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@14 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/nrrd/nrrdDefines.h
+++ src/nrrd/nrrdDefines.h
@@ -1,37 +1,19 @@
 #ifndef NRRD_DEFINES_HAS_BEEN_INCLUDED
 #define NRRD_DEFINES_HAS_BEEN_INCLUDED
 
+#include <limits.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #define NRRD_MAX_DIM 12         /* The maximum dimension which we can handle */
 
-/* the C types which we will assume are present.  The #defines must give
-   types which can be given literally as a variable type in a source file.
-   Thus, if there is not a type available on the architecture which matches
-   the description in enum nrrdType in nrrd.h, define it to something which
-   does exist, and the library will detect the deficiency (ha ha maybe someday)
-   /usr/include/inttypes.h may be useful, if it exists.
-   >>---> THIS MUST BE IN SYNC WITH THE ENUMS IN nrrd.h <---<<
-*/
-#define NRRD_CHAR signed char
-#define NRRD_UCHAR unsigned char
-#define NRRD_SHORT signed short int
-#define NRRD_USHORT unsigned short int
-#define NRRD_INT signed int
-#define NRRD_UINT unsigned int
-#define NRRD_LLONG signed long long int
-#define NRRD_ULLONG unsigned long long int
-#define NRRD_FLOAT float
-#define NRRD_DOUBLE double
-#define NRRD_LDOUBLE long double
-
 #define NRRD_BIG_INT long long  /* biggest integral type allowed on system; 
 				   used to hold number of items in array 
 				   (MUST be a signed type, even though as a 
 				   store for # of things, could be unsigned)
-				   using "int" will give a maximum power-of-2
+				   using "int" would give a maximum pow-of-2
 				   array size of 1024x1024x1024 
 				   (really 1024x1024x2048-1) */
 #define NRRD_BIG_INT_PRINTF "%lld"
@@ -42,7 +24,46 @@ extern "C" {
 #define NRRD_HUGE_STRLEN  1025  /* sentences */
 #define NRRD_ERR_STRLEN   2049  /* error message accumulation */
 
-#define NRRD_MAX_KERNEL_ARGS 5  /* max # arguments to a resampling kernel */
+#define NRRD_MAX_KERNEL_PARAMS 5  /* max # arguments to a resampling kernel */
+
+#define NRRD_NO_CONTENT "?"     /* to fill in for an unset "content" field */
+
+/* for the 64-bit integer types (not standard except in C9X), we try to
+   use the names for the _MIN and _MAX values which are used in C9X
+   (as well as gcc) such as LLONG_MAX; if these aren't defined, we try
+   the ones used on SGI such as LONGLONG_MAX, if still not defined, we
+   go wild and define something ourselves, with total disregard to
+   what the architecture and compiler actually support */
+
+#ifdef LLONG_MAX
+#  define NRRD_LLONG_MAX LLONG_MAX
+#else
+#  ifdef LONGLONG_MAX
+#    define NRRD_LLONG_MAX LONGLONG_MAX
+#  else
+#    define NRRD_LLONG_MAX 9223372036854775807LL
+#  endif
+#endif
+
+#ifdef LLONG_MIN
+#  define NRRD_LLONG_MIN LLONG_MIN
+#else
+#  ifdef LONGLONG_MIN
+#    define NRRD_LLONG_MIN LONGLONG_MIN
+#  else
+#    define NRRD_LLONG_MIN (-NRRD_LLONG_MAX-1LL)
+#  endif
+#endif
+
+#ifdef ULLONG_MAX
+#  define NRRD_ULLONG_MAX ULLONG_MAX
+#else
+#  ifdef ULONGLONG_MAX
+#    define NRRD_ULLONG_MAX ULONGLONG_MAX
+#  else
+#    define NRRD_ULLONG_MAX 18446744073709551615LLU
+#  endif
+#endif
 
 /* -------------------------------------------------------- */
 /* ----------- END of user-alterable defines -------------- */
