55c217699531f71554bacdb39be270c532548fcf
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2001-04-06 05:38:11 +0000

    here comes the sun
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@93 3d70eeeb-363e-0410-a505-8a46323a89f2

---
+++ src/make/solaris.mk
@@ -0,0 +1,31 @@
+#
+# The contents of this file are subject to the University of Utah Public
+# License (the "License"); you may not use this file except in
+# compliance with the License.
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+# the License for the specific language governing rights and limitations
+# under the License.
+#
+# The Original Source Code is "teem", released March 23, 2001.
+#  
+# The Original Source Code was developed by the University of Utah.
+# Portions created by UNIVERSITY are Copyright (C) 2001, 1998 University
+# of Utah. All Rights Reserved.
+#
+#
+CC = /opt/SUNWspro/bin/cc
+LD = /usr/ccs/bin/ld
+AR = /usr/ccs/bin/ar
+RM = /usr/bin/rm -f
+INSTALL = /usr/local/gnu/bin/install
+CHMOD = /usr/bin/chmod
+
+SHEXT = so
+OPT_CFLAG = -xO2
+STATIC_CFLAG = -Bstatic
+SHARED_CFLAG = -Bdynamic
+SHARED_LDFLAG = -G
+ARCH_CFLAG = 
+ARCH_LDFLAG = 
