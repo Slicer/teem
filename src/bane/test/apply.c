445e101568f9996276b5329aff66cc4fe0fe5c6d
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2001-04-10 09:54:39 +0000

    various changes of undisclosed nature
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@122 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/bane/test/apply.c
+++ src/bane/test/apply.c
@@ -29,7 +29,6 @@ usage() {
 
 int
 main(int argc, char *argv[]) {
-  FILE *file;
   int measr;
   char *iStr, *mStr, *oStr;
   Nrrd *nin, *nout;
@@ -42,30 +41,23 @@ main(int argc, char *argv[]) {
   mStr = argv[2];
   oStr = argv[3];
   
-  if (!(file = fopen(iStr, "r"))) {
-    fprintf(stderr, "%s: couldn't open %s for reading\n", me, iStr);
-    usage();
-  }
-  if (!(nin = nrrdNewRead(file))) {
+  if (!(nin = nrrdNewLoad(iStr))) {
     fprintf(stderr, "%s: trouble reading input nrrd:\n%s\n", me, 
 	    biffGet(NRRD));
     usage();
   }
-  fclose(file);
+
   if (1 != sscanf(mStr, "%d", &measr)) {
     fprintf(stderr, "%s: couldn't parse %s as int\n", me, mStr);
     usage();
   }
-
+  
   if (!(nout = baneNewApplyMeasr(nin, measr))) {
     fprintf(stderr, "%s: trouble:\n%s\n", me, biffGet(BANE));
     exit(1);
   }
-  if (!(file = fopen(oStr, "w"))) {
-    fprintf(stderr, "%s: couldn't open %s for writing\n", me, oStr);
-    exit(1);
-  }
-  if (nrrdWrite(file, nout)) {
+
+  if (nrrdSave(oStr, nout)) {
     fprintf(stderr, "%s: trouble writing output nrrd:\n%s\n", me,
 	    biffGet(NRRD));
     usage();
--- src/bane/test/tprobe.c
+++ src/bane/test/tprobe.c
@@ -21,21 +21,43 @@
 
 int
 main(int argc, char *argv[]) {
+  char *me;
   float ans[BANE_PROBE_ANSLEN], *data, x, y, z;
   baneProbeK3Pack *pack;
   Nrrd *nin, *nout;
-  int query, size, bx, by, bz, xi, yi, zi;
+  int query, size, bx, by, bz, xi, yi, zi, ho;
+  double t0, t1;
 
+  me = argv[0];
   baneProbeDebug = 0;
 
   pack = baneProbeK3PackNew();
+
+
   pack->k[0] = nrrdKernelBCCubic;
   pack->k[1] = nrrdKernelBCCubicD;
   pack->k[2] = nrrdKernelBCCubicDD;
+
   pack->param[0][0] = 1.0; pack->param[0][1] = 1.0; pack->param[0][2] = 0;
   pack->param[1][0] = 1.0; pack->param[1][1] = 1.0; pack->param[1][2] = 0;
   pack->param[2][0] = 1.0; pack->param[2][1] = 1.0; pack->param[2][2] = 0;
 
+  /*
+  pack->param[0][0] = 1.0; pack->param[0][1] = 0.0; pack->param[0][2] = 0.5;
+  pack->param[1][0] = 1.0; pack->param[1][1] = 0.0; pack->param[1][2] = 0.5;
+  pack->param[2][0] = 1.0; pack->param[2][1] = 0.0; pack->param[2][2] = 0.5;
+  */
+
+  /*
+  pack->k[0] = nrrdKernelAQuartic;
+  pack->k[1] = nrrdKernelAQuarticD;
+  pack->k[2] = nrrdKernelAQuarticDD;
+  pack->param[0][0] = 1.0; pack->param[0][1] = 0.25;
+  pack->param[1][0] = 1.0; pack->param[1][1] = 0.25;
+  pack->param[2][0] = 1.0; pack->param[2][1] = 0.25;
+  */
+
+  /*
   printf("v = [\n");
   for (xi=0; xi<=30; xi++) {
     x = AIR_AFFINE(0, xi, 30, -2, 2);
@@ -44,38 +66,52 @@ main(int argc, char *argv[]) {
     printf("%g %g %g;\n", x, y, z);
   }
   printf("]\n");
-
-  /*
-  pack = baneProbeK3PackNew();
-  pack->k[0] = nrrdKernelTent;
-  pack->k[1] = nrrdKernelForwDiff;
   */
 
-  nin = nrrdNewOpen("test/engine-crop.nrrd");
-  query = BANE_PROBE_GRADMAG | BANE_PROBE_2NDDD;
+  if (!(nin = nrrdNewLoad(argv[1]))) {
+    fprintf(stderr, "%s: can't open it.\n", me);
+  }
+  query = BANE_PROBE_GRADMAG | BANE_PROBE_2NDDD | BANE_PROBE_HESS;
+  ho = baneProbeAnsOffset[baneProbeHess];
 
-  bx = 30;
-  by = 80;
-  bz = 10;
-  size = 120;
-  nout = nrrdNewAlloc(size*size*size, nrrdTypeFloat, 3);
-  nout->size[0] = size;
+  bx = 4;
+  by = 4;
+  bz = 4;
+  size = 100;
+  nout = nrrdNewAlloc(6*size*size*size, nrrdTypeFloat, 4);
+  nout->size[0] = 6;
   nout->size[1] = size;
   nout->size[2] = size;
+  nout->size[3] = size;
   data = nout->data;
+  t0 = airDoubleTime();
   for (zi=0; zi<=size-1; zi++) {
-    z = AIR_AFFINE(0, zi, size-1, bx, bx+50);
+    z = AIR_AFFINE(0, zi, size-1, bx, bx+100);
     for (yi=0; yi<=size-1; yi++) {
-      y = AIR_AFFINE(0, yi, size-1, by, by+50);
+      y = AIR_AFFINE(0, yi, size-1, by, by+100);
       for (xi=0; xi<=size-1; xi++) {
-	x = AIR_AFFINE(0, xi, size-1, bz, bz+50);
+	x = AIR_AFFINE(0, xi, size-1, bz, bz+100);
 	baneProbe(ans, nin, query, pack, x, y, z);
+	
+	data[0 + 6*(xi + size*(yi + size*zi))] = ans[0 + ho];
+	data[1 + 6*(xi + size*(yi + size*zi))] = ans[1 + ho];
+	data[2 + 6*(xi + size*(yi + size*zi))] = ans[2 + ho];
+	data[3 + 6*(xi + size*(yi + size*zi))] = ans[3 + ho];
+	data[4 + 6*(xi + size*(yi + size*zi))] = ans[4 + ho];
+	data[5 + 6*(xi + size*(yi + size*zi))] = ans[5 + ho];
+
+	/*
 	data[xi + size*(yi + size*zi)] = 
 	  ans[baneProbeAnsOffset[baneProbeGradMag]];
-	/* data[xi + size*(yi + size*zi)] = *ans; */
+	*/
+	/*
+	data[xi + size*(yi + size*zi)] = *ans;
+	*/
       }
     }
   }
+  t1 = airDoubleTime();
+  printf("probe rate = %g/sec\n", size*size*size/(t1-t0));
   
   nrrdSave("out.nrrd", nout);
 
