9f28b0143eb0b416a0339f9897ef209620ed12e2
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2001-05-14 06:53:24 +0000

    wee little test programs
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@182 3d70eeeb-363e-0410-a505-8a46323a89f2

---
+++ src/ell/test/cubic.c
@@ -0,0 +1,76 @@
+/*
+  The contents of this file are subject to the University of Utah Public
+  License (the "License"); you may not use this file except in
+  compliance with the License.
+  
+  Software distributed under the License is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+  the License for the specific language governing rights and limitations
+  under the License.
+
+  The Original Source Code is "teem", released March 23, 2001.
+  
+  The Original Source Code was developed by the University of Utah.
+  Portions created by UNIVERSITY are Copyright (C) 2001, 1998 University
+  of Utah. All Rights Reserved.
+*/
+
+
+#include <stdio.h>
+#include <ell.h>
+
+char *me;
+
+void
+usage() {
+  /*                      0   1   2   3   (4) */
+  fprintf(stderr, "usage: %s <A> <B> <C>\n", me);
+  fprintf(stderr, "for cubic x^3 + Ax^2 + Bx + C == 0\n");
+  exit(1);
+}
+
+int
+main(int argc, char **argv) {
+  char buf[512];
+  double ans0, ans1, ans2, A, B, C;
+  int ret;
+  double r[3];
+
+  me = argv[0];
+  if (argc != 4) {
+    usage();
+  }
+
+  sprintf(buf, "%s %s %s", argv[1], argv[2], argv[3]);
+  if (3 != sscanf(buf, "%lf %lf %lf", &A, &B, &C)) {
+    fprintf(stderr, "%s: couldn't parse 3 floats from command line\n", me);
+    exit(1);
+  }
+
+  ellDebug = AIR_TRUE;
+  ret = ellCubic(A, B, C, r, AIR_TRUE);
+  ans0 = C + r[0]*(B + r[0]*(A + r[0]));
+  switch(ret) {
+  case ellCubicRootSingle:
+    printf("1 single root: %lg -> %lf\n", r[0], ans0);
+    break;
+  case ellCubicRootTriple:
+    printf("1 triple root: %lg -> %lf\n", r[0], ans0);
+    break;
+  case ellCubicRootSingleDouble:
+    ans1 = C + r[1]*(B + r[1]*(A + r[1]));
+    printf("1 single root %lg -> %lf, 1 double root %lg -> %lf\n", 
+	   r[0], ans0, r[1], ans1);
+    break;
+  case ellCubicRootThree:
+    ans1 = C + r[1]*(B + r[1]*(A + r[1]));
+    ans2 = C + r[2]*(B + r[2]*(A + r[2]));
+    printf("3 distinct roots:\n %lg -> %lf\n %lg -> %lf\n %lg -> %lf\n",
+	   r[0], ans0, r[1], ans1, r[2], ans2);
+    break;
+  default:
+    printf("%s: something fatally wacky happened\n", me);
+    exit(1);
+  }
+  exit(0);
+}
---
+++ src/ell/test/sort3.c
@@ -0,0 +1,57 @@
+/*
+  The contents of this file are subject to the University of Utah Public
+  License (the "License"); you may not use this file except in
+  compliance with the License.
+  
+  Software distributed under the License is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+  the License for the specific language governing rights and limitations
+  under the License.
+
+  The Original Source Code is "teem", released March 23, 2001.
+  
+  The Original Source Code was developed by the University of Utah.
+  Portions created by UNIVERSITY are Copyright (C) 2001, 1998 University
+  of Utah. All Rights Reserved.
+*/
+
+
+#include <stdio.h>
+#include <ell.h>
+
+char *me;
+
+void
+usage() {
+  /*                      0   1   2   3   (4) */
+  fprintf(stderr, "usage: %s <a> <b> <c>\n", me);
+  exit(1);
+}
+
+int
+main(int argc, char *argv[]) {
+  char *aS, *bS, *cS;
+  float a, b, c, t;
+
+  me = argv[0];
+  if (4 != argc) {
+    usage();
+  }
+
+  aS = argv[1];
+  bS = argv[2];
+  cS = argv[3];
+  if (3 != (sscanf(aS, "%g", &a) +
+	    sscanf(bS, "%g", &b) +
+	    sscanf(cS, "%g", &c))) {
+    fprintf(stderr, "%s: couldn't parse \"%s\", \"%s\", \"%s\" as floats\n",
+	    me, aS, bS, cS);
+    usage();
+  }
+  
+  printf("%g %g %g --> ", a, b, c);
+  ELL_SORT3(a, b, c, t);
+  printf("%g %g %g\n", a, b, c);
+
+  exit(0);
+}
