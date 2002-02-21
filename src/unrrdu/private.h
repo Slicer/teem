4c5d4949caeeecd977ae8d24b1de424a10f5913c
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2002-02-21 10:41:02 +0000

    blah
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@321 3d70eeeb-363e-0410-a505-8a46323a89f2

---
+++ src/unrrdu/private.h
@@ -0,0 +1,52 @@
+/*
+  The contents of this file are subject to the University of Utah Public
+  License (the "License"); you may not use this file except in
+  compliance with the License.
+  
+  Software distributed under the License is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+  the License for the specific language governing rights and limitations
+  under the License.
+
+  The Original Source Code is "teem", released March 23, 2001.
+  
+  The Original Source Code was developed by the University of Utah.
+  Portions created by UNIVERSITY are Copyright (C) 2001, 1998 University
+  of Utah. All Rights Reserved.
+*/
+
+#include <air.h>
+#include <biff.h>
+#include <hest.h>
+#include <nrrd.h>
+
+/*
+** OPT_ADD_...
+**
+** These macros are used for setting up command-line options for the various
+** unu commands.  They define options which are common across many different
+** commands, so that the unu interface is as consistent as possible.  They
+** all assume a hestOpt *opt variable, but they take the option variable
+** and option description as arguments.
+*/
+#define OPT_ADD_NIN(var, desc) \
+  hestOptAdd(&opt, "i|input", "nin", airTypeOther, 1, 1, &(var), "-", desc, \
+	     NULL, &hestNrrdCB)
+#define OPT_ADD_NOUT(var, desc) \
+  hestOptAdd(&opt, "o|output", "nout", airTypeString, 1, 1, &(var), "-", desc)
+#define OPT_ADD_AXIS(var, desc) \
+  hestOptAdd(&opt, "a|axis", "axis", airTypeInt, 1, 1, &(var), NULL, desc)
+#define OPT_ADD_POS(var, desc) \
+  hestOptAdd(&opt, "p|position", "pos", airTypeInt, 1, 1, &(var), NULL, desc)
+#define OPT_ADD_TYPE(var, desc) \
+  hestOptAdd(&opt, "t|type", "type", airTypeOther, 1, 1, &(var), NULL, desc, \
+             NULL, typeCB);
+
+extern hestParm *hparm;
+extern hestCB hestNrrdCB;
+
+typedef struct {
+  char *name, *info;
+  int (*main)(int, char **, char*);
+} unuCmd;
+
