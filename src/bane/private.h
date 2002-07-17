2ac8c10fa4caeabca3d54ecaf0834047fe810e56
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2002-07-17 23:45:55 +0000

    1.4
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@591 3d70eeeb-363e-0410-a505-8a46323a89f2

---
+++ src/bane/private.h
@@ -0,0 +1,61 @@
+/*
+  teem: Gordon Kindlmann's research software
+  Copyright (C) 2002, 2001, 2000, 1999, 1998 University of Utah
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef BANE_PRIVATE_HAS_BEEN_INCLUDED
+#define BANE_PRIVATE_HAS_BEEN_INCLUDED
+
+/* range.c */
+extern baneRange _baneRangePos;
+extern baneRange _baneRangeNeg;
+extern baneRange _baneRangeZeroCent;
+extern baneRange _baneRangeFloat;
+
+/* inc.c */
+extern Nrrd *_baneInc_HistNew(double *incParm);
+extern baneInc _baneIncAbsolute;
+extern baneInc _baneIncRangeRatio;
+extern baneInc _baneIncPercentile;
+extern baneInc _baneIncStdv;
+
+/* measr.c */
+extern baneMeasr _baneMeasrVal;
+extern baneMeasr _baneMeasrGradMag;
+extern baneMeasr _baneMeasrLapl;
+extern baneMeasr _baneMeasrHess;
+extern baneMeasr _baneMeasrCurvedness;
+extern baneMeasr _baneMeasrShadeTrace;
+
+/* clip.c */
+extern baneClip _baneClipAbsolute;
+extern baneClip _baneClipPeakRatio;
+extern baneClip _baneClipPercentile;
+extern baneClip _baneClipTopN;
+
+/* hvol.c */
+extern int _baneValidAxis(baneAxis *ax);
+
+#endif /* BANE_PRIVATE_HAS_BEEN_INCLUDED */
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/bane/test/tblah.c
@@ -0,0 +1,95 @@
+/*
+  teem: Gordon Kindlmann's research software
+  Copyright (C) 2002, 2001, 2000, 1999, 1998 University of Utah
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "../bane.h"
+
+char *me;
+
+void
+rangeTest(char *me, double imin, double imax) {
+  double omin, omax;
+  baneRange *range;
+  int i;
+
+  printf("input range (%g,%g) ---------------------\n", imin, imax);
+  for (i=1; i<=BANE_RANGE_MAX; i++) {
+    range = baneRangeArray[i];
+    range->ans(&omin, &omax, imin, imax);
+    printf("%s: range %s --> (%g,%g)\n",
+	   me, range->name, omin, omax);
+  }
+}
+
+void
+incTest(char *me, int num, baneRange *range) {
+  double *val, tmp, incParm[BANE_INC_PARM_NUM], omin, omax;
+  baneInc *inc;
+  Nrrd *hist;
+  int i, j;
+
+  val = (double*)malloc(num*sizeof(double));
+  for (j=0; j<num; j++) {
+    tmp = AIR_AFFINE(0.0, airRand(), 1.0, -1.0, 2.0);
+    val[j] = tmp*tmp*tmp;
+  }
+  
+  for (i=1; i<=BANE_INC_MAX; i++) {
+    inc = baneIncArray[i];
+    printf("%s: inclusion %s ------\n", me, inc->name);
+    switch(i) {
+    case baneIncAbsolute_e:
+      ELL_3V_SET(incParm, -0.8, 1.5, AIR_NAN);
+      break;
+    case baneIncRangeRatio_e:
+      ELL_3V_SET(incParm, 0.9, AIR_NAN, AIR_NAN);
+      break;
+    case baneIncPercentile_e:
+      break;
+    case baneIncStdv_e:
+      break;
+    }
+    hist = inc->histNew(incParm);
+    if (inc->passA) {
+      for (j=0; j<num; j++)
+	inc->passA(hist, val[j], incParm);
+    }
+    if (inc->passB) {
+      for (j=0; j<num; j++)
+	inc->passB(hist, val[j], incParm);
+    }
+    inc->ans(&omin, &omax, hist, incParm, range);
+    printf(" --> (%g,%g)\n", omin, omax);
+  }
+
+  free(val);
+}
+
+int
+main(int argc, char *argv[]) {
+  me = argv[0];
+
+  printf("%s ================================ range testing\n", me);
+  rangeTest(me, -1, 2);
+  rangeTest(me, -3, 2);
+
+  printf("%s ================================ inclusion testing\n", me);
+  incTest(me, 1000, baneRangeFloat);
+
+  return 0;
+}
