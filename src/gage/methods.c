166e16245460ba6043305c3258acae52c34bda3a
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2002-08-19 09:45:38 +0000

    finished gageContextCopy, fixed gagePerVolumeNew
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@721 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/gage/methods.c
+++ src/gage/methods.c
@@ -23,52 +23,48 @@
 /*
 ******** gageContextNew()
 **
+** doesn't use biff
 */
 gageContext *
 gageContextNew() {
-  char me[]="gageContextNew", err[AIR_STRLEN_MED];
-  int i;
   gageContext *ctx;
+  int i;
   
   ctx = (gageContext*)calloc(1, sizeof(gageContext));
-  if (!(ctx)) {
-    sprintf(err, "%s: couldn't allocate struct", me);
-    biffAdd(GAGE, err); return NULL;
+  if (ctx) {
+    ctx->verbose = gageDefVerbose;
+    ctx->gradMagMin = gageDefGradMagMin;
+    ctx->renormalize = gageDefRenormalize;
+    ctx->checkIntegrals = gageDefCheckIntegrals;
+    ctx->noRepadWhenSmaller = gageDefNoRepadWhenSmaller;
+    ctx->integralNearZero = gageDefIntegralNearZero;
+    ctx->k3pack = gageDefK3Pack;
+    gageKernelReset(ctx);
+    for (i=0; i<GAGE_PERVOLUME_NUM; i++)
+      ctx->pvl[i] = NULL;
+    ctx->numPvl = 0;
+    ctx->padder = _gageStandardPadder;
+    ctx->nixer = _gageStandardNixer;
+    for (i=0; i<GAGE_FLAG_NUM; i++) {
+      ctx->flag[i] = AIR_FALSE;
+      ctx->pvlFlag[i] = AIR_FALSE;
+    }
+    ctx->thisIsACopy = AIR_FALSE;
+    ctx->needPad = ctx->havePad = ctx->fr = ctx->fd = -1;
+    ctx->fsl = ctx->fw = NULL;
+    ctx->off = NULL;
+    ctx->sx = ctx->sy = ctx->sz = -1;
+    ctx->xs = ctx->ys = ctx->zs = AIR_NAN;
+    for (i=gageKernelUnknown+1; i<gageKernelLast; i++) {
+      ctx->fwScale[i][0] = ctx->fwScale[i][1] = ctx->fwScale[i][2] = AIR_NAN;
+    }
+    ctx->needD[0] = ctx->needD[1] = ctx->needD[2] = AIR_FALSE;
+    for (i=gageKernelUnknown+1; i<gageKernelLast; i++) {
+      ctx->needK[i] = AIR_FALSE;
+    }
+    ctx->xf = ctx->xf = ctx->xf = AIR_NAN;
+    ctx->bidx = -1;
   }
-
-  ctx->verbose = gageDefVerbose;
-  ctx->gradMagMin = gageDefGradMagMin;
-  ctx->renormalize = gageDefRenormalize;
-  ctx->checkIntegrals = gageDefCheckIntegrals;
-  ctx->noRepadWhenSmaller = gageDefNoRepadWhenSmaller;
-  ctx->integralNearZero = gageDefIntegralNearZero;
-  ctx->k3pack = gageDefK3Pack;
-  gageKernelReset(ctx);
-  for (i=0; i<GAGE_PERVOLUME_NUM; i++)
-    ctx->pvl[i] = NULL;
-  ctx->numPvl = 0;
-  ctx->padder = _gageStandardPadder;
-  ctx->nixer = _gageStandardNixer;
-  for (i=0; i<GAGE_FLAG_NUM; i++) {
-    ctx->flag[i] = AIR_FALSE;
-    ctx->pvlFlag[i] = AIR_FALSE;
-  }
-  ctx->thisIsACopy = AIR_FALSE;
-  ctx->needPad = ctx->havePad = ctx->fr = ctx->fd = -1;
-  ctx->fsl = ctx->fw = NULL;
-  ctx->off = NULL;
-  ctx->sx = ctx->sy = ctx->sz = -1;
-  ctx->xs = ctx->ys = ctx->zs = AIR_NAN;
-  for (i=gageKernelUnknown+1; i<gageKernelLast; i++) {
-    ctx->fwScale[i][0] = ctx->fwScale[i][1] = ctx->fwScale[i][2] = AIR_NAN;
-  }
-  ctx->needD[0] = ctx->needD[1] = ctx->needD[2] = AIR_FALSE;
-  for (i=gageKernelUnknown+1; i<gageKernelLast; i++) {
-    ctx->needK[i] = AIR_FALSE;
-  }
-  ctx->xf = ctx->xf = ctx->xf = AIR_NAN;
-  ctx->bidx = -1;
-
   return ctx;
 }
 
@@ -85,14 +81,69 @@ gageContextNew() {
 ** to do the exact same thing in many different threads.  If you want to 
 ** change state, then gageContextNix() all the copy contexts, gage...Set(),
 ** gageUpdate(), and gageContextCopy() again.
+**
+** Does use biff, unlike all other functions in this file
 */
 gageContext *
 gageContextCopy(gageContext *ctx) {
-  char me[]="gageContextCopy" /*, err[AIR_STRLEN_MED] */ ;
+  char me[]="gageContextCopy", err[AIR_STRLEN_MED];
+  gageContext *ntx;
+  gagePerVolume *pvl;
+  int i, E;
 
-  fprintf(stderr, "%s: sorry, not yet implemented.\n", me);
+  ntx = gageContextNew();
+  if (!ntx) {
+    sprintf(err, "%s: couldn't make a gageContext", me);
+    biffAdd(GAGE, err); return NULL;
+  }
+  gageSet(ntx, gageVerbose, ctx->verbose);
+  gageSet(ntx, gageRenormalize, ctx->renormalize);
+  gageSet(ntx, gageCheckIntegrals, ctx->checkIntegrals);
+  gageSet(ntx, gageNoRepadWhenSmaller, ctx->noRepadWhenSmaller);
+  gageSet(ntx, gageK3Pack, ctx->k3pack);
 
-  return NULL;
+  /* This is how to do the cleverness which makes this function useful.
+     We'll explicitly set pvl->nin below, but because we're setting the
+     padder and nixer to NULL, nothing is ever done with npad, so that
+     only the original context will free it on gageContextNix() */
+  gagePadderSet(ntx, NULL);
+  gageNixerSet(ntx, NULL);
+
+  /* gageSet is only good for ints... */
+  ntx->gradMagMin = ctx->gradMagMin;
+  ntx->integralNearZero = ctx->integralNearZero;
+
+  /* now start the things that can generate errors */
+  E = 0;
+  for (i=0; i<GAGE_KERNEL_NUM; i++) {
+    if (ctx->k[i]) {
+      if (!E) E |= gageKernelSet(ntx, i, ctx->k[i], ctx->kparm[i]);
+    }
+  }
+  if (E) {
+    sprintf(err, "%s: trouble setting kernels", me);
+    biffAdd(GAGE, err); return NULL;
+  }
+  for (i=0; i<ctx->numPvl; i++) {
+    pvl = gagePerVolumeNew(ctx->pvl[i]->kind);
+    if (!pvl) {
+      sprintf(err, "%s: couldn't create pvl %d", me, i);
+      biffAdd(GAGE, err); return NULL;
+    }
+    if (!E) E |= gagePerVolumeAttach(ntx, pvl);
+    if (!E) E |= gageQuerySet(ntx, pvl, ctx->pvl[i]->query);
+    if (!E) E |= gageVolumeSet(ntx, pvl, ctx->pvl[i]->nin);
+    if (!E) pvl->padInfo = ctx->pvl[i]->padInfo;
+    /* the sneakiness */
+    if (!E) pvl->npad = ctx->pvl[i]->npad;
+  }
+  if (!E) E |= gageUpdate(ntx);
+  if (E) {
+    sprintf("%s:", me);
+    biffAdd(GAGE, err); return NULL;
+  }
+
+  return ntx;
 }
 
 
@@ -102,6 +153,8 @@ gageContextCopy(gageContext *ctx) {
 ** responsible for freeing and clearing up everything hanging off a 
 ** context so that things can be returned to the way they were prior
 ** to gageContextNew().
+**
+** does not use biff
 */
 gageContext *
 gageContextNix(gageContext *ctx) {
@@ -123,34 +176,30 @@ gageContextNix(gageContext *ctx) {
 **
 ** creates a new pervolume of a known kind, but nothing besides the
 ** answer struct is allocated
+**
+** does not use biff
 */
 gagePerVolume *
 gagePerVolumeNew(gageKind *kind) {
-  char me[]="gagePerVolumeNew", err[AIR_STRLEN_MED];
   gagePerVolume *pvl;
 
   if (!( kind )) {
-    sprintf(err, "%s: got NULL pointer", me);
-    biffAdd(GAGE, err); return NULL;
+    return NULL;
   }
   pvl = (gagePerVolume *)calloc(1, sizeof(gagePerVolume));
-  if (!( pvl )) {
-    sprintf(err, "%s: couldn't allocate struct", me);
-    biffAdd(GAGE, err); return NULL;
+  if (pvl) {
+    pvl->kind = kind;
+    pvl->query = 0;
+    pvl->nin = NULL;
+    pvl->padInfo = NULL;
+    pvl->npad = NULL;
+    pvl->thisIsACopy = AIR_FALSE;
+    pvl->flagNin = AIR_FALSE;
+    pvl->iv3 = pvl->iv2 = pvl->iv1 = NULL;
+    pvl->lup = NULL;
+    pvl->needD[0] = pvl->needD[1] = pvl->needD[2] = AIR_FALSE;
+    pvl->ansStruct = pvl->kind->ansNew();
   }
-
-  pvl->kind = kind;
-  pvl->query = 0;
-  pvl->nin = NULL;
-  pvl->padInfo = NULL;
-  pvl->npad = NULL;
-  pvl->thisIsACopy = AIR_FALSE;
-  pvl->flagNin = AIR_FALSE;
-  pvl->iv3 = pvl->iv2 = pvl->iv1 = NULL;
-  pvl->lup = NULL;
-  pvl->needD[0] = pvl->needD[1] = pvl->needD[2] = AIR_FALSE;
-  pvl->ansStruct = pvl->kind->ansNew();
-
   return pvl;
 }
 
@@ -162,6 +211,8 @@ gagePerVolumeNew(gageKind *kind) {
 **
 ** Unfortunately, because the nixer is in the gageContext, that has
 ** to be passed to this function.
+**
+** does not use biff
 */
 gagePerVolume *
 gagePerVolumeNix(gageContext *ctx, gagePerVolume *pvl) {
