cd2b7dc2bfc2e20d046105a90140a6b5e6ce1ab0
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2002-09-30 07:59:06 +0000

    starting to add support for encoding flags
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@987 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/unrrdu/save.c
+++ src/unrrdu/save.c
@@ -20,6 +20,57 @@
 #include "unrrdu.h"
 #include "privateUnrrdu.h"
 
+/*
+******** unrrduHestSaveEncCB
+** 
+** for parsing output encoding information.  The compression encodings
+** can have flags for controlling the compression level and strategy
+** val[0]: 
+*/
+int
+unrrduParseSaveEnc(void *ptr, char *_str, char err[AIR_STRLEN_HUGE]) {
+  char me[]="unrrduParseSaveEnc", *str;
+  int *val;
+  char *col;
+
+  if (!(ptr && _str)) {
+    sprintf(err, "%s: got NULL pointer", me);
+    return 1;
+  }
+  val = ptr;
+  str = airStrdup(_str);
+  val[0] = val[1] = val[2] = -1;
+  val[0] = airEnumVal(nrrdEncoding, str);
+  if (val[0] != nrrdEncodingUnknown) {
+    /* we got a simple encoding string, we're done */
+    free(str); return 0;
+  }
+  col = strchr(str, ':');
+  if (!col) {
+    sprintf(err, "%s: \"%s\" not simple encoding, but no colon seen", me, str);
+    free(str); return 1;
+  }
+  *col = '\0';
+  val[0] = airEnumVal(nrrdEncoding, str);
+  if (nrrdEncodingUnknown == val[0]) {
+    sprintf(err, "%s: \"%s\" not a recognized encoding", me, str);
+    free(str); return 0;
+  }
+  if (!(nrrdEncodingIsCompression[val[0]])) {
+    sprintf(err, "%s: only compression encodings have parameters", me);
+    free(str); return 0;
+  }
+  
+  return 0;
+}
+
+hestCB unrrduHestSaveEncCB = {
+  3*sizeof(int),
+  "output encoding",
+  unrrduParseSaveEnc,
+  NULL
+};
+
 /* bad bad bad Gordon */
 extern void _nrrdGuessFormat(NrrdIO *io, const char *filename);
 
