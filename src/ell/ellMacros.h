a2b75b6d1faeeeac224a507634a2230fb521f264
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2003-03-31 11:32:18 +0000

    matrix L2NORM --> FROBNORM
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@1331 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/ell/ellMacros.h
+++ src/ell/ellMacros.h
@@ -246,7 +246,7 @@ extern "C" {
 
 #define ELL_3M_TRACE(m) ((m)[0] + (m)[4] + (m)[8])
 
-#define ELL_3M_L2NORM(m) \
+#define ELL_3M_FROBNORM(m) \
   (sqrt((m)[0]*(m)[0] + (m)[1]*(m)[1] + (m)[2]*(m)[2] + \
         (m)[3]*(m)[3] + (m)[4]*(m)[4] + (m)[5]*(m)[5] + \
         (m)[6]*(m)[6] + (m)[7]*(m)[7] + (m)[8]*(m)[8]))
