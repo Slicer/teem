0a01a9db64ed9a9941524fdfca98c6090890b0ef
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2003-04-04 23:01:55 +0000

    added TEEM_BIGBITFIELD, and new TEEM_ARCH aix
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@1344 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/make/errorCheck.mk
+++ src/make/errorCheck.mk
@@ -23,7 +23,7 @@
 #### use
 
 # all the architectures currently supported
-KNOWN_ARCH = irix6.n32 irix6.64 linux cygwin solaris darwin
+KNOWN_ARCH = irix6.n32 irix6.64 linux cygwin solaris darwin aix
 
 # there is no default architecture
 checkArchSet = $(if $(TEEM_ARCH),,\
