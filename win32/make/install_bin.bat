cae3a901800e5d8de25ba6ed89527c2849a831a1
ikits <ikits@3d70eeeb-363e-0410-a505-8a46323a89f2>
2003-05-09 18:57:02 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@1374 3d70eeeb-363e-0410-a505-8a46323a89f2

--- win32/make/install_bin.bat
+++ win32/make/install_bin.bat
@@ -3,9 +3,9 @@ if "%TEEM_DEST%" == "" goto local
 set __bdest=%TEEM_DEST%\bin
 goto install
 :local
-set __bdest=..\..\bin
+set __bdest=..\..\bin\%2
 :install
 echo Installing %1.exe into %__bdest%
 mkdir "%__bdest%"
-copy /y /b ..\bin\%1.exe "%__bdest%"
+copy /y /b ..\bin\%2\%1.exe "%__bdest%"
 :end
