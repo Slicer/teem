2dbad504680837b3761c731ef135d7f98c4ea9a8
gk <gk@3d70eeeb-363e-0410-a505-8a46323a89f2>
2003-05-22 23:47:09 +0000

    Fixed three serious bugs in ell3mEigensolve: 1) 1D and 2D nullspace calculation was sometimes wrong because the heuristic for aligning vectors (so that their pair-wise dot-products are positive) was broken 2) When there is a single and double root, with two large roots and one small root, the eigenvalues and eigenvectors returned were all wrong 3) The eigenframes returned in the eigenvectors were sometimes right- handed, and sometimes left-handed, which complicates using them as a rotation matrix.  They are now always right-handed.
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@1390 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/ell/eigen.c
+++ src/ell/eigen.c
@@ -20,6 +20,84 @@
 
 #include "ell.h"
 
+/* lop A
+  fprintf(stderr, "_ellAlign3: ----------\n");
+  fprintf(stderr, "_ellAlign3: v0 = %g %g %g\n", (v+0)[0], (v+0)[1], (v+0)[2]);
+  fprintf(stderr, "_ellAlign3: v3 = %g %g %g\n", (v+3)[0], (v+3)[1], (v+3)[2]);
+  fprintf(stderr, "_ellAlign3: v6 = %g %g %g\n", (v+6)[0], (v+6)[1], (v+6)[2]);
+  fprintf(stderr, "_ellAlign3: d = %g %g %g -> %d %d %d\n",
+	  d0, d1, d2, Mi, ai, bi);
+  fprintf(stderr, "_ellAlign3:  pre dot signs (03, 06, 36): %d %d %d\n",
+	  airSgn(ELL_3V_DOT(v+0, v+3)),
+	  airSgn(ELL_3V_DOT(v+0, v+6)),
+	  airSgn(ELL_3V_DOT(v+3, v+6)));
+  */
+
+/* lop B
+  fprintf(stderr, "_ellAlign3: v0 = %g %g %g\n", (v+0)[0], (v+0)[1], (v+0)[2]);
+  fprintf(stderr, "_ellAlign3: v3 = %g %g %g\n", (v+3)[0], (v+3)[1], (v+3)[2]);
+  fprintf(stderr, "_ellAlign3: v6 = %g %g %g\n", (v+6)[0], (v+6)[1], (v+6)[2]);
+  fprintf(stderr, "_ellAlign3:  post dot signs %d %d %d\n",
+	  airSgn(ELL_3V_DOT(v+0, v+3)),
+	  airSgn(ELL_3V_DOT(v+0, v+6)),
+	  airSgn(ELL_3V_DOT(v+3, v+6)));
+  if (airSgn(ELL_3V_DOT(v+0, v+3)) < 0
+      || airSgn(ELL_3V_DOT(v+0, v+6)) < 0
+      || airSgn(ELL_3V_DOT(v+3, v+6)) < 0) {
+    exit(1);
+  }
+  */
+
+void
+_ellAlign3(double v[9]) {
+  double d0, d1, d2;
+  int Mi, ai, bi;
+  
+  d0 = ELL_3V_DOT(v+0, v+0);
+  d1 = ELL_3V_DOT(v+3, v+3);
+  d2 = ELL_3V_DOT(v+6, v+6);
+  Mi = ELL_MAX3_IDX(d0, d1, d2);
+  ai = (Mi + 1) % 3;
+  bi = (Mi + 2) % 3;
+  /* lop A */
+  if (ELL_3V_DOT(v+3*Mi, v+3*ai) < 0) {
+    ELL_3V_SCALE(v+3*ai, -1, v+3*ai);
+  }
+  if (ELL_3V_DOT(v+3*Mi, v+3*bi) < 0) {
+    ELL_3V_SCALE(v+3*bi, -1, v+3*bi);
+  }
+  /* lob B */
+  /* we can't guarantee that dot(v+3*ai,v+3*bi) > 0 ... */
+}
+
+void
+_ellFixerUpper(double v[9]) {
+  double x[3];
+  
+  ELL_3V_CROSS(x, v+3*0, v+3*1);
+  if (0 > ELL_3V_DOT(x, v+3*2)) {
+    ELL_3V_SCALE(v+3*2, -1, v+3*2);
+  }
+}
+
+/* lop A
+  fprintf(stderr, "===  pre ===\n");
+  fprintf(stderr, "crosses:  %g %g %g\n", (t+0)[0], (t+0)[1], (t+0)[2]);
+  fprintf(stderr, "          %g %g %g\n", (t+3)[0], (t+3)[1], (t+3)[2]);
+  fprintf(stderr, "          %g %g %g\n", (t+6)[0], (t+6)[1], (t+6)[2]);
+  fprintf(stderr, "cross dots:  %g %g %g\n",
+	  ELL_3V_DOT(t+0, t+3), ELL_3V_DOT(t+0, t+6), ELL_3V_DOT(t+3, t+6));
+*/
+
+/* lop B
+  fprintf(stderr, "=== post ===\n");
+  fprintf(stderr, "crosses:  %g %g %g\n", (t+0)[0], (t+0)[1], (t+0)[2]);
+  fprintf(stderr, "          %g %g %g\n", (t+3)[0], (t+3)[1], (t+3)[2]);
+  fprintf(stderr, "          %g %g %g\n", (t+6)[0], (t+6)[1], (t+6)[2]);
+  fprintf(stderr, "cross dots:  %g %g %g\n",
+	  ELL_3V_DOT(t+0, t+3), ELL_3V_DOT(t+0, t+6), ELL_3V_DOT(t+3, t+6));
+*/
+
 /*
 ******** ell3mNullspace1()
 **
@@ -33,27 +111,17 @@
 */
 void
 ell3mNullspace1(double ans[3], double n[9]) {
-  double t0[3], t1[3], t2[3], norm;
+  double t[9], norm;
   
   /* find the three cross-products of pairs of column vectors of n */
-  ELL_3V_CROSS(t0, n+0, n+3);
-  ELL_3V_CROSS(t1, n+0, n+6);
-  ELL_3V_CROSS(t2, n+3, n+6);
-
-  /* point them the same way: you might think that only the first two
-     of these three statements would be needed, but there is the 
-     possibility that t1 = (0,0,0), in which case you can end up with
-     t0 = -t2, so that when you add t0+t1+t2, you get (0,0,0), which
-     isn't easy to normalize ... */
-  if (ELL_3V_DOT(t0, t1) < 0)
-    ELL_3V_SCALE(t1, -1, t1);
-  if (ELL_3V_DOT(t1, t2) < 0)
-    ELL_3V_SCALE(t2, -1, t2);
-  if (ELL_3V_DOT(t0, t2) < 0)
-    ELL_3V_SCALE(t2, -1, t2);
-
-  /* add them up (longer, hence more accurate, ones will dominate) */
-  ELL_3V_ADD3(ans, t0, t1, t2);
+  ELL_3V_CROSS(t+0, n+0, n+3);
+  ELL_3V_CROSS(t+3, n+0, n+6);
+  ELL_3V_CROSS(t+6, n+3, n+6);
+  /* lop A */
+  _ellAlign3(t);
+  /* lop B */
+  /* add them up (longer, hence more accurate, should dominate) */
+  ELL_3V_ADD3(ans, t+0, t+3, t+6);
 
   /* normalize */
   ELL_3V_NORM(ans, ans, norm);
@@ -75,19 +143,8 @@ void
 ell3mNullspace2(double ans0[3], double ans1[3], double _n[9]) {
   double n[9], tmp[3], norm;
 
-  /* copy to local matrix */
-  memcpy(n, _n, 9*sizeof(double));
-  
-  /* make the column vectors point the same way: see the comment
-     above in ell3mNullspace1() regarding this operation */
-  if (ELL_3V_DOT(n+0, n+3) < 0)
-    ELL_3V_SCALE(n+3, -1, n+3);
-  if (ELL_3V_DOT(n+3, n+6) < 0)
-    ELL_3V_SCALE(n+6, -1, n+6);
-  if (ELL_3V_DOT(n+0, n+6) < 0)
-    ELL_3V_SCALE(n+6, -1, n+6);
-
-  /* add them up (longer, hence more accurate, ones will dominate) */
+  ELL_3M_COPY(n, _n);
+  _ellAlign3(n);
   ELL_3V_ADD3(tmp, n+0, n+3, n+6);
   ELL_3V_NORM(tmp, tmp, norm);
   
@@ -167,10 +224,9 @@ ell3mEigensolve(double eval[3], double evec[9], double m[9], int newton) {
     printf("lineal3Eigensolve: numroots = %d\n", numroots);
     } */
 
-  /* we form m - lambda.I by doing a memcpy from m, and then
+  /* we form m - lambda*I by doing a memcpy from m, and then
      (repeatedly) over-writing the diagonal elements */
-  memcpy(n, m, 9*sizeof(double));
-  /* ELL_3M_COPY(n, m); */
+  ELL_3M_COPY(n, m);
   switch (roots) {
   case ellCubicRootThree:
     ELL_SORT3(e0, e1, e2, t);
@@ -184,31 +240,34 @@ ell3mEigensolve(double eval[3], double evec[9], double m[9], int newton) {
     ell3mNullspace1(evec+3, n);
     ELL_3M_DIAG_SET(n, m[0]-e2, m[4]-e2, m[8]-e2);
     ell3mNullspace1(evec+6, n);
+    _ellFixerUpper(evec);
     ELL_3V_SET(eval, e0, e1, e2);
     break;
   case ellCubicRootSingleDouble:
+    ELL_SORT3(e0, e1, e2, t);
     if (e0 > e1) {
-      /* one big (e0) , two small (e1) : more like a cigar */
+      /* one big (e0) , two small (e1, e2) : more like a cigar */
       ELL_3M_DIAG_SET(n, m[0]-e0, m[4]-e0, m[8]-e0);
       ell3mNullspace1(evec+0, n);
       ELL_3M_DIAG_SET(n, m[0]-e1, m[4]-e1, m[8]-e1);
       ell3mNullspace2(evec+3, evec+6, n);
-      ELL_3V_SET(eval, e0, e1, e1);
     }
     else {
-      /* two big (e1), one small (e0): more like a pancake */
-      ELL_3M_DIAG_SET(n, m[0]-e1, m[4]-e1, m[8]-e1);
-      ell3mNullspace2(evec+0, evec+3, n);
+      /* two big (e0, e1), one small (e2): more like a pancake */
       ELL_3M_DIAG_SET(n, m[0]-e0, m[4]-e0, m[8]-e0);
+      ell3mNullspace2(evec+0, evec+3, n);
+      ELL_3M_DIAG_SET(n, m[0]-e2, m[4]-e2, m[8]-e2);
       ell3mNullspace1(evec+6, n);
-      ELL_3V_SET(eval, e1, e1, e0);
     }
+    _ellFixerUpper(evec);
+    ELL_3V_SET(eval, e0, e1, e2);
     break;
   case ellCubicRootTriple:
     /* one triple root; use any basis as the eigenvectors */
     ELL_3V_SET(evec+0, 1, 0, 0);
     ELL_3V_SET(evec+3, 0, 1, 0);
     ELL_3V_SET(evec+6, 0, 0, 1);
+    ELL_3V_SET(eval, e0, e1, e2);
     break;
   case ellCubicRootSingle:
     /* only one real root */
@@ -216,6 +275,7 @@ ell3mEigensolve(double eval[3], double evec[9], double m[9], int newton) {
     ell3mNullspace1(evec+0, n);
     ELL_3V_SET(evec+3, AIR_NAN, AIR_NAN, AIR_NAN);
     ELL_3V_SET(evec+6, AIR_NAN, AIR_NAN, AIR_NAN);
+    ELL_3V_SET(eval, e0, AIR_NAN, AIR_NAN);
     break;
   }
   /* if (ellDebug) {
