0cff49636060f91a2637ba08a8826870c2350e93
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2004-02-09 07:05:33 +0000

    now outputs an OFF file, instead of doing the rendering itself
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@2020 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/limn/test/soid.c
+++ src/limn/test/soid.c
@@ -20,7 +20,7 @@
 
 #include "../limn.h"
 
-char *info = ("Render a single ellipsoid in postscript.");
+char *info = ("Save a single ellipsoid or superquadric into an OFF file.");
 
 void
 washQtoM3(float m[9], float q[4]) {
@@ -52,7 +52,6 @@ washQtoM3(float m[9], float q[4]) {
 int
 main(int argc, char *argv[]) {
   char *me, *err, *outS;
-  limnCamera *cam;
   float p[3], q[4], mR[9], eval[3], len, sh, cl, cp, qA, qB;
   float matA[16], matB[16], os, sc[3], rad, edgeWidth[5];
   hestOpt *hopt=NULL;
@@ -60,34 +59,14 @@ main(int argc, char *argv[]) {
   limnObj *obj;
   limnSP *sp;
   limnPart *r;
-  limnWin *win;
   int ri, si, res, axis, sphere;
-  Nrrd *nmap;
+  FILE *file;
 
   mop = airMopNew();
-  cam = limnCameraNew();
-  airMopAdd(mop, cam, (airMopper)limnCameraNix, airMopAlways);
   
   edgeWidth[0] = 0;
   edgeWidth[1] = 0;
   me = argv[0];
-  hestOptAdd(&hopt, "fr", "from point", airTypeDouble, 3, 3, cam->from,"4 4 4",
-	     "position of camera, used to determine view vector");
-  hestOptAdd(&hopt, "at", "at point", airTypeDouble, 3, 3, cam->at, "0 0 0",
-	     "camera look-at point, used to determine view vector");
-  hestOptAdd(&hopt, "up", "up vector", airTypeDouble, 3, 3, cam->up, "0 0 1",
-	     "camera pseudo-up vector, used to determine view coordinates");
-  hestOptAdd(&hopt, "rh", NULL, airTypeInt, 0, 0, &(cam->rightHanded), NULL,
-	     "use a right-handed UVN frame (V points down)");
-  hestOptAdd(&hopt, "or", NULL, airTypeInt, 0, 0, &(cam->orthographic), NULL,
-	     "use orthogonal projection");
-  hestOptAdd(&hopt, "ur", "uMin uMax", airTypeDouble, 2, 2, cam->uRange,
-	     "-1 1", "range in U direction of image plane");
-  hestOptAdd(&hopt, "vr", "vMin vMax", airTypeDouble, 2, 2, cam->vRange,
-	     "-1 1", "range in V direction of image plane");
-  hestOptAdd(&hopt, "e", "envmap", airTypeOther, 1, 1, &nmap, NULL,
-	     "16checker-based environment map",
-	     NULL, NULL, nrrdHestNrrd);
   hestOptAdd(&hopt, "sc", "scalings", airTypeFloat, 3, 3, sc, "1 1 1",
 	     "axis-aligned scaling to do on ellipsoid");
   hestOptAdd(&hopt, "os", "over-all scaling", airTypeFloat, 1, 1, &os, "1",
@@ -96,45 +75,31 @@ main(int argc, char *argv[]) {
 	     "how much to sharpen edges as a "
 	     "function of differences between eigenvalues");
   hestOptAdd(&hopt, "sphere", NULL, airTypeInt, 0, 0, &sphere, NULL,
-	     "none of this superquadric crap: use a sphere, dammit");
+	     "use a sphere instead of a superquadric");
   hestOptAdd(&hopt, "p", "x y z", airTypeFloat, 3, 3, p, "0 0 0",
 	     "location in quaternion quotient space");
   hestOptAdd(&hopt, "r", "radius", airTypeFloat, 1, 1, &rad, "0.015",
 	     "black axis cylinder radius (or 0.0 to not drawn these)");
   hestOptAdd(&hopt, "res", "resolution", airTypeInt, 1, 1, &res, "25",
 	     "tesselation resolution for both glyph and axis cylinders");
-  hestOptAdd(&hopt, "wd", "3 widths", airTypeFloat, 3, 3, edgeWidth + 2,
-	     "1.5 0.7 0.0",
-	     "width of edges drawn for three kinds of "
-	     "edges: silohuette, crease, non-crease");
-  hestOptAdd(&hopt, "o", "output PS", airTypeString, 1, 1, &outS, "out.ps",
-	     "output file to render postscript into");
+  hestOptAdd(&hopt, "o", "output OFF", airTypeString, 1, 1, &outS, "out.off",
+	     "output file to save OFF into");
   hestParseOrDie(hopt, argc-1, argv+1, NULL,
 		 me, info, AIR_TRUE, AIR_TRUE, AIR_TRUE);
   airMopAdd(mop, hopt, (airMopper)hestOptFree, airMopAlways);
   airMopAdd(mop, hopt, (airMopper)hestParseFree, airMopAlways);
   
-  cam->neer = -0.000000001;
-  cam->dist = 0;
-  cam->faar = 0.0000000001;
-  cam->atRelative = AIR_TRUE;
-
-  if (limnCameraUpdate(cam)) {
-    fprintf(stderr, "%s: trouble:\n%s\n", me, err = biffGet(LIMN));
-    free(err);
-    return 1;
-  }
   obj = limnObjNew(10, AIR_TRUE);
   airMopAdd(mop, obj, (airMopper)limnObjNix, airMopAlways);
 
   /* create limnSPs for ellipsoid (#0) and for rods (#1) */
   si = airArrayIncrLen(obj->sA, 2);
   sp = obj->s + si + 0;
-  ELL_4V_SET(sp->rgba, 1, 1, 1, 1);  /* this is kind of silly */
+  ELL_4V_SET(sp->rgba, 1, 1, 1, 1);
   ELL_3V_SET(sp->k, 0.2, 0.8, 0);
   sp->spec = 0;
   sp = obj->s + si + 1;
-  ELL_4V_SET(sp->rgba, 1, 1, 1, 1);  /* this is kind of silly */
+  ELL_4V_SET(sp->rgba, 0, 0, 0, 1);
   ELL_3V_SET(sp->k, 1, 0, 0);
   sp->spec = 0;
 
@@ -168,7 +133,7 @@ main(int argc, char *argv[]) {
   } else {
     ri = limnObjPolarSuperquadAdd(obj, 0, axis, qA, qB, 2*res, res);
   }
-  r = obj->r + ri; ELL_4V_SET(r->rgba, 1, 1, 1, 1);
+  r = obj->r + ri;
   ELL_4M_IDENTITY_SET(matA);
   ELL_4M_SCALE_SET(matB, sc[0], sc[1], sc[2]);
   ell_4m_post_mul_f(matA, matB);
@@ -178,7 +143,7 @@ main(int argc, char *argv[]) {
 
   if (rad) {
     ri = limnObjCylinderAdd(obj, 1, 0, res);
-    r = obj->r + ri; ELL_4V_SET(r->rgba, 0, 0, 0, 1);
+    r = obj->r + ri;
     ELL_4M_IDENTITY_SET(matA);
     ELL_4M_SCALE_SET(matB, (1-sc[0])/2, rad, rad);
     ell_4m_post_mul_f(matA, matB);
@@ -187,7 +152,7 @@ main(int argc, char *argv[]) {
     limnObjPartTransform(obj, ri, matA);
     
     ri = limnObjCylinderAdd(obj, 1, 0, res);
-    r = obj->r + ri; ELL_4V_SET(r->rgba, 0, 0, 0, 1);
+    r = obj->r + ri;
     ELL_4M_IDENTITY_SET(matA);
     ELL_4M_SCALE_SET(matB, (1-sc[0])/2, rad, rad);
     ell_4m_post_mul_f(matA, matB);
@@ -197,7 +162,7 @@ main(int argc, char *argv[]) {
     
     
     ri = limnObjCylinderAdd(obj, 1, 1, res);
-    r = obj->r + ri; ELL_4V_SET(r->rgba, 0, 0, 0, 1);
+    r = obj->r + ri;
     ELL_4M_IDENTITY_SET(matA);
     ELL_4M_SCALE_SET(matB, rad, (1-sc[1])/2, rad);
     ell_4m_post_mul_f(matA, matB);
@@ -206,7 +171,7 @@ main(int argc, char *argv[]) {
     limnObjPartTransform(obj, ri, matA);
     
     ri = limnObjCylinderAdd(obj, 1, 1, res);
-    r = obj->r + ri; ELL_4V_SET(r->rgba, 0, 0, 0, 1);
+    r = obj->r + ri;
     ELL_4M_IDENTITY_SET(matA);
     ELL_4M_SCALE_SET(matB, rad, (1-sc[1])/2, rad);
     ell_4m_post_mul_f(matA, matB);
@@ -214,9 +179,8 @@ main(int argc, char *argv[]) {
     ell_4m_post_mul_f(matA, matB);
     limnObjPartTransform(obj, ri, matA);
     
-    
     ri = limnObjCylinderAdd(obj, 1, 2, res);
-    r = obj->r + ri; ELL_4V_SET(r->rgba, 0, 0, 0, 1);
+    r = obj->r + ri;
     ELL_4M_IDENTITY_SET(matA);
     ELL_4M_SCALE_SET(matB, rad, rad, (1-sc[2])/2);
     ell_4m_post_mul_f(matA, matB);
@@ -225,7 +189,7 @@ main(int argc, char *argv[]) {
     limnObjPartTransform(obj, ri, matA);
     
     ri = limnObjCylinderAdd(obj, 1, 2, res);
-    r = obj->r + ri; ELL_4V_SET(r->rgba, 0, 0, 0, 1);
+    r = obj->r + ri;
     ELL_4M_IDENTITY_SET(matA);
     ELL_4M_SCALE_SET(matB, rad, rad, (1-sc[2])/2);
     ell_4m_post_mul_f(matA, matB);
@@ -234,20 +198,14 @@ main(int argc, char *argv[]) {
     limnObjPartTransform(obj, ri, matA);
   }
 
-  win = limnWinNew(limnDevicePS);
-  ELL_5V_COPY(win->ps.edgeWidth, edgeWidth);
-  win->scale = 200;
+  file = airFopen(outS, stdout, "w");
+  airMopAdd(mop, file, (airMopper)airFclose, airMopAlways);
 
-  win->file = fopen(outS, "w");
-  airMopAdd(mop, win, (airMopper)limnWinNix, airMopAlways);
-
-  if (limnObjRender(obj, cam, win)
-      || limnObjPSDraw(obj, cam, nmap, win)) {
+  if (limnObjOFFWrite(file, obj)) {
     airMopAdd(mop, err = biffGetDone(LIMN), airFree, airMopAlways);
     fprintf(stderr, "%s: trouble:\n%s\n", me, err);
     airMopError(mop); return 1;
   }
-  fclose(win->file);
   
   airMopOkay(mop);
   return 0;
