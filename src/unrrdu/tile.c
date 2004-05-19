f54f2402e29f62f20717f52a0c54d08aec4e21df
bigler <bigler@3d70eeeb-363e-0410-a505-8a46323a89f2>
2004-05-19 19:14:37 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@2214 3d70eeeb-363e-0410-a505-8a46323a89f2

---
+++ src/unrrdu/tile.c
@@ -0,0 +1,69 @@
+/*
+  teem: Gordon Kindlmann's research software
+  Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998 University of Utah
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "unrrdu.h"
+#include "privateUnrrdu.h"
+
+#define INFO "Used to tile one axis agaist two others"
+char *_unrrdu_tileInfoL =
+(INFO
+ ". To tile the data set you split one axis, permute the pieces "
+ "against two others.  The axes are then merged.  The net result "
+ "is a nrrd with one fewer dimension with a tiled look.");
+
+int
+unrrdu_tileMain(int argc, char **argv, char *me, hestParm *hparm) {
+  hestOpt *opt = NULL;
+  char *out, *err;
+  Nrrd *nin, *nout;
+  int size[2], axes[3], pret;
+  airArray *mop;
+
+  hestOptAdd(&opt, "s", "fast, slow sizes", airTypeInt, 2, 2, size, NULL,
+	     "fast and slow axis sizes to produce as result of splitting "
+	     "given axis.");
+  hestOptAdd(&opt, "a", "ax0 ax1 axSplit", airTypeInt, 3, 3, axes, NULL,
+	     "axSplit is divided and merged with ax0 and ax1");
+  OPT_ADD_NIN(nin, "input nrrd");
+  OPT_ADD_NOUT(out, "output nrrd");
+
+  mop = airMopNew();
+  airMopAdd(mop, opt, (airMopper)hestOptFree, airMopAlways);
+
+  USAGE(_unrrdu_tileInfoL);
+  PARSE();
+  airMopAdd(mop, opt, (airMopper)hestParseFree, airMopAlways);
+
+  nout = nrrdNew();
+  airMopAdd(mop, nout, (airMopper)nrrdNuke, airMopAlways);
+  
+  if (nrrdTile(nout, nin, axes[0], axes[1], axes[2], size[0], size[1])) {
+    airMopAdd(mop, err = biffGetDone(NRRD), airFree, airMopAlways);
+    fprintf(stderr, "%s: error tiling nrrd:\n%s", me, err);
+    airMopError(mop);
+    return 1;
+  }
+
+  SAVE(out, nout, NULL);
+
+  airMopOkay(mop);
+  return 0;
+}
+
+UNRRDU_CMD(tile, INFO);
