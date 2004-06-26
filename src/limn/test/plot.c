4965cf51dfdfc1882ab1681cabb81acfc8345d76
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2004-06-26 08:29:41 +0000

    great, great, more cryptic command-line options
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@2262 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/limn/test/plot.c
+++ src/limn/test/plot.c
@@ -31,12 +31,17 @@ typedef struct {
 } plotPS;
 
 typedef struct {
-  double axisThick;
   /* these are allocated per-graph BY HEST */
   double *graphThick, *graphGray, *dotDiameter, *dotGray;
+
   /* also hest-allocated */
   char *axisHorzLabel, *axisVertLabel;
+  double *horzTick, *vertTick;
   
+  int numHorzTick, numVertTick;
+  int labelHorzTick, labelVertTick;
+  double axisThick, tickThick, tickLabelSize, tickLength;
+  double horzTickLabelOffset, vertTickLabelOffset;
   double dotInnerDiameterFraction;
   double axisOrig[2];
   double dbox[4];
@@ -100,43 +105,105 @@ plotGray(plotPS *pps, plotParm *pparm, double gray) {
 
 void
 plotLine(plotPS *pps, plotParm *pparm,
-	 double width, double x0, double y0, double x1, double y1) {
+	 double x0, double y0, double x1, double y1) {
   
-  fprintf(pps->file, "%g W\n", pps->psc*width);
   fprintf(pps->file, "%g %g M\n", PPS_X(x0), PPS_Y(y0));
   fprintf(pps->file, "%g %g L S\n", PPS_X(x1), PPS_Y(y1));
   return;
 }
 
 void
-plotLabel(plotPS *pps, plotParm *pparm,
+plotLabel(plotPS *pps, plotParm *pparm, int centered, 
 	  double x, double y, char *str) {
   
-  fprintf(pps->file, "%g %g M (%s) show\n", PPS_X(x), PPS_Y(y), str);
+  fprintf(pps->file, "gsave\n");
+  plotWidth(pps, pparm, 0);
+  if (centered) {
+    fprintf(pps->file,  
+	    "0 0 M (%s) false charpath pathbbox\n"
+	    "exch 4 1 roll sub 2 div 3 1 roll sub -2 div\n"  /* don't ask */
+	    "newpath %g add exch %g add M (%s) show\n",
+	    str, PPS_X(x), PPS_Y(y), str);
+  } else {
+    fprintf(pps->file,  "%g %g M (%s) show\n", PPS_X(x), PPS_Y(y), str);
+  }
+  fprintf(pps->file, "grestore\n");
   return;
 }
 
 void
 plotAxes(plotPS *pps, plotParm *pparm, Nrrd *ndata) {
-  double axX, axY;
+  double axX, axY, xx, yy, toff;
+  char buff[AIR_STRLEN_SMALL];
+  int ti;
   
-  axX = AIR_AFFINE(ndata->axis[1].min, pparm->axisOrig[0], ndata->axis[1].max,
+  axX = AIR_AFFINE(pparm->dbox[0], pparm->axisOrig[0], pparm->dbox[2],
 		   pps->bbox[0], pps->bbox[2]);
   axY = AIR_AFFINE(pparm->dbox[1], pparm->axisOrig[1], pparm->dbox[3],
 		   pps->bbox[1], pps->bbox[3]);
   plotGray(pps, pparm, 0);
-  plotLine(pps, pparm, pparm->axisThick,
+  plotWidth(pps, pparm, pparm->axisThick);
+  plotLine(pps, pparm,
 	   axX, pps->bbox[1], axX, pps->bbox[3]);
-  plotLine(pps, pparm, pparm->axisThick,
+  plotLine(pps, pparm,
 	   pps->bbox[0], axY, pps->bbox[2], axY);
 
   if (strlen(pparm->axisHorzLabel) || strlen(pparm->axisVertLabel)) {
     fprintf(pps->file, "/Helvetica findfont 20 scalefont setfont\n");
     if (strlen(pparm->axisHorzLabel)) {
-      plotLabel(pps, pparm, pps->bbox[2], axY, pparm->axisHorzLabel);
+      plotLabel(pps, pparm, AIR_FALSE, 
+		pps->bbox[2], axY, pparm->axisHorzLabel);
     }
     if (strlen(pparm->axisVertLabel)) {
-      plotLabel(pps, pparm, axX, pps->bbox[3], pparm->axisVertLabel);
+      plotLabel(pps, pparm, AIR_FALSE, 
+		axX, pps->bbox[3], pparm->axisVertLabel);
+    }
+  }
+
+  if (pparm->numHorzTick) {
+    if (pparm->tickThick > 0) {
+      toff = pparm->tickLength/2;
+      plotGray(pps, pparm, 0);
+      plotWidth(pps, pparm, pparm->tickThick);
+      for (ti=0; ti<pparm->numHorzTick; ti++) {
+	xx = AIR_AFFINE(pparm->dbox[0], pparm->horzTick[ti], pparm->dbox[2],
+			pps->bbox[0], pps->bbox[2]);
+	plotLine(pps, pparm, xx, axY - toff, xx, axY + toff);
+      }
+    }
+    if (pparm->tickLabelSize) {
+      fprintf(pps->file, "/Helvetica findfont %g scalefont setfont\n",
+	      pparm->tickLabelSize);
+      for (ti=0; ti<pparm->numHorzTick; ti++) {
+	xx = AIR_AFFINE(pparm->dbox[0], pparm->horzTick[ti], pparm->dbox[2],
+			pps->bbox[0], pps->bbox[2]);
+	yy = axY + pparm->horzTickLabelOffset;
+	sprintf(buff, "%g", pparm->horzTick[ti]);
+	plotLabel(pps, pparm, AIR_TRUE, xx, yy, buff);
+      }
+    }
+  }
+  if (pparm->numVertTick) {
+    if (pparm->tickThick > 0) {
+      toff = pparm->tickLength/2;
+      plotGray(pps, pparm, 0);
+      plotWidth(pps, pparm, pparm->tickThick);
+      for (ti=0; ti<pparm->numVertTick; ti++) {
+	yy = AIR_AFFINE(pparm->dbox[1], pparm->vertTick[ti], pparm->dbox[3],
+			pps->bbox[1], pps->bbox[3]);
+	plotLine(pps, pparm, axX - toff, yy, axX + toff, yy);
+      }
+    }
+    if (pparm->tickLabelSize) {
+      fprintf(pps->file, "/Helvetica findfont %g scalefont setfont\n",
+	      pparm->tickLabelSize);
+      for (ti=0; ti<pparm->numVertTick; ti++) {
+	yy = AIR_AFFINE(pparm->dbox[1], pparm->vertTick[ti], pparm->dbox[3],
+			pps->bbox[1], pps->bbox[3]);
+	xx = axX + pparm->vertTickLabelOffset;
+	sprintf(buff, "%g", pparm->vertTick[ti]);
+	plotLabel(pps, pparm, AIR_TRUE, xx, yy, buff);
+      }
     }
   }
 }
@@ -178,6 +245,7 @@ plotDots(plotPS *pps, plotParm *pparm, Nrrd **ndata, int nidx) {
   }
 
   fprintf(pps->file, "gsave\n");
+  fprintf(pps->file, "newpath\n");
   plotWidth(pps, pparm, 0);
   data = (double *)(ndata[nidx]->data);
   npts = ndata[nidx]->axis[1].size;
@@ -230,36 +298,73 @@ main(int argc, char *argv[]) {
   hestOptAdd(&hopt, "i", "data", airTypeOther, 1, -1, &_ndata, NULL,
 	     "input nrrd containing data to plot",
 	     &numNrrd, NULL, nrrdHestNrrd);
-  hestOptAdd(&hopt, "dbox", "minX minY maxX maxY", airTypeDouble, 4, 4, pparm.dbox, NULL,
+  hestOptAdd(&hopt, "dbox", "minX minY maxX maxY", airTypeDouble,
+	     4, 4, pparm.dbox, NULL,
 	     "bounding box, in data space");
   
-  hestOptAdd(&hopt, "bbox", "minX minY maxX maxY", airTypeDouble, 4, 4, pps.bbox, NULL,
+  hestOptAdd(&hopt, "bbox", "minX minY maxX maxY", airTypeDouble,
+	     4, 4, pps.bbox, NULL,
 	     "bounding box, in graph space");
   hestOptAdd(&hopt, "psc", "PS scale", airTypeDouble, 1, 1, &(pps.psc), "300",
 	     "scaling from graph space to PostScript points");
-
-  hestOptAdd(&hopt, "grth", "graph thickness", airTypeDouble, 1, -1, &(pparm.graphThick), "0.01",
-	     "thickness of line for graph, or \"0\" for no graph line", &numGrth);
-  hestOptAdd(&hopt, "grgr", "graph gray", airTypeDouble, 1, -1, &(pparm.graphGray), "0",
+  
+  hestOptAdd(&hopt, "grth", "graph thickness", airTypeDouble,
+	     1, -1, &(pparm.graphThick), "0.01",
+	     "thickness of line for graph, or \"0\" for no graph line", 
+	     &numGrth);
+  hestOptAdd(&hopt, "grgr", "graph gray", airTypeDouble,
+	     1, -1, &(pparm.graphGray), "0",
 	     "grayscale to use for graph", &numGrgr);
-  hestOptAdd(&hopt, "dtdi", "dot diameter", airTypeDouble, 1, -1, &(pparm.dotDiameter), "0.1",
-	     "radius of dot drawn at data points, or \"0\" for no dots", &numDtdi);
-  hestOptAdd(&hopt, "dtgr", "dot gray", airTypeDouble, 1, -1, &(pparm.dotGray), "0",
+  hestOptAdd(&hopt, "dtdi", "dot diameter", airTypeDouble,
+	     1, -1, &(pparm.dotDiameter), "0.1",
+	     "radius of dot drawn at data points, or \"0\" for no dots",
+	     &numDtdi);
+  hestOptAdd(&hopt, "dtgr", "dot gray", airTypeDouble,
+	     1, -1, &(pparm.dotGray), "0",
 	     "grayscale to use for dots", &numDtgr);
-  hestOptAdd(&hopt, "dtid", "dot inner diam frac", airTypeDouble, 1, 1,
-	     &(pparm.dotInnerDiameterFraction), "0.0",
+  hestOptAdd(&hopt, "dtid", "dot inner diam frac", airTypeDouble,
+	     1, 1, &(pparm.dotInnerDiameterFraction), "0.0",
 	     "fractional radius of white dot drawn within dot");
 
-  hestOptAdd(&hopt, "axth", "axis thickness", airTypeDouble, 1, 1, &(pparm.axisThick), "0.01",
+  hestOptAdd(&hopt, "tihz", "pos", airTypeDouble,
+	     0, -1, &(pparm.horzTick), "",
+	     "locations for tickmarks on horizontal axis",
+	     &(pparm.numHorzTick));
+  hestOptAdd(&hopt, "tivt", "pos", airTypeDouble,
+	     0, -1, &(pparm.vertTick), "",
+	     "locations for tickmarks on vertical axis",
+	     &(pparm.numVertTick));
+  hestOptAdd(&hopt, "tiho", "offset", airTypeDouble,
+	     1, 1, &(pparm.horzTickLabelOffset), "0",
+	     "horizontal tick label offset");
+  hestOptAdd(&hopt, "tivo", "offset", airTypeDouble,
+	     1, 1, &(pparm.vertTickLabelOffset), "0",
+	     "vertical tick label offset");
+  hestOptAdd(&hopt, "tils", "size", airTypeDouble,
+	     1, 1, &(pparm.tickLabelSize), "0",
+	     "font size for labels on tick marks, or \"0\" for no labels");
+  hestOptAdd(&hopt, "tith", "tick thickness", airTypeDouble,
+	     1, 1, &(pparm.tickThick), "0.01",
+	     "thickness of lines for tick marks");
+  hestOptAdd(&hopt, "tiln", "tick length", airTypeDouble,
+	     1, 1, &(pparm.tickLength), "0.08",
+	     "length of lines for tick marks");
+
+  hestOptAdd(&hopt, "axth", "axis thickness", airTypeDouble,
+	     1, 1, &(pparm.axisThick), "0.01",
 	     "thickness of lines for axes");
-  hestOptAdd(&hopt, "axor", "axis origin", airTypeDouble, 2, 2, &(pparm.axisOrig), "0 0",
+  hestOptAdd(&hopt, "axor", "axis origin", airTypeDouble,
+	     2, 2, &(pparm.axisOrig), "0 0",
 	     "origin of lines for axes, in data space");
-  hestOptAdd(&hopt, "axhl", "horiz axis label", airTypeString, 1, 1, &(pparm.axisHorzLabel), "",
+  hestOptAdd(&hopt, "axhl", "horiz axis label", airTypeString,
+	     1, 1, &(pparm.axisHorzLabel), "",
 	     "label on horizontal axis");
-  hestOptAdd(&hopt, "axvl", "vert axis label", airTypeString, 1, 1, &(pparm.axisVertLabel), "",
+  hestOptAdd(&hopt, "axvl", "vert axis label", airTypeString,
+	     1, 1, &(pparm.axisVertLabel), "",
 	     "label on vertical axis");
 
-  hestOptAdd(&hopt, "o", "output PS", airTypeString, 1, 1, &outS, "out.ps",
+  hestOptAdd(&hopt, "o", "output PS", airTypeString,
+	     1, 1, &outS, "out.ps",
 	     "output file to render postscript into");
   hestParseOrDie(hopt, argc-1, argv+1, NULL,
 		 me, info, AIR_TRUE, AIR_TRUE, AIR_TRUE);
@@ -284,7 +389,8 @@ main(int argc, char *argv[]) {
   for (ni=0; ni<numNrrd; ni++) {
     if (!( (1 == _ndata[ni]->dim || 2 == _ndata[ni]->dim) 
 	   && nrrdTypeBlock != _ndata[ni]->type )) {
-      fprintf(stderr, "%s: input nrrd must be 1-D or 2-D array of scalars", me);
+      fprintf(stderr, "%s: input nrrd must be 1-D or 2-D array of scalars",
+	      me);
       airMopError(mop); return 1;
     }
   }
