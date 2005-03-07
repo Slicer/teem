eeb4f1934884c661f984d8484c567d2948521f3e
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2005-03-07 19:53:25 +0000

    encodings changed API
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@2532 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/limn/test/triimg.c
+++ src/limn/test/triimg.c
@@ -124,8 +124,9 @@ main(int argc, char *argv[]) {
   fprintf(file, "[%d 0 0 -%d 0 %d]\n", sx, sy, sy);
   fprintf(file, "{currentfile linestr readhexstring pop} %s\n",
           gray ? "image" : "false 3 colorimage");
-  nio->dataFile = file;
-  nrrdEncodingHex->write(nin, nio);
+  nrrdEncodingHex->write(file, nin->data, nrrdElementNumber(nin),
+                         nin, nio);
+
   nio->dataFile = NULL;
   fprintf(file, "\n");
 
