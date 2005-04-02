b5e02c0ac946d63bfa155eef6fb865e3c3e1885a
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2005-04-02 16:13:32 +0000

    now, like tend glyph, tend ellipse can take a separate array of sample positions
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@2555 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/ten/tendEllipse.c
+++ src/ten/tendEllipse.c
@@ -27,34 +27,40 @@ char *_tend_ellipseInfoL =
    ".  Not much to look at here.");
 
 int
-tend_ellipseDoit(FILE *file, Nrrd *nten,
+tend_ellipseDoit(FILE *file, Nrrd *nten, Nrrd *npos,
+                 float min[2], float max[2],
                  float gscale, float cthresh, int invert) {
-  int sx, sy, x, y;
-  float aspect, scale, minX, minY, maxX, maxY, px, py, 
-    conf, Dxx, Dxy, Dyy, (*lup)(const void*, size_t);
-
-  lup = nrrdFLookup[nten->type];
-  sx = nten->axis[1].size;
-  sy = nten->axis[2].size;
-  aspect = sx/sy;
+  int sx=0, sy=0, x, y, nt, ti;
+  float aspect, minX, minY, maxX, maxY, px, py, 
+    conf, Dxx, Dxy, Dyy, *tdata, *pdata;
+  
+  tdata = (float*)nten->data;
+  if (npos) {
+    pdata = (float*)npos->data;
+    nt = npos->axis[1].size;
+    aspect = (max[0] - min[0])/(max[1] - min[1]);
+  } else {
+    pdata = NULL;
+    sx = nten->axis[1].size;
+    sy = nten->axis[2].size;
+    nt = sx*sy;
+    aspect = (float)sx/sy;
+  }
   if (aspect > 7.5/10) {
     /* image has a wider aspect ratio than safely printable page area */
     minX = 0.5;
     maxX = 8.0;
-    minY = 5.50 - 7.5*sy/sx/2;
-    maxY = 5.50 + 7.5*sy/sx/2;
-    scale = 7.5/sx;
+    minY = 5.50 - 7.5/2/aspect;
+    maxY = 5.50 + 7.5/2/aspect;
   } else {
     /* image is taller ... */
-    minX = 4.25 - 10.0*sx/sy/2;
-    maxX = 4.25 + 10.0*sx/sy/2;
+    minX = 4.25 - 10.0/2*aspect;
+    maxX = 4.25 + 10.0/2*aspect;
     minY = 0.5;
     maxY = 10.5;
-    scale = 10.0/sy;
   }
   minX *= 72; minY *= 72;
   maxX *= 72; maxY *= 72;
-  scale *= 72;
 
   fprintf(file, "%%!PS-Adobe-3.0 EPSF-3.0\n");
   fprintf(file, "%%%%Creator: tend ellipse\n");
@@ -79,25 +85,32 @@ tend_ellipseDoit(FILE *file, Nrrd *nten,
     fprintf(file, "closepath fill\n");
     fprintf(file, "1 setgray\n");
   }
-  for (y=0; y<sy; y++) {
-    py = NRRD_CELL_POS(minY, maxY, sy, sy-1-y);
-    for (x=0; x<sx; x++) {
-      conf = lup(nten->data, 0 + 4*(x + sx*y));
-      if (conf > cthresh) {
-        Dxx = lup(nten->data, 1 + 4*(x + sx*y));
-        Dxy = lup(nten->data, 2 + 4*(x + sx*y));
-        Dyy = lup(nten->data, 3 + 4*(x + sx*y));
-        px = NRRD_CELL_POS(minX, maxX, sx, x);
-        fprintf(file, "gsave\n");
-        fprintf(file, "matrix currentmatrix\n");
-        fprintf(file, "[%g %g %g %g %g %g] concat\n",
-                Dxx, -Dxy, -Dxy, Dyy, px, py);
-        fprintf(file, "0 0 %g 0 360 arc closepath\n", gscale);
-        fprintf(file, "setmatrix\n");
-        fprintf(file, "fill\n");
-        fprintf(file, "grestore\n");
-      }
+  for (ti=0; ti<nt; ti++) {
+    if (npos) {
+      px = AIR_AFFINE(min[0], pdata[0], max[0], minX, maxX);
+      py = AIR_AFFINE(min[1], pdata[1], max[1], maxY, minY);
+      pdata += 2;
+    } else {
+      x = ti % sx;
+      y = ti / sx;
+      px = NRRD_CELL_POS(minX, maxX, sx, x);
+      py = NRRD_CELL_POS(minY, maxY, sy, sy-1-y);
     }
+    conf = tdata[0];
+    if (conf > cthresh) {
+      Dxx = tdata[1];
+      Dxy = tdata[2];
+      Dyy = tdata[3];
+      fprintf(file, "gsave\n");
+      fprintf(file, "matrix currentmatrix\n");
+      fprintf(file, "[%g %g %g %g %g %g] concat\n",
+              Dxx, -Dxy, -Dxy, Dyy, px, py);
+      fprintf(file, "0 0 %g 0 360 arc closepath\n", gscale);
+      fprintf(file, "setmatrix\n");
+      fprintf(file, "fill\n");
+      fprintf(file, "grestore\n");
+    }
+    tdata += 4;
   }
   fprintf(file, "grestore\n");
   
@@ -111,9 +124,9 @@ tend_ellipseMain(int argc, char **argv, char *me, hestParm *hparm) {
   char *perr;
   airArray *mop;
 
-  Nrrd *nten;
+  Nrrd *nten, *npos;
   char *outS;
-  float gscale, cthresh;
+  float gscale, cthresh, min[2], max[2];
   FILE *fout;
   int invert;
 
@@ -122,14 +135,22 @@ tend_ellipseMain(int argc, char **argv, char *me, hestParm *hparm) {
   hestOptAdd(&hopt, "ctr", "conf thresh", airTypeFloat, 1, 1, &cthresh, "0.5",
              "Glyphs will be drawn only for tensors with confidence "
              "values greater than this threshold");
-  hestOptAdd(&hopt, "gsc", "scale", airTypeFloat, 1, 1, &gscale,
-             "1", "over-all glyph size");
+  hestOptAdd(&hopt, "gsc", "scale", airTypeFloat, 1, 1, &gscale, "1",
+             "over-all glyph size");
   hestOptAdd(&hopt, "inv", NULL, airTypeInt, 0, 0, &invert, NULL,
              "use white ellipses on black background, instead of reverse");
+  hestOptAdd(&hopt, "min", "minX minY", airTypeFloat, 2, 2, min, "-1 -1",
+             "when using \"-p\", minimum corner");
+  hestOptAdd(&hopt, "max", "maxX maxY", airTypeFloat, 2, 2, max, "1 1",
+             "when using \"-p\", maximum corner");
 
   /* input/output */
   hestOptAdd(&hopt, "i", "nin", airTypeOther, 1, 1, &nten, "-",
              "image of 2D tensors", NULL, NULL, nrrdHestNrrd);
+  hestOptAdd(&hopt, "p", "pos array", airTypeOther, 1, 1, &npos, "",
+             "Instead of being on a grid, tensors are at arbitrary locations, "
+             "as defined by this 2-by-N array of floats", NULL, NULL,
+             nrrdHestNrrd);
   hestOptAdd(&hopt, "o", "nout", airTypeString, 1, 1, &outS, "-",
              "output PostScript file");
 
@@ -138,8 +159,27 @@ tend_ellipseMain(int argc, char **argv, char *me, hestParm *hparm) {
   PARSE();
   airMopAdd(mop, hopt, (airMopper)hestParseFree, airMopAlways);
 
-  if (!(3 == nten->dim && 4 == nten->axis[0].size)) {
-    fprintf(stderr, "%s: didn't get a 3-D 4-by-X-by-Y 2D tensor array\n", me);
+  if (npos) {
+    if (!( 2 == nten->dim && 4 == nten->axis[0].size
+           && 2 == npos->dim && 2 == npos->axis[0].size
+           && nten->axis[1].size == npos->axis[1].size )) {
+      fprintf(stderr, "%s: didn't get matching lists of tensors and pos's\n",
+              me);
+      airMopError(mop); return 1;
+    }
+    if (!( nrrdTypeFloat == npos->type )) {
+      fprintf(stderr, "%s: didn't get float type positions\n", me);
+      airMopError(mop); return 1;
+    }
+  } else {
+    if (!(3 == nten->dim && 4 == nten->axis[0].size)) {
+      fprintf(stderr, "%s: didn't get a 3-D 4-by-X-by-Y 2D tensor array\n",
+              me);
+      airMopError(mop); return 1;
+    }
+  }
+  if (!( nrrdTypeFloat == nten->type )) {
+    fprintf(stderr, "%s: didn't get float type tensors\n", me);
     airMopError(mop); return 1;
   }
   if (!(fout = airFopen(outS, stdout, "wb"))) {
@@ -148,7 +188,7 @@ tend_ellipseMain(int argc, char **argv, char *me, hestParm *hparm) {
   }
   airMopAdd(mop, fout, (airMopper)airFclose, airMopAlways);
 
-  tend_ellipseDoit(fout, nten, gscale, cthresh, invert);
+  tend_ellipseDoit(fout, nten, npos, min, max, gscale, cthresh, invert);
 
   airMopOkay(mop);
   return 0;
