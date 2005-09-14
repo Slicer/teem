7adb34297cfeb59d86c6a6c565e98e985bb2a677
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2005-09-14 13:20:07 +0000

    changed preamble
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@2854 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/hest/test/ex0.c
+++ src/hest/test/ex0.c
@@ -1,5 +1,5 @@
 /*
-  teem: Gordon Kindlmann's research software
+  Teem: Tools to process and visualize scientific data and images
   Copyright (C) 2005  Gordon Kindlmann
   Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
 
@@ -7,6 +7,8 @@
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also
+  include exceptions to the LGPL that facilitate static linking.
 
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
