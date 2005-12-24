af60cc5e45ed6fc53d1d5c1ec03bcd8a4fd1ba05
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2005-12-24 17:40:42 +0000

    release date fix
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/branches/Teem-1-9-0@3203 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/air/miscAir.c
+++ src/air/miscAir.c
@@ -43,7 +43,7 @@
 const char *
 airTeemVersion = TEEM_VERSION_STRING;
 const char *
-airTeemReleaseDate = "16 December 2005";
+airTeemReleaseDate = "26 December 2005";
 
 double
 _airSanityHelper(double val) {
