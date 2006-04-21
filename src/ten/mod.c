e2390b665b45791bd0628e3b09a51a3cf8fc0059
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2006-04-21 17:50:46 +0000

    added tenExp and tenLog, and refactored all simple per-tensor modifications
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@3413 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/ten/mod.c
+++ src/ten/mod.c
@@ -24,74 +24,41 @@
 #include "ten.h"
 #include "privateTen.h"
 
-int
-tenSizeNormalize(Nrrd *nout, const Nrrd *nin, double _weight[3],
-                 double amount, double target) {
-  char me[]="tenSizeNormalize", err[BIFF_STRLEN];
-  float *tin, *tout, eval[3], evec[9], size, weight[3];
-  size_t N, I;
+typedef struct {
+  double weight[3], amount, target;            /* tenSizeNormalize */
+  /* amount: tenSizeScale */
+  double scale; int fixDet; int makePositive;  /* tenAnisoScale */
+  double min, max;                             /* tenEigenvalueClamp */
+  double expo;                                 /* tenEigenvaluePower */
+  double val;                                  /* tenEigenvalueAdd */
+} funcParm;
 
-  if (!(nout && nin)) {
-    sprintf(err, "%s: got NULL pointer", me);
+enum {
+  funcUnknown,
+  funcSizeNormalize,
+  funcSizeScale,
+  funcAnisoScale,
+  funcEigenvalueClamp,
+  funcEigenvaluePower,
+  funcEigenvalueAdd,
+  funcLog,
+  funcExp,
+  funcLast
+};
+
+static int
+theFunc(Nrrd *nout, const Nrrd *nin, int func, funcParm *parm) {
+  char me[]="theFunc", err[BIFF_STRLEN];
+  float *tin, *tout, eval[3], evec[9], weight[3], size, mean;
+  size_t NN, II;
+  unsigned int ri;
+
+  if (!AIR_IN_OP(funcUnknown, func, funcLast)) {
+    sprintf(err, "%s: given func %d out of range [%d,%d]", me, func,
+            funcUnknown+1, funcLast-1);
     biffAdd(TEN, err); return 1;
   }
-  if (tenTensorCheck(nin, nrrdTypeFloat, AIR_FALSE, AIR_TRUE)) {
-    sprintf(err, "%s: didn't get a tensor nrrd", me);
-    biffAdd(TEN, err); return 1;
-  }
-  if (nrrdCopy(nout, nin)) {
-    sprintf(err, "%s: couldn't allocate output", me);
-    biffMove(TEN, err, NRRD); return 1;
-  }
-
-  ELL_3V_COPY_TT(weight, float, _weight);
-  size = weight[0] + weight[1] + weight[2];
-  if (!size) {
-    sprintf(err, "%s: some of eigenvalue weights is zero", me);
-    biffAdd(TEN, err); return 1;
-  }
-  weight[0] /= size;
-  weight[1] /= size;
-  weight[2] /= size;
-  /*
-  fprintf(stderr, "!%s: real weight = %g %g %g; amount = %g\n",
-          me, weight[0], weight[1], weight[2], amount);
-  */
-  tin = (float*)(nin->data);
-  tout = (float*)(nout->data);
-  N = nrrdElementNumber(nin)/7;
-  for (I=0; I<=N-1; I++) {
-    tenEigensolve_f(eval, evec, tin);
-    size = (weight[0]*AIR_ABS(eval[0])
-            + weight[1]*AIR_ABS(eval[1])
-            + weight[2]*AIR_ABS(eval[2]));
-    /*
-    fprintf(stderr, "!%s: eval = %g %g %g --> size = %g --> ",
-            me, eval[0], eval[1], eval[2], size);
-    */
-    ELL_3V_SET_TT(eval, float,
-                  AIR_AFFINE(0, amount, 1, eval[0], target*eval[0]/size),
-                  AIR_AFFINE(0, amount, 1, eval[1], target*eval[1]/size),
-                  AIR_AFFINE(0, amount, 1, eval[2], target*eval[2]/size));
-    /*
-    fprintf(stderr, "%g %g %g\n", eval[0], eval[1], eval[2]);
-    */
-    tenMakeOne_f(tout, tin[0], eval, evec);
-    tin += 7;
-    tout += 7;
-  }
-  /* basic and per-axis info handled by nrrdCopy above */
-  
-  return 0;
-}
-
-int
-tenSizeScale(Nrrd *nout, const Nrrd *nin, double amount) {
-  char me[]="tenSizeScale", err[BIFF_STRLEN];
-  size_t I, N;
-  float *tin, *tout;
-
-  if (!(nout && nin)) {
+  if (!(nout && nin && parm)) {
     sprintf(err, "%s: got NULL pointer", me);
     biffAdd(TEN, err); return 1;
   }
@@ -105,20 +72,180 @@ tenSizeScale(Nrrd *nout, const Nrrd *nin, double amount) {
       biffMove(TEN, err, NRRD); return 1;
     }
   }
-  tin = (float *)(nin->data);
-  tout = (float *)(nout->data);
-  N = nrrdElementNumber(nin)/7;
-  for (I=0; I<N; I++) {
-    TEN_T_SET_TT(tout, float,
-                 tin[0],
-                 amount*tin[1], amount*tin[2], amount*tin[3],
-                 amount*tin[4], amount*tin[5],
-                 amount*tin[6]);
-    tin += 7;
-    tout += 7;
-  }
-  /* basic and per-axis info handled by nrrdCopy above */
 
+  tin = (float*)(nin->data);
+  tout = (float*)(nout->data);
+  NN = nrrdElementNumber(nin)/7;
+  switch(func) {
+  case funcSizeNormalize:
+    ELL_3V_COPY_TT(weight, float, parm->weight);
+    size = weight[0] + weight[1] + weight[2];
+    if (!size) {
+      sprintf(err, "%s: some of eigenvalue weights is zero", me);
+      biffAdd(TEN, err); return 1;
+    }
+    weight[0] /= size;
+    weight[1] /= size;
+    weight[2] /= size;
+    for (II=0; II<=NN-1; II++) {
+      tenEigensolve_f(eval, evec, tin);
+      size = (weight[0]*AIR_ABS(eval[0])
+              + weight[1]*AIR_ABS(eval[1])
+              + weight[2]*AIR_ABS(eval[2]));
+      ELL_3V_SET_TT(eval, float,
+                    AIR_AFFINE(0, parm->amount, 1,
+                               eval[0], parm->target*eval[0]/size),
+                    AIR_AFFINE(0, parm->amount, 1,
+                               eval[1], parm->target*eval[1]/size),
+                    AIR_AFFINE(0, parm->amount, 1,
+                               eval[2], parm->target*eval[2]/size));
+      tenMakeOne_f(tout, tin[0], eval, evec);
+      tin += 7;
+      tout += 7;
+    }
+    break;
+  case funcSizeScale:
+    for (II=0; II<=NN-1; II++) {
+      TEN_T_SET_TT(tout, float,
+                   tin[0],
+                   parm->amount*tin[1],
+                   parm->amount*tin[2],
+                   parm->amount*tin[3],
+                   parm->amount*tin[4],
+                   parm->amount*tin[5],
+                   parm->amount*tin[6]);
+      tin += 7;
+      tout += 7;
+    }
+    break;
+  case funcAnisoScale:
+    for (II=0; II<=NN-1; II++) {
+      tenEigensolve_f(eval, evec, tin);
+      if (parm->fixDet) {
+        eval[0] = AIR_MAX(eval[0], 0.00001f);
+        eval[1] = AIR_MAX(eval[1], 0.00001f);
+        eval[2] = AIR_MAX(eval[2], 0.00001f);
+        ELL_3V_SET_TT(eval, float, log(eval[0]), log(eval[1]), log(eval[2]));
+      }
+      mean = (eval[0] + eval[1] + eval[2])/3.0f;
+      ELL_3V_SET_TT(eval, float,
+                    AIR_LERP(parm->scale, mean, eval[0]),
+                    AIR_LERP(parm->scale, mean, eval[1]),
+                    AIR_LERP(parm->scale, mean, eval[2]));
+      if (parm->fixDet) {
+        ELL_3V_SET_TT(eval, float, exp(eval[0]), exp(eval[1]), exp(eval[2]));
+      }
+      if (eval[2] < 0 && parm->makePositive) {
+        eval[0] = AIR_MAX(eval[0], 0.0f);
+        eval[1] = AIR_MAX(eval[1], 0.0f);
+        eval[2] = AIR_MAX(eval[2], 0.0f);
+      }
+      tenMakeOne_f(tout, tin[0], eval, evec);
+      tin += 7;
+      tout += 7;
+    }
+    break;
+  case funcEigenvalueClamp:
+    for (II=0; II<=NN-1; II++) {
+      tenEigensolve_f(eval, evec, tin);
+      if (AIR_EXISTS(parm->min)) {
+        ELL_3V_SET_TT(eval, float,
+                      AIR_MAX(eval[0], parm->min),
+                      AIR_MAX(eval[1], parm->min),
+                      AIR_MAX(eval[2], parm->min));
+      }
+      if (AIR_EXISTS(parm->max)) {
+        ELL_3V_SET_TT(eval, float,
+                      AIR_MIN(eval[0], parm->max),
+                      AIR_MIN(eval[1], parm->max),
+                      AIR_MIN(eval[2], parm->max));
+      }
+      tenMakeOne_f(tout, tin[0], eval, evec);
+      tin += 7;
+      tout += 7;
+    }
+    break;
+  case funcEigenvaluePower:
+    for (II=0; II<=NN-1; II++) {
+      tenEigensolve_f(eval, evec, tin);
+      ELL_3V_SET_TT(eval, float,
+                    pow(eval[0], parm->expo),
+                    pow(eval[1], parm->expo),
+                    pow(eval[2], parm->expo));
+      tenMakeOne_f(tout, tin[0], eval, evec);
+      tin += 7;
+      tout += 7;
+    }
+    break;
+  case funcEigenvalueAdd:
+    for (II=0; II<=NN-1; II++) {
+      /* HEY: this doesn't require eigensolve */
+      tenEigensolve_f(eval, evec, tin); 
+      ELL_3V_SET_TT(eval, float,
+                    eval[0] + parm->val,
+                    eval[1] + parm->val,
+                    eval[2] + parm->val);
+      tenMakeOne_f(tout, tin[0], eval, evec);
+      tin += 7;
+      tout += 7;
+    }
+    break;
+  case funcLog:
+    for (II=0; II<=NN-1; II++) {
+      tenEigensolve_f(eval, evec, tin);
+      for (ri=0; ri<3; ri++) {
+        eval[ri] = log(eval[ri]);
+        eval[ri] = AIR_EXISTS(eval[ri]) ? eval[ri] : -1000000;
+      }
+      tenMakeOne_f(tout, tin[0], eval, evec);
+      tin += 7;
+      tout += 7;
+    }
+    break;
+  case funcExp:
+    for (II=0; II<=NN-1; II++) {
+      tenEigensolve_f(eval, evec, tin);
+      for (ri=0; ri<3; ri++) {
+        eval[ri] = exp(eval[ri]);
+        eval[ri] = AIR_EXISTS(eval[ri]) ? eval[ri] : 0;
+      }
+      tenMakeOne_f(tout, tin[0], eval, evec);
+      tin += 7;
+      tout += 7;
+    }
+    break;
+  }
+  
+  /* basic and per-axis info handled by nrrdCopy above */
+  return 0;
+}
+
+int
+tenSizeNormalize(Nrrd *nout, const Nrrd *nin, double _weight[3],
+                 double amount, double target) {
+  char me[]="tenSizeNormalize", err[BIFF_STRLEN];
+  funcParm parm;
+
+  ELL_3V_COPY(parm.weight, _weight);
+  parm.amount = amount;
+  parm.target = target;
+  if (theFunc(nout, nin, funcSizeNormalize, &parm)) {
+    sprintf(err, "%s: trouble", me);
+    biffAdd(TEN, err); return 1;
+  }
+  return 0;
+}
+
+int
+tenSizeScale(Nrrd *nout, const Nrrd *nin, double amount) {
+  char me[]="tenSizeScale", err[BIFF_STRLEN];
+  funcParm parm;
+
+  parm.amount = amount;
+  if (theFunc(nout, nin, funcSizeScale, &parm)) {
+    sprintf(err, "%s: trouble", me);
+    biffAdd(TEN, err); return 1;
+  }
   return 0;
 }
 
@@ -132,53 +259,15 @@ int
 tenAnisoScale(Nrrd *nout, const Nrrd *nin, double scale,
               int fixDet, int makePositive) {
   char me[]="tenAnisoScale", err[BIFF_STRLEN];
-  size_t I, N;
-  float *tin, *tout, mean, eval[3], evec[9];
+  funcParm parm;
 
-  if (!(nout && nin)) {
-    sprintf(err, "%s: got NULL pointer", me);
+  parm.scale = scale;
+  parm.fixDet = fixDet;
+  parm.makePositive = makePositive;
+  if (theFunc(nout, nin, funcAnisoScale, &parm)) {
+    sprintf(err, "%s: trouble", me);
     biffAdd(TEN, err); return 1;
   }
-  if (tenTensorCheck(nin, nrrdTypeFloat, AIR_FALSE, AIR_TRUE)) {
-    sprintf(err, "%s: didn't get a tensor nrrd", me);
-    biffAdd(TEN, err); return 1;
-  }
-  if (nout != nin) {
-    if (nrrdCopy(nout, nin)) {
-      sprintf(err, "%s: couldn't allocate output", me);
-      biffMove(TEN, err, NRRD); return 1;
-    }
-  }
-  tin = (float *)(nin->data);
-  tout = (float *)(nout->data);
-  N = nrrdElementNumber(nin)/7;
-  for (I=0; I<N; I++) {
-    tenEigensolve_f(eval, evec, tin);
-    if (fixDet) {
-      eval[0] = AIR_MAX(eval[0], 0.00001f);
-      eval[1] = AIR_MAX(eval[1], 0.00001f);
-      eval[2] = AIR_MAX(eval[2], 0.00001f);
-      ELL_3V_SET_TT(eval, float, log(eval[0]), log(eval[1]), log(eval[2]));
-    }
-    mean = (eval[0] + eval[1] + eval[2])/3.0f;
-    ELL_3V_SET_TT(eval, float,
-                  AIR_LERP(scale, mean, eval[0]),
-                  AIR_LERP(scale, mean, eval[1]),
-                  AIR_LERP(scale, mean, eval[2]));
-    if (fixDet) {
-      ELL_3V_SET_TT(eval, float, exp(eval[0]), exp(eval[1]), exp(eval[2]));
-    }
-    if (eval[2] < 0 && makePositive) {
-      eval[0] = AIR_MAX(eval[0], 0.0f);
-      eval[1] = AIR_MAX(eval[1], 0.0f);
-      eval[2] = AIR_MAX(eval[2], 0.0f);
-    }
-    tenMakeOne_f(tout, tin[0], eval, evec);
-    tin += 7;
-    tout += 7;
-  }
-  /* basic and per-axis info handled by nrrdCopy above */
-
   return 0;
 }
 
@@ -190,46 +279,14 @@ tenAnisoScale(Nrrd *nout, const Nrrd *nin, double scale,
 int
 tenEigenvalueClamp(Nrrd *nout, const Nrrd *nin, double min, double max) {
   char me[]="tenEigenvalueClamp", err[BIFF_STRLEN];
-  size_t I, N;
-  float *tin, *tout, eval[3], evec[9];
+  funcParm parm;
 
-  if (!(nout && nin)) {
-    sprintf(err, "%s: got NULL pointer", me);
+  parm.min = min;
+  parm.max = max;
+  if (theFunc(nout, nin, funcEigenvalueClamp, &parm)) {
+    sprintf(err, "%s: trouble", me);
     biffAdd(TEN, err); return 1;
   }
-  if (tenTensorCheck(nin, nrrdTypeFloat, AIR_FALSE, AIR_TRUE)) {
-    sprintf(err, "%s: didn't get a tensor nrrd", me);
-    biffAdd(TEN, err); return 1;
-  }
-  if (nout != nin) {
-    if (nrrdCopy(nout, nin)) {
-      sprintf(err, "%s: couldn't allocate output", me);
-      biffMove(TEN, err, NRRD); return 1;
-    }
-  }
-  tin = (float *)(nin->data);
-  tout = (float *)(nout->data);
-  N = nrrdElementNumber(nin)/7;
-  for (I=0; I<N; I++) {
-    tenEigensolve_f(eval, evec, tin);
-    if (AIR_EXISTS(min)) {
-      ELL_3V_SET_TT(eval, float,
-                    AIR_MAX(eval[0], min),
-                    AIR_MAX(eval[1], min),
-                    AIR_MAX(eval[2], min));
-    }
-    if (AIR_EXISTS(max)) {
-      ELL_3V_SET_TT(eval, float,
-                    AIR_MIN(eval[0], max),
-                    AIR_MIN(eval[1], max),
-                    AIR_MIN(eval[2], max));
-    }
-    tenMakeOne_f(tout, tin[0], eval, evec);
-    tin += 7;
-    tout += 7;
-  }
-  /* basic and per-axis info handled by nrrdCopy above */
-
   return 0;
 }
 
@@ -241,38 +298,13 @@ tenEigenvalueClamp(Nrrd *nout, const Nrrd *nin, double min, double max) {
 int
 tenEigenvaluePower(Nrrd *nout, const Nrrd *nin, double expo) {
   char me[]="tenEigenvaluePower", err[BIFF_STRLEN];
-  size_t I, N;
-  float *tin, *tout, eval[3], evec[9];
+  funcParm parm;
 
-  if (!(nout && nin)) {
-    sprintf(err, "%s: got NULL pointer", me);
+  parm.expo = expo;
+  if (theFunc(nout, nin, funcEigenvaluePower, &parm)) {
+    sprintf(err, "%s: trouble", me);
     biffAdd(TEN, err); return 1;
   }
-  if (tenTensorCheck(nin, nrrdTypeFloat, AIR_FALSE, AIR_TRUE)) {
-    sprintf(err, "%s: didn't get a tensor nrrd", me);
-    biffAdd(TEN, err); return 1;
-  }
-  if (nout != nin) {
-    if (nrrdCopy(nout, nin)) {
-      sprintf(err, "%s: couldn't allocate output", me);
-      biffMove(TEN, err, NRRD); return 1;
-    }
-  }
-  tin = (float *)(nin->data);
-  tout = (float *)(nout->data);
-  N = nrrdElementNumber(nin)/7;
-  for (I=0; I<N; I++) {
-    tenEigensolve_f(eval, evec, tin);
-    ELL_3V_SET_TT(eval, float,
-                  pow(eval[0], expo),
-                  pow(eval[1], expo),
-                  pow(eval[2], expo));
-    tenMakeOne_f(tout, tin[0], eval, evec);
-    tin += 7;
-    tout += 7;
-  }
-  /* basic and per-axis info handled by nrrdCopy above */
-
   return 0;
 }
 
@@ -284,37 +316,46 @@ tenEigenvaluePower(Nrrd *nout, const Nrrd *nin, double expo) {
 int
 tenEigenvalueAdd(Nrrd *nout, const Nrrd *nin, double val) {
   char me[]="tenEigenvalueAdd", err[BIFF_STRLEN];
-  size_t I, N;
-  float *tin, *tout, eval[3], evec[9];
-
-  if (!(nout && nin)) {
-    sprintf(err, "%s: got NULL pointer", me);
+  funcParm parm;
+  
+  parm.val = val;
+  if (theFunc(nout, nin, funcEigenvalueAdd, &parm)) {
+    sprintf(err, "%s: trouble", me);
     biffAdd(TEN, err); return 1;
   }
-  if (tenTensorCheck(nin, nrrdTypeFloat, AIR_FALSE, AIR_TRUE)) {
-    sprintf(err, "%s: didn't get a tensor nrrd", me);
+  return 0;
+}
+
+/*
+******** tenLog
+**
+** takes the logarithm (by taking the log of the eigenvalues)
+*/
+int
+tenLog(Nrrd *nout, const Nrrd *nin) {
+  char me[]="tenLog", err[BIFF_STRLEN];
+  funcParm parm;
+
+  if (theFunc(nout, nin, funcLog, &parm)) {
+    sprintf(err, "%s: trouble", me);
     biffAdd(TEN, err); return 1;
   }
-  if (nout != nin) {
-    if (nrrdCopy(nout, nin)) {
-      sprintf(err, "%s: couldn't allocate output", me);
-      biffMove(TEN, err, NRRD); return 1;
-    }
-  }
-  tin = (float *)(nin->data);
-  tout = (float *)(nout->data);
-  N = nrrdElementNumber(nin)/7;
-  for (I=0; I<N; I++) {
-    tenEigensolve_f(eval, evec, tin);
-    ELL_3V_SET_TT(eval, float,
-                  eval[0] + val,
-                  eval[1] + val,
-                  eval[2] + val);
-    tenMakeOne_f(tout, tin[0], eval, evec);
-    tin += 7;
-    tout += 7;
-  }
-  /* basic and per-axis info handled by nrrdCopy above */
+  return 0;
+}
 
+/*
+******** tenExp
+**
+** takes the exp()  (by taking exp() of the eigenvalues)
+*/
+int
+tenExp(Nrrd *nout, const Nrrd *nin) {
+  char me[]="tenExp", err[BIFF_STRLEN];
+  funcParm parm;
+
+  if (theFunc(nout, nin, funcExp, &parm)) {
+    sprintf(err, "%s: trouble", me);
+    biffAdd(TEN, err); return 1;
+  }
   return 0;
 }
