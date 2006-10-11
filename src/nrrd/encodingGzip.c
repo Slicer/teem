916e78cfef1012518ab78b1ddb0e52e1fb4f0833
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2006-10-11 17:15:33 +0000

    negative byteskip now compatible with gzip encoding.  functionality contributed by Katharina Quintus
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@3484 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/nrrd/encodingGzip.c
+++ src/nrrd/encodingGzip.c
@@ -23,6 +23,12 @@
 #include "nrrd.h"
 #include "privateNrrd.h"
 
+typedef union {
+  char **c;
+  void **v;
+} ptrHack;
+
+
 int
 _nrrdEncodingGzip_available(void) {
 
@@ -33,19 +39,22 @@ _nrrdEncodingGzip_available(void) {
 #endif
 }
 
+/*
+** nio->byteSkip < 0 functionality contributed by Katharina Quintus
+*/
 int
 _nrrdEncodingGzip_read(FILE *file, void *_data, size_t elNum,
                        Nrrd *nrrd, NrrdIoState *nio) {
   char me[]="_nrrdEncodingGzip_read", err[BIFF_STRLEN];
 #if TEEM_ZLIB
-  size_t bsize, total_read, block_size;
+  size_t sizeData, sizeRed, sizeChunk;
   int i, error;
   unsigned int read;
   char *data;
   gzFile gzfin;
-  
-  bsize = nrrdElementSize(nrrd)*elNum;
+  ptrHack hack;
 
+  sizeData = nrrdElementSize(nrrd)*elNum;
   /* Create the gzFile for reading in the gzipped data. */
   if ((gzfin = _nrrdGzOpen(file, "rb")) == Z_NULL) {
     /* there was a problem */
@@ -54,72 +63,119 @@ _nrrdEncodingGzip_read(FILE *file, void *_data, size_t elNum,
     return 1;
   }
 
-  /* Here is where we do the byte skipping. */
-  for(i = 0; i < nio->byteSkip; i++) {
-    unsigned char b;
-    /* Check to see if a single byte was able to be read. */
-    if (_nrrdGzRead(gzfin, &b, 1, &read) != 0 || read != 1) {
-      sprintf(err, "%s: hit an error skipping byte %d of %d",
-              me, i, nio->byteSkip);
+  /* keeps track of how many bytes have been successfully read in */
+  sizeRed = 0;
+  
+  /* zlib can only handle data sizes up to UINT_MAX ==> if there's more
+     than UINT_MAX bytes to read in, we read in in chunks */
+  sizeChunk = AIR_MIN(sizeData, UINT_MAX);
+  
+  if (nio->byteSkip < 0) { 
+    /* We don't know the size of the size to skip before the data, so
+       decompress the data first into a temporary memory buffer.  Then
+       the byteskipping is then just memcpy-ing the appropriate region
+       of memory from "buff" into the given "_data" pointer */
+    char *buff;
+    airArray *buffArr;
+      
+    /* setting the airArray increment to twice the chunk size means that for
+       headers that are small compared to the data, the airArray never
+       actually has to reallocate.  The unit is 1 because we are managing
+       the reading in terms of bytes (sizeof(char)==1 by definition) */
+    buff = NULL;
+    hack.c = &buff;
+    buffArr = airArrayNew(hack.v, NULL, 1, 2*sizeChunk);
+    airArrayLenSet(buffArr, sizeChunk);
+    if (!( buffArr && buffArr->data )) {
+      sprintf(err, "%s: couldn't initialize airArray\n", me);
+      biffAdd(NRRD, err);
+      return 1;
+    }
+    
+    /* we keep reading in chunks as long as there hasn't been an error,
+       and we haven't hit EOF (EOF signified by read == 0).  Unlike the
+       code below (for positive byteskip), we are obligated to read until
+       the bitter end, and can't update sizeChunk to encompass only the 
+       required data. */
+    while (!(error = _nrrdGzRead(gzfin, buff + sizeRed, sizeChunk, &read))
+           && read > 0) {
+      sizeRed += read;
+      if (read >= sizeChunk) {
+        /* we were able to read as much data as we requested, maybe there is
+           more, so we need to make our temp buffer bigger */
+        airArrayLenIncr(buffArr, sizeChunk);
+        if (!buffArr->data) {
+          sprintf(err, "%s: couldn't re-allocate data buffer", me);
+          biffAdd(NRRD, err);
+          return 1;
+        }
+      }
+    }
+    if (error) {
+      sprintf(err, "%s: error reading from gzFile", me);
+      biffAdd(NRRD, err);
+      return 1;
+    }
+    if (sizeRed < sizeData + (-nio->byteSkip - 1)) {
+      sprintf(err, "%s: expected " _AIR_SIZE_T_CNV " bytes and received only "
+              _AIR_SIZE_T_CNV " bytes",
+              me, sizeData + (-nio->byteSkip - 1), sizeRed);
+      biffAdd(NRRD, err);
+      return 1;
+    }
+    /* also handles nio->byteSkip == -N-1 signifying extra N bytes at end */
+    memcpy(_data, buff + sizeRed - sizeData - (-nio->byteSkip - 1), sizeData);
+    airArrayNuke(buffArr);
+  } else {
+    /* no negative byteskip: after byteskipping, we can read directly
+       into given data buffer */
+    if (nio->byteSkip > 0) {
+      for (i=0; i<nio->byteSkip; i++) {
+        unsigned char b;
+        /* Check to see if a single byte was able to be read. */
+        if (_nrrdGzRead(gzfin, &b, 1, &read) != 0 || read != 1) {
+          sprintf(err, "%s: hit an error skipping byte %d of %d",
+                  me, i, nio->byteSkip);
+          biffAdd(NRRD, err);
+          return 1;
+        }
+      }
+    }
+    /* Pointer to chunks as we read them. */
+    data = AIR_CAST(char *, _data);
+    while (!(error = _nrrdGzRead(gzfin, data, sizeChunk, &read))
+           && read > 0) {
+      /* Increment the data pointer to the next available chunk. */
+      data += read; 
+      sizeRed += read;
+      /* We only want to read as much data as we need, so we need to check
+         to make sure that we don't request data that might be there but that
+         we don't want.  This will reduce sizeChunk when we get to the last
+         block (which may be smaller than sizeChunk). */
+      if (sizeData >= sizeRed 
+          && sizeData - sizeRed < sizeChunk) {
+        sizeChunk = sizeData - sizeRed;
+      }
+    }
+    if (error) {
+      sprintf(err, "%s: error reading from gzFile", me);
+      biffAdd(NRRD, err);
+      return 1;
+    }
+    /* Check to see if we got out as much as we thought we should. */
+    if (sizeRed != sizeData) {
+      sprintf(err, "%s: expected " _AIR_SIZE_T_CNV " bytes and received "
+              _AIR_SIZE_T_CNV " bytes",
+              me, sizeData, sizeRed);
       biffAdd(NRRD, err);
       return 1;
     }
   }
-  
-  /* zlib can handle data sizes up to UINT_MAX, so we can't just 
-     pass in the size, because it might be too large for an 
-     unsigned int.  Therefore it must be read in chunks 
-     if the size is larger than UINT_MAX. */
-  if (bsize <= UINT_MAX) {
-    block_size = bsize;
-  } else {
-    block_size = UINT_MAX;
-  }
-
-  /* This counter will help us to make sure that we read as much data
-     as we think we should. */
-  total_read = 0;
-  /* Pointer to the blocks as we read them. */
-  data = (char *)_data;
-  
-  /* Ok, now we can begin reading. */
-  while ((error = _nrrdGzRead(gzfin, data, block_size, &read)) == 0 
-         && read > 0) {
-    /* Increment the data pointer to the next available spot. */
-    data += read; 
-    total_read += read;
-    /* We only want to read as much data as we need, so we need to check
-       to make sure that we don't request data that might be there but that
-       we don't want.  This will reduce block_size when we get to the last
-       block (which may be smaller than block_size).
-    */
-    if (bsize >= total_read 
-        && bsize - total_read < block_size) {
-      block_size = bsize - total_read;
-    }
-  }
-
-  /* Check if we stopped because of an error. */
-  if (error != 0)
-  {
-    sprintf(err, "%s: error reading from gzFile", me);
-    biffAdd(NRRD, err);
-    return 1;
-  }
 
   /* Close the gzFile.  Since _nrrdGzClose does not close the FILE* we
      will not encounter problems when dataFile is closed later. */
-  if (_nrrdGzClose(gzfin) != 0) {
+  if (_nrrdGzClose(gzfin)) {
     sprintf(err, "%s: error closing gzFile", me);
-    biffAdd(NRRD, err);
-    return 1;
-  }
-  
-  /* Check to see if we got out as much as we thought we should. */
-  if (total_read != bsize) {
-    sprintf(err, "%s: expected " _AIR_SIZE_T_CNV " bytes and received "
-            _AIR_SIZE_T_CNV " bytes",
-            me, bsize, total_read);
     biffAdd(NRRD, err);
     return 1;
   }
@@ -141,13 +197,13 @@ _nrrdEncodingGzip_write(FILE *file, const void *_data, size_t elNum,
                         const Nrrd *nrrd, NrrdIoState *nio) {
   char me[]="_nrrdEncodingGzip_write", err[BIFF_STRLEN];
 #if TEEM_ZLIB
-  size_t bsize, total_written, block_size;
+  size_t sizeData, sizeWrit, sizeChunk;
   int fmt_i=0, error;
   char *data, fmt[4];
   gzFile gzfout;
   unsigned int wrote;
   
-  bsize = nrrdElementSize(nrrd)*elNum;
+  sizeData = nrrdElementSize(nrrd)*elNum;
 
   /* Set format string based on the NrrdIoState parameters. */
   fmt[fmt_i++] = 'w';
@@ -174,59 +230,50 @@ _nrrdEncodingGzip_write(FILE *file, const void *_data, size_t elNum,
     return 1;
   }
 
-  /* zlib can handle data sizes up to UINT_MAX, so we can't just 
-     pass in the bsize, because it might be too large for an 
-     unsigned int.  Therefore it must be read in chunks 
-     if the bsize is larger than UINT_MAX. */
-  if (bsize <= UINT_MAX) {
-    block_size = bsize;
-  } else {
-    block_size = UINT_MAX;
-  }
+  /* zlib can only handle data sizes up to UINT_MAX ==> if there's more
+     than UINT_MAX bytes to write out, we write out in chunks */
+  sizeChunk = AIR_MIN(sizeData, UINT_MAX);
 
-  /* This counter will help us to make sure that we write as much data
-     as we think we should. */
-  total_written = 0;
-  /* Pointer to the blocks as we write them. */
+  /* keeps track of what how much has been successfully written */
+  sizeWrit = 0;
+  /* Pointer to the chunks as we write them. */
   data = (char *)_data;
   
   /* Ok, now we can begin writing. */
-  while ((error = _nrrdGzWrite(gzfout, data, block_size, &wrote)) == 0 
+  while ((error = _nrrdGzWrite(gzfout, data, sizeChunk, &wrote)) == 0 
          && wrote > 0) {
     /* Increment the data pointer to the next available spot. */
     data += wrote;
-    total_written += wrote;
+    sizeWrit += wrote;
     /* We only want to write as much data as we need, so we need to check
        to make sure that we don't write more data than is there.  This
-       will reduce block_size when we get to the last block (which may
-       be smaller than block_size).
+       will reduce sizeChunk when we get to the last block (which may
+       be smaller than sizeChunk).
     */
-    if (bsize >= total_written
-        && (unsigned int)(bsize - total_written) < block_size)
-      block_size = bsize - total_written;
+    if (sizeData >= sizeWrit
+        && (unsigned int)(sizeData - sizeWrit) < sizeChunk)
+      sizeChunk = sizeData - sizeWrit;
   }
   
-  /* Check if we stopped because of an error. */
-  if (error != 0)
-  {
-    sprintf(err, "%s: error reading from gzFile", me);
+  if (error) {
+    sprintf(err, "%s: error writing to gzFile", me);
     biffAdd(NRRD, err);
     return 1;
   }
 
-  /* Close the gzFile.  Since _nrrdGzClose does not close the FILE* we
-     will not encounter problems when dataFile is closed later. */
-  if (_nrrdGzClose(gzfout) != 0) {
-    sprintf(err, "%s: error closing gzFile", me);
+  /* Check to see if we wrote out as much as we thought we should. */
+  if (sizeWrit != sizeData) {
+    sprintf(err, "%s: expected to write " _AIR_SIZE_T_CNV " bytes, but only "
+            "wrote " _AIR_SIZE_T_CNV,
+            me, sizeData, sizeWrit);
     biffAdd(NRRD, err);
     return 1;
   }
   
-  /* Check to see if we got out as much as we thought we should. */
-  if (total_written != bsize) {
-    sprintf(err, "%s: expected to write " _AIR_SIZE_T_CNV " bytes, but only "
-            "wrote " _AIR_SIZE_T_CNV,
-            me, bsize, total_written);
+  /* Close the gzFile.  Since _nrrdGzClose does not close the FILE* we
+     will not encounter problems when dataFile is closed later. */
+  if (_nrrdGzClose(gzfout)) {
+    sprintf(err, "%s: error closing gzFile", me);
     biffAdd(NRRD, err);
     return 1;
   }
