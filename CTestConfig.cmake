e292d2473a8b4ca60cf474e4329f1c667dec633f
bigler <bigler@3d70eeeb-363e-0410-a505-8a46323a89f2>
2008-02-28 23:10:38 +0000

    CTestConfig.cmake
      Use the SCI Dart server.
    
    
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@3759 3d70eeeb-363e-0410-a505-8a46323a89f2

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,8 @@
 SET (CTEST_PROJECT_NAME "TEEM")
-SET (CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
-SET (CTEST_DROP_METHOD "http")
-SET (CTEST_DROP_SITE "www.na-mic.org")
-SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
-SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-NAMIC.cgi")
+SET(CTEST_DROP_METHOD "xmlrpc")
+SET(CTEST_DROP_SITE "http://dashboard.sci.utah.edu:8081")
+SET(CTEST_DROP_LOCATION "Teem")
+# I'm not sure which one I need to set, so I'll both of these
+SET(NIGHTLY_START_TIME "21:00:00 MST")
+SET(CTEST_NIGHTLY_START_TIME "21:00:00 MST")
 
