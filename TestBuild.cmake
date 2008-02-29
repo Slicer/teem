dd096a4ab6f4d7dddd2b1b6606549a6ed6ae38a8
bigler <bigler@3d70eeeb-363e-0410-a505-8a46323a89f2>
2008-02-29 18:55:30 +0000

    TestBuild.cmake
      A CTest script to facilitate running a test build.
    
    
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@3760 3d70eeeb-363e-0410-a505-8a46323a89f2

---
+++ TestBuild.cmake
@@ -0,0 +1,35 @@
+# Where the source code lives
+SET (CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}")
+SET (CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/build-ctest")
+
+# Make sure we always reconfigure cmake stuff from scratch and don't
+# rely on previously built libraries
+SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+SET (CTEST_CMAKE_COMMAND "cmake")
+SET (CTEST_CVS_COMMAND "svn")
+
+# A smoke test only builds the code and doesn't run any tests, so we
+# exclude all tests here
+#SET (CTEST_COMMAND "ctest -D Nightly")
+SET (CTEST_COMMAND "ctest -D Experimental")
+
+SET(CTEST_INITIAL_CACHE "
+  BUILD_EXPERIMENTAL:BOOL=OFF
+  BUILD_HEX:BOOL=OFF
+  BUILD_SHARED_LIBS:BOOL=OFF
+  BUILD_TESTING:BOOL=ON
+  CMAKE_BUILD_TYPE:STRING=Release
+  DART_TESTING_TIMEOUT:INTEGER=600
+  TEEM_BZIP2:BOOL=ON
+  TEEM_PNG:BOOL=ON
+  TEEM_ZLIB:BOOL=ON
+")
+
+# Here are some other intersting things you can set for the initial cache
+# MAKECOMMAND:STRING=/usr/bin/make -j16
+# BUILDNAME:STRING=MySpecialConfig
+
+
+
+
