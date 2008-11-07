202a523d81db0b20e4d23824a6ad732abb4db595
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2008-11-07 17:14:00 +0000

    fixing FSF postal address in preamble, and other preamble glitches
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@3931 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/nrrd/test/histrad.c
+++ src/nrrd/test/histrad.c
@@ -4,8 +4,8 @@
   Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
 
   This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
+  modify it under the terms of the GNU Lesser General Public License
+  (LGPL) as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
   The terms of redistributing and/or modifying this software also
   include exceptions to the LGPL that facilitate static linking.
@@ -15,9 +15,9 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "../nrrd.h"
