dc6e5884fb47476017490c4eef8134ee6d751c66
ayla <ayla@3d70eeeb-363e-0410-a505-8a46323a89f2>
2008-11-26 03:00:39 +0000

    Update CTest configuration to submit tests to new public dashboard at http://www.cdash.org/CDashPublic/index.php?project=Teem.
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@4026 3d70eeeb-363e-0410-a505-8a46323a89f2

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,5 +1,5 @@
 #
-# Teem: Tools to process and visualize scientific data and images              
+# Teem: Tools to process and visualize scientific data and images   
 # Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
 # Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
 #
@@ -20,11 +20,16 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-SET (CTEST_PROJECT_NAME "Teem")
-SET(CTEST_DROP_METHOD "xmlrpc")
-SET(CTEST_DROP_SITE "http://dashboard.sci.utah.edu:8081")
-SET(CTEST_DROP_LOCATION "Teem")
-# I'm not sure which one I need to set, so I'll both of these
-SET(NIGHTLY_START_TIME "21:00:00 MST")
-SET(CTEST_NIGHTLY_START_TIME "21:00:00 MST")
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(Dart)
+set(CTEST_PROJECT_NAME "Teem")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Teem")
+set(CTEST_DROP_SITE_CDASH TRUE)
