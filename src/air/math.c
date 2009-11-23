06d92b5b022f46d4594250be728ed2f1fe2ffeca
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2009-11-23 19:28:31 +0000

    making airCbrt with __STRICT_ANSI__ (otherwise cbrt is defined but returns the wrong answer
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@4422 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/air/math.c
+++ src/air/math.c
@@ -249,9 +249,7 @@ airSgn(double v) {
 */
 double
 airCbrt(double v) {
-#ifdef _WIN32
-  /* msvc does not know how to take powers of small negative numbers,
-   * so we have to tell it to do it right */
+#if defined(_WIN32) || defined(__STRICT_ANSI__)
   return (v < 0.0 ? -pow(-v,1.0/3.0) : pow(v,1.0/3.0));
 #else
   return cbrt(v);
