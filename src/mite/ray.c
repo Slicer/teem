df77b68244d2677b3e5c2e634eb496566db6fd31
michelkitware <michelkitware@3d70eeeb-363e-0410-a505-8a46323a89f2>
2010-01-08 20:38:26 +0000

    BUG: used AIR_CAST and redeclarations as size_t to eliminate build errors on Win64.
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@4486 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/mite/ray.c
+++ src/mite/ray.c
@@ -246,7 +246,7 @@ miteSample(miteThread *mtt, miteRender *mrr, miteUser *muu,
 
   /* initialize txf range quantities, and apply all txfs */
   if (mtt->verbose) {
-    muu->debugIdx = airArrayLenIncr(muu->debugArr, muu->ndebug->axis[0].size);
+    muu->debugIdx = airArrayLenIncr(muu->debugArr, AIR_CAST(int, muu->ndebug->axis[0].size));
   }
 
   memcpy(mtt->range, muu->rangeInit, MITE_RANGE_NUM*sizeof(mite_t));
@@ -298,7 +298,8 @@ miteSample(miteThread *mtt, miteRender *mrr, miteUser *muu,
 
 int 
 miteRayEnd(miteThread *mtt, miteRender *mrr, miteUser *muu) {
-  int idx, slen, stageIdx;
+  int stageIdx;
+  size_t idx, slen;
   mite_t *imgData;
   double A;
   
