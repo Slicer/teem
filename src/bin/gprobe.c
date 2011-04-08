d4c916550f33321b12233100ef0dda0c5fae0da6
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2011-04-08 07:25:31 +0000

    now handles when spaceDim is set but space is not
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@4758 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/bin/gprobe.c
+++ src/bin/gprobe.c
@@ -764,7 +764,11 @@ main(int argc, char *argv[]) {
                                   | NRRD_BASIC_INFO_COMMENTS_BIT
                                   | NRRD_BASIC_INFO_KEYVALUEPAIRS_BIT));
     if (ctx->shape->fromOrientation) {
-      nrrdSpaceSet(nout, nin->space);
+      if (nin->space) {
+        nrrdSpaceSet(nout, nin->space);
+      } else {
+        nrrdSpaceDimensionSet(nout, nin->spaceDim);
+      }
       nrrdSpaceVecCopy(nout->spaceOrigin, nin->spaceOrigin);
       for (axi=0; axi<3; axi++) {
         double tmp;
