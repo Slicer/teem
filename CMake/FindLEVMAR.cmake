1e80c8b6a6394ee5227a2eee81c5748905764a3a
tststs <tststs@3d70eeeb-363e-0410-a505-8a46323a89f2>
2011-06-23 15:47:08 +0000

    CMake: Used FindBZ2.cmake as a template for Find{LEVMAR,FFTW3}.cmake
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@4879 3d70eeeb-363e-0410-a505-8a46323a89f2

--- CMake/FindLEVMAR.cmake
+++ CMake/FindLEVMAR.cmake
@@ -1,27 +1,22 @@
-# - Try to find LEVMAR
-# Once done this will define
-#  LEVMAR_FOUND - System has LEVMAR
-#  LEVMAR_INCLUDE_DIRS - The LEVMAR include directories
-#  LEVMAR_LIBRARIES - The libraries needed to use LEVMAR
-#  LEVMAR_DEFINITIONS - Compiler switches required for using LEVMAR
+FIND_PATH(LEVMAR_INCLUDE_DIR lm.h
+  /usr/local/include
+  /usr/include
+)
 
-find_package(PkgConfig)
-pkg_check_modules(PC_LEVMAR QUIET levmar)
-set(LEVMAR_DEFINITIONS ${PC_LEVMAR_CFLAGS_OTHER})
+FIND_LIBRARY(LEVMAR_LIBRARY levmar
+  /usr/lib
+  /usr/local/lib
+)
 
-find_path(LEVMAR_INCLUDE_DIR lm.h
-          HINTS ${PC_LEVMAR_INCLUDEDIR} ${PC_LEVMAR_INCLUDE_DIRS} )
+SET(LEVMAR_FOUND FALSE)
+IF(LEVMAR_INCLUDE_DIR AND LEVMAR_LIBRARY)
+    SET(LEVMAR_INCLUDE_DIRS ${LEVMAR_INCLUDE_DIR} )
+    SET(LEVMAR_LIBRARIES ${LEVMAR_LIBRARY} )
+    SET(LEVMAR_FOUND TRUE)
+ENDIF(LEVMAR_INCLUDE_DIR AND LEVMAR_LIBRARY)
 
-find_library(LEVMAR_LIBRARY NAMES levmar liblevmar
-             HINTS ${PC_LEVMAR_LIBDIR} ${PC_LEVMAR_LIBRARY_DIRS} )
-
-set(LEVMAR_LIBRARIES ${LEVMAR_LIBRARY} )
-set(LEVMAR_INCLUDE_DIRS ${LEVMAR_INCLUDE_DIR} )
-
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set LEVMAR_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(LEVMAR  DEFAULT_MSG
-                                  LEVMAR_LIBRARY LEVMAR_INCLUDE_DIR)
-
-mark_as_advanced(LEVMAR_INCLUDE_DIR LEVMAR_LIBRARY )
+MARK_AS_ADVANCED(
+  LEVMAR_INCLUDE_DIR
+  LEVMAR_LIBRARIES
+  LEVMAR_FOUND
+  )
