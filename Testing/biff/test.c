332837fb28c523e8f6408627afa4cb31ce55d0a7
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2012-08-14 23:06:53 +0000

    test now actually checks for correctness of output
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@5273 3d70eeeb-363e-0410-a505-8a46323a89f2

--- Testing/biff/test.c
+++ Testing/biff/test.c
@@ -1,5 +1,6 @@
 /*
   Teem: Tools to process and visualize scientific data and images              
+  Copyright (c) 2012, 2011, 2010, 2009  University of Chicago
   Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
   Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
 
@@ -23,59 +24,126 @@
 
 #include "teem/biff.h"
 
-int
-main() {
-  char *tmp;
+/*
+** Tests:
+** biffAdd
+** biffAddf
+** biffMove
+** biffMovef
+** biffGet
+** biffDone
+** biffGetDone
+**
+** Also uses:
+** airMopNew, airMopAdd, airMopError, airMopDone
+*/
 
+int
+main(int argc, const char *argv[]) {
+  const char *me;
+  char *tmp;
+  airArray *mop;
+
+  AIR_UNUSED(argc);
+  me = argv[0];
+  mop = airMopNew();
+
+  /* HEY: the creation and comparison of these strings is not very
+     flexible, especially the hard-coded "good[]" strings */
   biffAdd("axis", "the first error axis");
-  biffAdd("axis", "the second error axis");
-  biffAdd("axis", "the third error axis");
   biffAdd("chard", "the first error chard");
   biffAdd("chard", "the second error chard");
+  biffAdd("axis", "the second error axis");
   biffAdd("chard", "the third error chard");
   biffAdd("bingo", "zero-eth bingo message");
   biffMove("bingo", NULL, "chard");
   biffAdd("bingo", "the first error bingo");
   biffAdd("bingo", "the second bll boo boo boo error bingo");
   biffAdd("bingo", "the third error bingo");
-  printf("%s\n", (tmp = biffGet("bingo")));
-  free(tmp);
-  biffDone("bingo");
-  printf("%s\n", (tmp = biffGet("chard")));
-  free(tmp);
-  biffDone("chard");
-  printf("%s\n", (tmp = biffGet("axis")));
-  free(tmp);
-  biffDone("axis");
-  
+  biffAdd("axis", "the third error axis");
+  {
+    char good[] = ("[bingo] the third error bingo\n"
+                   "[bingo] the second bll boo boo boo error bingo\n"
+                   "[bingo] the first error bingo\n"
+                   "[bingo] [chard] the third error chard\n"
+                   "[bingo] [chard] the second error chard\n"
+                   "[bingo] [chard] the first error chard\n"
+                   "[bingo] zero-eth bingo message\n");
+    tmp = biffGet("bingo");
+    airMopAdd(mop, tmp, airFree, airMopAlways);
+
+    /* an ugly macro */
+#define COMPARE(N)                                                 \
+    airMopAdd(mop, tmp, airFree, airMopAlways);                    \
+    if (strcmp(tmp, good)) {                                       \
+      fprintf(stderr, "%s: %d: #%s# != #%s#\n", me, N, tmp, good); \
+      airMopError(mop);                                            \
+      exit(1);                                                     \
+    }
+
+    COMPARE(1);
+  }
+
+  {
+    char good[] = "";
+    tmp = biffGet("chard");
+    COMPARE(2);
+  }
+
+  {
+    char good[] = ("[axis] the third error axis\n"
+                   "[axis] the second error axis\n"
+                   "[axis] the first error axis\n");
+    tmp = biffGet("axis");
+    COMPARE(3);
+  }
+
   biffAdd("harold", "the first error harold");
   biffAdd("harold", "the second error harold");
   biffAdd("harold", "the third error harold");
-  printf("%s\n", (tmp = biffGet("harold")));
-  free(tmp);
+  {
+    char good[] = ("[harold] the third error harold\n"
+                   "[harold] the second error harold\n"
+                   "[harold] the first error harold\n");
+    tmp = biffGetDone("harold");
+    COMPARE(4);
+  }
 
+  biffDone("bingo");
+  biffDone("axis");
+  biffDone("chard");
+  
   biffAdd("axis", "the first error axis");
   biffAdd("axis", "the second error axis");
   biffAdd("axis", "the third error axis");
   biffAdd("axis", "the fourth error axis");
   biffAdd("axis", "the fifth error axis");
-  printf("%s\n", (tmp = biffGet("axis")));
-  free(tmp);
-  biffDone("axis");
+  { 
+    char good[] = ("[axis] the fifth error axis\n"
+                   "[axis] the fourth error axis\n"
+                   "[axis] the third error axis\n"
+                   "[axis] the second error axis\n"
+                   "[axis] the first error axis\n");
+    tmp = biffGetDone("axis");
+    COMPARE(5);
+  }
+  
+  biffAddf("test", "%s: this is a test of biffAddf %d %g", "me", 1, 4.2);
+  {
+    char good[] = "[test] me: this is a test of biffAddf 1 4.2\n";
+    tmp = biffGetDone("test");
+    COMPARE(6);
+  }
 
-  biffAddf("test", "%s: this is a test of biffAddf %d %f", "me", 1, 2.0);
-  printf("%s\n", (tmp = biffGet("test")));
-  free(tmp);
-  biffDone("test");
+  biffAddf("test2", "%s: this is a test of biffAddf %d %g", "me", 1, 4.2);
+  biffMovef("test3", "test2", "%s: testing biffMove %d.", "me", 1729);
+  {
+    char good[] = ("[test3] me: testing biffMove 1729.\n"
+                   "[test3] [test2] me: this is a test of biffAddf 1 4.2\n");
+    tmp = biffGet("test3");
+    COMPARE(7);
+  }
 
-  biffAddf("test2", "%s: this is a test of biffAddf %d %f", "me", 1, 2.0);
-  biffMovef("test3", "test2", "%s: testing biffMove %d.", "me", 69);
-  printf("%s\n", (tmp = biffGet("test3")));
-  free(tmp);
-  biffDone("test3");
-
+  airMopOkay(mop);
   exit(0);
 }
-
-
-
