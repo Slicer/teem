b5e6075af6fabe4ec3e7a83b0bab4e19b727f9ae
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2012-08-26 20:02:29 +0000

    more use of experimental /*Teem annotation
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@5382 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/meet/meetGage.c
+++ src/meet/meetGage.c
@@ -1,5 +1,6 @@
 /*
   Teem: Tools to process and visualize scientific data and images              
+  Copyright (C) 2012, 2011, 2010, 2009  University of Chicago
   Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
   Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
 
@@ -22,7 +23,8 @@
 
 #include "meet.h"
 
-gageKind *
+
+gageKind * /*Teem: error if (!ret) */
 _meetGageKindParse(const char *_str, int constOnly) {
   char *str;
   gageKind *ret;
@@ -49,13 +51,13 @@ _meetGageKindParse(const char *_str, int constOnly) {
   return ret;
 }
 
-gageKind *
+gageKind * /*Teem: error if (!ret) */
 meetGageKindParse(const char *_str) {
 
   return _meetGageKindParse(_str, AIR_FALSE);
 }
 
-const gageKind *
+const gageKind * /*Teem: error if (!ret) */
 meetConstGageKindParse(const char *_str) {
 
   return _meetGageKindParse(_str, AIR_TRUE);
