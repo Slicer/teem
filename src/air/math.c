a67fcac07daa868d1b92a510053722733f2a455d
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2012-09-15 05:48:55 +0000

    removing duplicate const
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@5511 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/air/math.c
+++ src/air/math.c
@@ -755,7 +755,7 @@ airHalton(double *out, unsigned int index,
 }
 
 /* via "Table[Prime[n], {n, 1000}]" in Mathematica */
-const unsigned int const airPrimeList[AIR_PRIME_NUM] = {
+const unsigned int airPrimeList[AIR_PRIME_NUM] = {
   2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
   67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
   139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211,
