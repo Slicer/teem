5f7ba1a8b3ffa5dee451b6dfce357d65009a10a8
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2012-09-16 22:03:16 +0000

    adding epsilon to nrrdCompare call
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@5528 3d70eeeb-363e-0410-a505-8a46323a89f2

--- Testing/ten/glyphBqd.c
+++ Testing/ten/glyphBqd.c
@@ -70,7 +70,7 @@ main(int argc, const char **argv) {
   airMopAdd(mop, nref, (airMopper)nrrdNuke, airMopAlways);
   if (nrrdLoad(nref, refname, NULL)
       || nrrdCompare(nref, nabc, AIR_FALSE /* onlyData */,
-                     &differ, explain)) {
+                     0.0 /* epsilon */, &differ, explain)) {
     airMopAdd(mop, err = biffGetDone(NRRD), airFree, airMopAlways);
     fprintf(stderr, "trouble loading or comparing with ref:\n%s", err);
     airMopError(mop); return 1;
