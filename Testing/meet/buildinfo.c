539e9d66af4b108f11d8745dc5ceaa8da3e3698b
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2012-09-20 08:25:22 +0000

    removing unused variable
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@5563 3d70eeeb-363e-0410-a505-8a46323a89f2

--- Testing/meet/buildinfo.c
+++ Testing/meet/buildinfo.c
@@ -44,7 +44,6 @@ main(int argc, const char **argv) {
   char expapps[] = "_off_";
 #endif
 
-  char liblist[AIR_STRLEN_LARGE];
   char stmp1[AIR_STRLEN_SMALL], stmp2[AIR_STRLEN_SMALL];
   AIR_UNUSED(argc);
   AIR_UNUSED(argv);
