05c81d8e973b406749252ba3088196e6fe576b27
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2013-08-14 06:05:54 +0000

    trying to get kernall test to pass
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6040 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/nrrd/kernel.c
+++ src/nrrd/kernel.c
@@ -3391,7 +3391,7 @@ nrrdKernelCheck(const NrrdKernel *kern,
       float specEps;
       if (nrrdKernelForwDiff == kern) {
         specEps = 0.000000005;
-      } if (nrrdKernelForwDiff == kern) {
+      } if (nrrdKernelBCCubic == kern) {
         specEps = 8e-18;
       } else {
         specEps = 0.0;
