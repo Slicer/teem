2de062e71b7b001c274193feec0967a615708e81
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2013-12-10 22:10:30 +0000

    added the abiliyt to pad the axis just created, using a given boundary spec.  This also requires being slightly smarter about how the kind is set - the default stub kind is not appropriate in the case of padding, so we use the new hestSource enum to see if the kind is set via default vs via user
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6154 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/unrrdu/axinsert.c
+++ src/unrrdu/axinsert.c
@@ -30,7 +30,7 @@ static const char *_unrrdu_axinsertInfoL =
  ". The underlying linear ordering of the samples is "
  "unchanged, and the information about the other axes is "
  "shifted upwards as needed.\n "
- "* Uses nrrdAxesInsert");
+ "* Uses nrrdAxesInsert, and with \"-s\", nrrdPad_nva");
 
 int
 unrrdu_axinsertMain(int argc, const char **argv, const char *me,
@@ -39,17 +39,30 @@ unrrdu_axinsertMain(int argc, const char **argv, const char *me,
   char *out, *err, *label;
   Nrrd *nin, *nout;
   int pret, kind;
-  unsigned int axis;
+  unsigned int axis, size, opi;
   double mm[2];
   airArray *mop;
+  NrrdBoundarySpec *bspec;
 
+  hparm->elideSingleOtherDefault = AIR_FALSE;
   OPT_ADD_AXIS(axis, "dimension (axis index) at which to insert the new axis");
   hestOptAdd(&opt, "l,label", "label", airTypeString, 1, 1, &label, "",
              "label to associate with new axis");
-  hestOptAdd(&opt, "k,kind", "kind", airTypeEnum, 1, 1, &kind, "stub",
-             "axis kind to associate with new axis", NULL, nrrdKind);
+  opi = hestOptAdd(&opt, "k,kind", "kind", airTypeEnum, 1, 1, &kind, "stub",
+                   "axis kind to associate with new axis", NULL, nrrdKind);
   hestOptAdd(&opt, "mm,minmax", "min max", airTypeDouble, 2, 2, mm, "nan nan",
              "min and max values along new axis");
+  hestOptAdd(&opt, "s,size", "size", airTypeUInt, 1, 1, &size, "1",
+             "after inserting stub axis, also pad out to some length, "
+             "according to the \"-b\" option");
+  hestOptAdd(&opt, "b,boundary", "behavior", airTypeOther, 1, 1, &bspec,
+             "bleed",
+             "How to handle samples beyond the input bounds:\n "
+             "\b\bo \"pad:<val>\": use specified value\n "
+             "\b\bo \"bleed\": extend border values outward\n "
+             "\b\bo \"mirror\": repeated reflections\n "
+             "\b\bo \"wrap\": wrap-around to other side",
+             NULL, NULL, nrrdHestBoundarySpec);
   OPT_ADD_NIN(nin, "input nrrd");
   OPT_ADD_NOUT(out, "output nrrd");
 
@@ -69,6 +82,36 @@ unrrdu_axinsertMain(int argc, const char **argv, const char *me,
     airMopError(mop);
     return 1;
   }
+  if (1 < size) {
+    /* we also do padding here */
+    ptrdiff_t min[NRRD_DIM_MAX], max[NRRD_DIM_MAX];
+    unsigned int ai;
+    Nrrd *npad;
+    for (ai=0; ai<nout->dim; ai++) {
+      min[ai] = 0;
+      max[ai] = nout->axis[ai].size - 1;
+    }
+    max[axis] = size-1;
+    npad = nrrdNew();
+    airMopAdd(mop, npad, (airMopper)nrrdNuke, airMopAlways);
+    if (nrrdPad_nva(npad, nout, min, max,
+                    bspec->boundary, bspec->padValue)) {
+      airMopAdd(mop, err = biffGetDone(NRRD), airFree, airMopAlways);
+      fprintf(stderr, "%s: error padding:\n%s", me, err);
+      airMopError(mop);
+      return 1;
+    }
+    /* sneaky, but ok; nothing changes in the mops */
+    nout = npad;
+    /* only set output kind if explicitly requested
+       (since the default is not appropriate) */
+    if (hestSourceUser == opt[opi].source) {
+      nout->axis[axis].kind = kind;
+    }
+  } else {
+    /* no request to pad; setting the default "stub" kind is sensible */
+    nout->axis[axis].kind = kind;
+  }
   if (strlen(label)) {
     nout->axis[axis].label = (char *)airFree(nout->axis[axis].label);
     nout->axis[axis].label = airStrdup(label);
@@ -79,7 +122,6 @@ unrrdu_axinsertMain(int argc, const char **argv, const char *me,
   if (AIR_EXISTS(mm[1])) {
     nout->axis[axis].max = mm[1];
   }
-  nout->axis[axis].kind = kind;
 
   SAVE(out, nout, NULL);
 
