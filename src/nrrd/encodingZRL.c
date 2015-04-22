Slicer/teem

commit c924e9966827b27bf641c5cf58f987bcaaa108a1

first pass at ZRL decompression
git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6219 3d70eeeb-363e-0410-a505-8a46323a89f2

src/nrrd/encodingZRL.c
@@ -35,6 +35,26 @@ _nrrdEncodingZRL_read(FILE *file, void *data, size_t elementNum,
                       Nrrd *nrrd, NrrdIoState *nio) {
   static const char me[]="_nrrdEncodingZRL_read";
 
+  AIR_UNUSED(nio);
+  unsigned char *output_buffer = (unsigned char *) data;
+  size_t toread = elementNum*nrrdElementSize(nrrd);
+  printf("!%s: looking for %u values (%u bytes) of type %s\n", me,
+         (unsigned int)elementNum, (unsigned int)toread, airEnumStr(nrrdType, nrrd->type));
+  int cc, dd;
+  unsigned int j = 0;
+  while (j < toread) {
+    if ((cc = fgetc(file)) == 0) {
+      if ((dd = fgetc(file)) == 0) {
+        j += dd + fgetc(file)*256;
+      } else {
+        j += (unsigned char)dd;
+      }
+    } else {
+      output_buffer[j] = (unsigned char)cc;
+      j++;
+    }
+  }
+
   return 0;
 }
 

commit e4746083c0e1dc0c137124c41eca5d23adf73bfa

COMP: Fix MSVC build error in encodingZRL.c
This commit fixes a regression introduced in r6219 (first pass at
ZRL decompression).

When building teem using microsoft compiler from vs2008, the following
error was reported:

..\teem\src\nrrd\encodingZRL.c(38) : error C2143: syntax error : missing ';' before 'type'

src/nrrd/encodingZRL.c
@@ -34,7 +34,6 @@ static int
 _nrrdEncodingZRL_read(FILE *file, void *data, size_t elementNum,
                       Nrrd *nrrd, NrrdIoState *nio) {
 
-  AIR_UNUSED(nio);
   unsigned char *output_buffer = (unsigned char *) data;
   size_t toread = elementNum*nrrdElementSize(nrrd);
   /*
@@ -44,6 +43,10 @@ _nrrdEncodingZRL_read(FILE *file, void *data, size_t elementNum,
          airEnumStr(nrrdType, nrrd->type)); */
   int cc, dd;
   unsigned int j = 0;
+
+  AIR_UNUSED(nrrd);
+  AIR_UNUSED(nio);
+
   while (j < toread) {
     cc = fgetc(file);
     if (cc == 0) {