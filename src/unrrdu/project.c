720b43afe1aa370034458ef022b690a92b6ce5d0
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2015-07-09 17:28:24 +0000

    since you often want to do multiple projections along multiple axes, now you can give multiple axes to -a (with matching number of outputs to -o) as well as multiple measures to -m
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6252 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/unrrdu/project.c
+++ src/unrrdu/project.c
@@ -33,31 +33,40 @@ static const char *_unrrdu_projectInfoL =
  "one less than input (except when the input is itself 1-D); "
  "the output type depends on "
  "the measure in a non-trivial way, or it can be set explicitly "
- "with the \"-t\" option.\n "
- "* Uses nrrdProject");
+ "with the \"-t\" option.  To save the overhead of multiple input data "
+ "reads if projections along different axes are needed, you can give "
+ "multiple axes to \"-a\" (and a matching number of output filenames "
+ "to \"-o\"), as well as multiple measures to \"-m\" (and possibly a "
+ "specific type to \"-t\" to permit their joining on fastest axis).\n "
+ "* Uses nrrdProject, and nrrdJoin if multiple measures");
 
 int
 unrrdu_projectMain(int argc, const char **argv, const char *me,
                    hestParm *hparm) {
   hestOpt *opt = NULL;
-  char *out, *err;
+  char **out, *err;
   Nrrd *nin, *nout;
-  unsigned int axis;
-  int measr, pret, type;
+  Nrrd **nslice;
+  unsigned int *axis, axisLen, outLen, measrLen, outIdx, measrIdx;
+  int *measr, pret, type;
   airArray *mop;
 
-  OPT_ADD_AXIS(axis, "axis to project along");
-  hestOptAdd(&opt, "m,measure", "measr", airTypeEnum, 1, 1, &measr, NULL,
+  hestOptAdd(&opt, "a,axis", "axis", airTypeUInt, 1, -1, &axis, NULL,
+             "axis or axes to project along", &axisLen);
+  hestOptAdd(&opt, "m,measure", "measr", airTypeEnum, 1, -1, &measr, NULL,
              "How to \"measure\" a scanline, by summarizing all its values "
-             "with a single scalar. " NRRD_MEASURE_DESC,
-             NULL, nrrdMeasure);
+             "with a single scalar. Multiple measures will be joined along "
+             "fastest axis if output, but you may need to set output type "
+             "explicitly via \"-t\" so that the join works. "
+             NRRD_MEASURE_DESC, &measrLen, nrrdMeasure);
   hestOptAdd(&opt, "t,type", "type", airTypeOther, 1, 1, &type, "default",
              "type to use for output. By default (not using this option), "
              "the output type is determined auto-magically",
              NULL, NULL, &unrrduHestMaybeTypeCB);
   OPT_ADD_NIN(nin, "input nrrd");
-  OPT_ADD_NOUT(out, "output nrrd");
-
+  hestOptAdd(&opt, "o,output", "nout", airTypeString, 1, -1, &out, "-",
+             "one or more output nrrd filenames. Number of names here "
+             "has to match number of axes specified.", &outLen);
   mop = airMopNew();
   airMopAdd(mop, opt, (airMopper)hestOptFree, airMopAlways);
 
@@ -65,17 +74,56 @@ unrrdu_projectMain(int argc, const char **argv, const char *me,
   PARSE();
   airMopAdd(mop, opt, (airMopper)hestParseFree, airMopAlways);
 
-  nout = nrrdNew();
-  airMopAdd(mop, nout, (airMopper)nrrdNuke, airMopAlways);
-
-  if (nrrdProject(nout, nin, axis, measr, type)) {
-    airMopAdd(mop, err = biffGetDone(NRRD), airFree, airMopAlways);
-    fprintf(stderr, "%s: error projecting nrrd:\n%s", me, err);
+  if (axisLen != outLen) {
+    fprintf(stderr, "%s: got %u \"-a\" axes but %u \"-o\" outputs\n", me,
+            axisLen, outLen);
     airMopError(mop);
     return 1;
   }
 
-  SAVE(out, nout, NULL);
+  if (measrLen > 1) {
+    nslice = AIR_CALLOC(measrLen, Nrrd *);
+    airMopAdd(mop, nslice, airFree, airMopAlways);
+    for (measrIdx=0; measrIdx<measrLen; measrIdx++) {
+      nslice[measrIdx] = nrrdNew();
+      airMopAdd(mop, nslice[measrIdx], (airMopper)nrrdNuke, airMopAlways);
+    }
+  }
+  nout = nrrdNew();
+  airMopAdd(mop, nout, (airMopper)nrrdNuke, airMopAlways);
+
+  for (outIdx=0; outIdx<outLen; outIdx++) {
+    if (measrLen > 1) {
+      /* first project into slices */
+      for (measrIdx=0; measrIdx<measrLen; measrIdx++) {
+        if (nrrdProject(nslice[measrIdx], nin, axis[outIdx],
+                        measr[measrIdx], type)) {
+          airMopAdd(mop, err = biffGetDone(NRRD), airFree, airMopAlways);
+          fprintf(stderr, "%s: error projecting nrrd %u/%u:\n%s",
+                  me, outIdx, measrIdx, err);
+          airMopError(mop);
+          return 1;
+        }
+      }
+      /* then join slices into output */
+      if (nrrdJoin(nout, (const Nrrd *const*)nslice, measrLen, 0, AIR_TRUE)) {
+        airMopAdd(mop, err = biffGetDone(NRRD), airFree, airMopAlways);
+        fprintf(stderr, "%s: error joining nrrd %u; will have to use \"-t\" "
+                "option to make sure all projections have same type:\n%s",
+                me, outIdx, err);
+        airMopError(mop);
+        return 1;
+      }
+    } else {
+      if (nrrdProject(nout, nin, axis[outIdx], measr[0], type)) {
+        airMopAdd(mop, err = biffGetDone(NRRD), airFree, airMopAlways);
+        fprintf(stderr, "%s: error projecting nrrd %u:\n%s", me, outIdx, err);
+        airMopError(mop);
+        return 1;
+      }
+    }
+    SAVE(out[outIdx], nout, NULL);
+  }
 
   airMopOkay(mop);
   return 0;
