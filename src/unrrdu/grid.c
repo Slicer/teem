d6d27a9dcaf45fbc49f501db6277f2a71ec3a13a
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2017-09-12 04:18:11 +0000

    adding sloppy -pg option, to generate a grid good for gprobe
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6324 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/unrrdu/grid.c
+++ src/unrrdu/grid.c
@@ -24,19 +24,8 @@
 #include "unrrdu.h"
 #include "privateUnrrdu.h"
 
-#define INFO "generate list of oriented grid locations"
-static const char *_unrrdu_gridInfoL =
-(INFO ". For a N-D grid, the output is (by default) "
- "a 2-D M-by-S array of grid sample "
- "locations, where M is the space dimension of the oriented grid, and S "
- "is the total number of real samples in the grid. "
- "With the -ps option, the shape of input axes is better preserved. "
- "Implementation currently incomplete, because of the number of "
- "unresolved design questions.\n "
- "* (not based on any particular nrrd function)");
-
 static int
-gridGen(Nrrd *nout, int typeOut, const Nrrd *nin, int psz) {
+gridGen(Nrrd *nout, int typeOut, const Nrrd *nin, int psz, int psg) {
   static const char me[]="gridGen";
   size_t II, NN, size[NRRD_DIM_MAX], osz[NRRD_DIM_MAX], coord[NRRD_DIM_MAX];
   double loc[NRRD_SPACE_DIM_MAX],
@@ -62,26 +51,31 @@ gridGen(Nrrd *nout, int typeOut, const Nrrd *nin, int psz) {
     return 1;
   }
   if (!( sdim <= dim )) {
-    biffAddf(UNRRDU, "%s: can't handle space dimension %u > dimension %u",
-             me, sdim, dim);
+    biffAddf(UNRRDU, "%s: sorry, can't currently handle space dimension %u "
+             "> dimension %u", me, sdim, dim);
     return 1;
   }
   base = dim - sdim;
   NN = 1;
   nrrdAxisInfoGet_nva(nin, nrrdAxisInfoSize, size);
   nrrdAxisInfoGet_nva(nin, nrrdAxisInfoSpaceDirection, sdir);
-  osz[oxi++] = sdim;
+  if (psg) {
+    osz[0] = 1+sdim;
+    osz[1] = 1+sdim;
+  } else {
+    osz[oxi++] = sdim;
+  }
   for (axi=base; axi<dim; axi++) {
     if (!nrrdSpaceVecExists(sdim, sdir[axi])) {
       biffAddf(UNRRDU, "%s: axis %u space dir didn't exist", me, axi);
       return 1;
     }
     NN *= size[axi];
-    if (psz) {
+    if (!psg && psz) {
       osz[oxi++] = size[axi];
     }
   }
-  if (!psz) {
+  if (!psg && !psz) {
     osz[1] = NN;
   }
   ins = nrrdDInsert[typeOut];
@@ -91,55 +85,87 @@ gridGen(Nrrd *nout, int typeOut, const Nrrd *nin, int psz) {
     return 1;
   }
   out = AIR_CAST(void *, nout->data);
-  for (axi=0; axi<dim; axi++) {
-    coord[axi] = 0;
-  }
-  for (II=0; II<NN; II++) {
+  if (psg) {
+    unsigned int jj;
+    ins(out, 0, sdim);
     nrrdSpaceVecCopy(loc, nin->spaceOrigin);
+    for (jj=0; jj<sdim; jj++) {
+      ins(out, 1 + jj, loc[jj]);
+    }
     for (axi=base; axi<dim; axi++) {
-      nrrdSpaceVecScaleAdd2(loc, 1, loc, coord[axi], sdir[axi]);
+      unsigned int oi = (1+sdim)*(1+axi-base);
+      ins(out, oi, size[axi]);
+      for (jj=0; jj<sdim; jj++) {
+        ins(out, oi+1+jj, sdir[axi][jj]);
+      }
     }
-    /*
-    fprintf(stderr, "!%s: (%u) %u %u %u: %g %g\n", me,
-            AIR_CAST(unsigned int, II),
-            AIR_CAST(unsigned int, coord[0]),
-            AIR_CAST(unsigned int, coord[1]),
-            AIR_CAST(unsigned int, coord[2]),
-            loc[0], loc[1]);
-    */
-    for (axi=0; axi<sdim; axi++) {
-      ins(out, axi + sdim*II, loc[axi]);
+  } else {
+    for (axi=0; axi<dim; axi++) {
+      coord[axi] = 0;
     }
-    NRRD_COORD_INCR(coord, size, dim, base);
+    for (II=0; II<NN; II++) {
+      nrrdSpaceVecCopy(loc, nin->spaceOrigin);
+      for (axi=base; axi<dim; axi++) {
+        nrrdSpaceVecScaleAdd2(loc, 1, loc, coord[axi], sdir[axi]);
+      }
+      /*
+      fprintf(stderr, "!%s: (%u) %u %u %u: %g %g\n", me,
+              AIR_CAST(unsigned int, II),
+              AIR_CAST(unsigned int, coord[0]),
+              AIR_CAST(unsigned int, coord[1]),
+              AIR_CAST(unsigned int, coord[2]),
+              loc[0], loc[1]);
+      */
+      for (axi=0; axi<sdim; axi++) {
+        ins(out, axi + sdim*II, loc[axi]);
+      }
+      NRRD_COORD_INCR(coord, size, dim, base);
+    }
   }
-
   return 0;
 }
+
+#define INFO "Describe image sample locations"
+static const char *_unrrdu_gridInfoL =
+(INFO ". For a N-D grid, the output is (by default) "
+ "a 2-D M-by-S array of grid sample "
+ "locations, where M is the space dimension of the oriented grid, and S "
+ "is the total number of real samples in the grid. "
+ "With the -ps option, the shape of input axes is better preserved. "
+ "With the -pg option, the output is a 2-D array that specifies the "
+ "sampling grid, in the format used by gprobe. Unfortunately the "
+ "implementation is currently incomplete, because of a number of "
+ "unresolved design questions.\n "
+ "* (not based on any particular nrrd function)");
 
 int
 unrrdu_gridMain(int argc, const char **argv, const char *me,
                    hestParm *hparm) {
   hestOpt *opt = NULL;
-  char *out, *err;
-  Nrrd *nin, *nout;
+  char *outS, *err;
   int pret;
   airArray *mop;
+  char *inS;
 
-  int typeOut, psz;
+  Nrrd *nin, *nout;
+  int typeOut, psz, psg;
+  NrrdIoState *nio;
 
-  hestOptAdd(&opt, "i,input", "nin", airTypeOther, 1, 1, &nin, NULL,
+  hestOptAdd(&opt, "i,input", "nin", airTypeString, 1, 1, &inS, NULL,
              "input nrrd.  That this argument is required instead of "
              "optional, as with most unu commands, is a quirk caused by the "
              "need to have \"unu grid\" generate usage info, combined "
              "with the fact that the other arguments have sensible "
-             "defaults",
-             NULL, NULL, nrrdHestNrrd);
+             "defaults");
   hestOptAdd(&opt, "ps", NULL, airTypeInt, 0, 0, &psz, NULL,
              "instead of the default behavior of flattening all but the "
              "fastest axis, preserve the sizes of axes, so that the output "
              "is more like that of the input");
+  hestOptAdd(&opt, "pg", NULL, airTypeInt, 0, 0, &psg, NULL,
+             "(overrides -ps) generate a 2D array that represents "
+             "the sampling grid in the way that \"gprobe -pg\" understands");
   OPT_ADD_TYPE(typeOut, "type of output", "double");
-  OPT_ADD_NOUT(out, "output nrrd");
+  OPT_ADD_NOUT(outS, "output nrrd");
 
   mop = airMopNew();
   airMopAdd(mop, opt, (airMopper)hestOptFree, airMopAlways);
@@ -148,20 +174,31 @@ unrrdu_gridMain(int argc, const char **argv, const char *me,
   PARSE();
   airMopAdd(mop, opt, (airMopper)hestParseFree, airMopAlways);
 
+  nio = nrrdIoStateNew();
+  airMopAdd(mop, nio, (airMopper)nrrdIoStateNix, airMopAlways);
+  nin = nrrdNew();
+  airMopAdd(mop, nin, (airMopper)nrrdNuke /* but Nix enough */, airMopAlways);
+  nio->skipData = AIR_TRUE;
+  if (nrrdLoad(nin, inS, nio)) {
+    airMopAdd(mop, err = biffGetDone(NRRD), airFree, airMopAlways);
+    fprintf(stderr, "%s: error reading input:\n%s", me, err);
+    airMopError(mop);
+    return 1;
+  }
   nout = nrrdNew();
   airMopAdd(mop, nout, (airMopper)nrrdNuke, airMopAlways);
 
-  if (gridGen(nout, typeOut, nin, psz)) {
+  if (gridGen(nout, typeOut, nin, psz, psg)) {
     airMopAdd(mop, err = biffGetDone(UNRRDU), airFree, airMopAlways);
     fprintf(stderr, "%s: error generating output:\n%s", me, err);
     airMopError(mop);
     return 1;
   }
 
-  SAVE(out, nout, NULL);
+  SAVE(outS, nout, NULL);
 
   airMopOkay(mop);
   return 0;
 }
 
-UNRRDU_CMD_HIDE(grid, INFO);
+UNRRDU_CMD(grid, INFO);
