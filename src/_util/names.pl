529419ad4ff8e49bdb052dbe6e1e6ba90a8a1b59
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2022-07-30 04:00:57 +0000

    updated for new location
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6579 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/_util/names.pl
+++ src/_util/names.pl
@@ -1,5 +1,5 @@
 #
-# Teem: Tools to process and visualize scientific data and images              
+# Teem: Tools to process and visualize scientific data and images
 # Copyright (C) 2013, 2012, 2011, 2010, 2009  University of Chicago
 # Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
 # Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
@@ -29,8 +29,8 @@
 # Currently this is very simple!  Usage is:
 #
 # (TEEM_LIB_LIST)
-# ls -1 {air,hest,biff,nrrd,ell,unrrdu,alan,moss,tijk,gage,dye,bane,limn,echo,hoover,seek,ten,elf,pull,coil,push,mite,meet}/*.c | perl names.pl | sort | wc -l
-# ls -1 {air,hest,biff,nrrd,ell,unrrdu,alan,moss,tijk,gage,dye,bane,limn,echo,hoover,seek,ten,elf,pull,coil,push,mite,meet}/*.c | perl names.pl | sort | uniq | wc -l
+# ls -1 ../{air,hest,biff,nrrd,ell,unrrdu,alan,moss,tijk,gage,dye,bane,limn,echo,hoover,seek,ten,elf,pull,coil,push,mite,meet}/*.c | perl names.pl | sort | wc -l
+# ls -1 ../{air,hest,biff,nrrd,ell,unrrdu,alan,moss,tijk,gage,dye,bane,limn,echo,hoover,seek,ten,elf,pull,coil,push,mite,meet}/*.c | perl names.pl | sort | uniq | wc -l
 #
 # and then make sure the two numbers are the same.
 
