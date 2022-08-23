1e62a713fd1e81290942f7ed749ee9aad66b295f
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2022-08-23 09:47:37 +0000

    fixing preambles, changing single decrement to while loop (starting work on finishing cubic spline fitting)
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6789 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/limn/splineFit.c
+++ src/limn/splineFit.c
@@ -4,21 +4,19 @@
   Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
   Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public License
-  (LGPL) as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-  The terms of redistributing and/or modifying this software also
-  include exceptions to the LGPL that facilitate static linking.
+  This library is free software; you can redistribute it and/or modify it under the terms
+  of the GNU Lesser General Public License (LGPL) as published by the Free Software
+  Foundation; either version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also include exceptions to
+  the LGPL that facilitate static linking.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+  This library is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public License
-  along with this library; if not, write to Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  You should have received a copy of the GNU Lesser General Public License along with
+  this library; if not, write to Free Software Foundation, Inc., 51 Franklin Street,
+  Fifth Floor, Boston, MA 02110-1301 USA
 */
 
 #include "limn.h"
@@ -111,7 +109,8 @@ pntNum(const limnPoints *lpnt, uint loi, uint hii) {
 static const double *
 pntCrd(const limnPoints *lpnt, uint loi, uint ii) {
   uint jj = loi + ii;
-  if (jj >= lpnt->num) jj -= lpnt->num;
+  while (jj >= lpnt->num)
+    jj -= lpnt->num;
   return PP(lpnt) + 2 * jj;
 }
 
