a6a185af7cec36ea5d04c4969fc9daf0d55d9dcb
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2022-10-12 03:52:25 +0000

    black formatting
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6848 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/_util/buildBiffDict.py
+++ src/_util/buildBiffDict.py
@@ -20,24 +20,25 @@
 # Fifth Floor, Boston, MA 02110-1301 USA
 
 # halt if python2; thanks to https://preview.tinyurl.com/44f2beza
-_x,*_y=1,2 # NOTE: A SyntaxError here means you need python3, not python2
+import re
+import argparse
+import os
+_x, *_y = 1, 2  # NOTE: A SyntaxError here means you need python3, not python2
 del _x, _y
 
-import os
-import argparse
-import re
 
 verbose = 1
 # TEEM_LIB_LIST
-tlibs = [ # 'air', 'hest', 'biff',  (these do not use biff)
-'nrrd', 'ell', 'moss', 'unrrdu', 'alan', 'tijk', 'gage',
-'dye', 'bane', 'limn', 'echo', 'hoover', 'seek', 'ten',
-'elf', 'pull', 'coil', 'push', 'mite', 'meet']
+tlibs = [  # 'air', 'hest', 'biff',  (these do not use biff)
+    'nrrd', 'ell', 'moss', 'unrrdu', 'alan', 'tijk', 'gage',
+    'dye', 'bane', 'limn', 'echo', 'hoover', 'seek', 'ten',
+    'elf', 'pull', 'coil', 'push', 'mite', 'meet']
+
 
 def argsCheck(tPath):
     if (not (os.path.isdir(tPath)
-            and os.path.isdir(f'{tPath}/arch')
-            and os.path.isdir(f'{tPath}/src') )):
+             and os.path.isdir(f'{tPath}/arch')
+             and os.path.isdir(f'{tPath}/src'))):
         raise Exception(f'Need {tPath} to be dir with "arch" and "src" subdirs')
 
 # the task is: given the string representation of the error return test value
@@ -47,13 +48,15 @@ def argsCheck(tPath):
 # This assumes that the user of our output has:
 # - imported _teem (for enum values)
 # - imported math as _math (for isnan)
+
+
 def rvtest(typ, tv, rvName):
     ret = None
     if 'int' in typ:
         try:
             vv = int(tv)
             ret = f'{vv} == {rvName}'
-        except ValueError: # int() conversion failed
+        except ValueError:  # int() conversion failed
             # going to take wild-ass assumption that this is an enum (e.g. hooverErrInit)
             ret = f'{"_teem.lib." + tv} == {rvName}'
             # HEY you know we ourselves could import _teem here and check this assumption ...
@@ -66,6 +69,7 @@ def rvtest(typ, tv, rvName):
         raise Exception(f'sorry don\'t yet know how to handle typ={typ}, tv={tv}')
     return ret
 
+
 def doAnnote(oFile, funcName, retQT, annoteCmt, fnln):
     qts = retQT.split(' ')
     # remove any comment within annotation
@@ -73,7 +77,7 @@ def doAnnote(oFile, funcName, retQT, annoteCmt, fnln):
         annote = annoteCmt.split('#')[0].strip()
     else:
         annote = annoteCmt
-    anlist = annote[6:].split(' ') # remove "Biff: ", and split into list
+    anlist = annote[6:].split(' ')  # remove "Biff: ", and split into list
     # For Python-wrapping the Teem API, we're ignoring some things:
     if 'static' in qts:
         # function not accessible anyway in the libteem library
@@ -90,16 +94,17 @@ def doAnnote(oFile, funcName, retQT, annoteCmt, fnln):
         raise Exception(f'got multiple words {anlist} (from "{annoteCmt}") but expected 1')
     anval = anlist[0]
     if anval.startswith('maybe:'):
-        mlist = anval.split(':') # mlist[0]='maybe:'
-        mubi = mlist[1] # for Maybe, useBiff index (1-based)
-        anval = mlist[2] # returned value(s) to check
+        mlist = anval.split(':')  # mlist[0]='maybe:'
+        mubi = mlist[1]  # for Maybe, useBiff index (1-based)
+        anval = mlist[2]  # returned value(s) to check
     else:
         mubi = 0
     if not (match := re.match(r'_*(.+?)[^a-z]', funcName)):
         raise Exception(f'couldn\'t extract library name from function name "{funcName}"')
     bkey = match.group(1)
     if not bkey in tlibs:
-        raise Exception(f'apparent library name prefix "{bkey}" of function "{funcName}" not in Teem library list {tlibs}')
+        raise Exception(
+            f'apparent library name prefix "{bkey}" of function "{funcName}" not in Teem library list {tlibs}')
     # ** NOTE: this is all very specific to the Python CFFI wrapper in
     # ** teem/python/cffi/teem.py. But the code above is useful logic
     # ** for anything seeking to use the Biff annotations, and wanting
@@ -123,6 +128,7 @@ def doAnnote(oFile, funcName, retQT, annoteCmt, fnln):
     # finally, write dict entry for handling errors in funcName
     print(f'    \'{funcName}\': ({rvtf}, {mubi}, b\'{bkey}\', \'{fnln}\'),', file=oFile)
 
+
 def doSrc(oFile, sFile, sFN):
     lines = [line.strip() for line in sFile.readlines()]
     nlen = len(lines)
@@ -139,6 +145,7 @@ def doSrc(oFile, sFile, sFN):
             doAnnote(oFile, funcName, retQT, annote, f'{sFN}:{idx}')
         idx += 1
 
+
 def doLib(oFile, tPath, lib):
     sPath = f'{tPath}/src/{lib}'
     # read the CMakeLists.txt file to get list of source files
@@ -153,12 +160,13 @@ def doLib(oFile, tPath, lib):
         with open(f'{sPath}/{FN}') as sFile:
             doSrc(oFile, sFile, f'{lib}/{FN}')
 
+
 def parse_args():
     # https://docs.python.org/3/library/argparse.html
     parser = argparse.ArgumentParser(description='Scans "Biff:" annotations, and generates '
-                                    'a Python dict representing the same information',
-                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-                                    # formatter_class=argparse.RawTextHelpFormatter)
+                                     'a Python dict representing the same information',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-v', metavar='verbosity', type=int, default=1, required=False,
                         help='verbosity level (0 for silent)')
     parser.add_argument('-o', metavar='outfile', default='../../python/cffi/teemPySrc/biffDict.py',
@@ -168,6 +176,7 @@ def parse_args():
     # we always do all the libraries (that might use biff)
     # regardless of "experimental"
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
