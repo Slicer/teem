e6efb2b41260b7e74dda4377c4ea15e5ab9046e6
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2023-06-07 22:36:20 +0000

    refactored many things out of build_teem.py and into exult.py
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6883 3d70eeeb-363e-0410-a505-8a46323a89f2

--- python/cffi/build_teem.py
+++ python/cffi/build_teem.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Teem: Tools to process and visualize scientific data and images
-# Copyright (C) 2009--2022  University of Chicago
+# Copyright (C) 2009--2023  University of Chicago
 # Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
 # Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
 #
@@ -26,11 +26,10 @@ module that wraps around libteem. Run "build_teem.py --help" for more info.
 GLK welcomes suggestions on how to refactor to avoid too-many-branches and too-many-statements
 warnings from from pylint.
 """
-import os
-import sys
 import argparse
 import re
-import cffi
+
+import exult
 
 # halt if python2; thanks to https://preview.tinyurl.com/44f2beza
 _x, *_y = 1, 2  # NOTE: A SyntaxError here means you need python3, not python2
@@ -48,113 +47,6 @@ del _x, _y
 
 # global control on verbosity level
 VERB = 0
-
-# Array of dicts (object literals) to list all the Teem libraries
-# air hest biff nrrd ell moss unrrdu alan tijk gage dye bane
-# limn echo hoover seek ten elf pull coil push mite meet
-# (TEEM_LIB_LIST)
-LIBS = [
-    {'name': 'air', 'expr': False},  # (don't need airExistsConf.h)
-    {'name': 'hest', 'expr': False},
-    {'name': 'biff', 'expr': False},
-    {'name': 'nrrd', 'expr': False},  # also need: nrrdEnums.h nrrdDefines.h
-    {'name': 'ell', 'expr': False},  # (don't need ellMacros.h)
-    {'name': 'moss', 'expr': False},
-    {'name': 'unrrdu', 'expr': False},
-    {'name': 'alan', 'expr': True},
-    {'name': 'tijk', 'expr': True},
-    {'name': 'gage', 'expr': False},
-    {'name': 'dye', 'expr': False},
-    {'name': 'bane', 'expr': True},
-    {'name': 'limn', 'expr': False},
-    {'name': 'echo', 'expr': False},
-    {'name': 'hoover', 'expr': False},
-    {'name': 'seek', 'expr': False},
-    {'name': 'ten', 'expr': False},
-    {'name': 'elf', 'expr': True},
-    {'name': 'pull', 'expr': False},
-    {'name': 'coil', 'expr': True},
-    {'name': 'push', 'expr': True},
-    {'name': 'mite', 'expr': False},
-    {'name': 'meet', 'expr': False},
-]
-
-
-def add_extra_nrrd_h(hdrs: list[str]) -> list[str]:
-    """Supplements list of header files with extra headers for nrrd"""
-    idx = hdrs.index('nrrd.h')
-    if idx >= 0:
-        # other headers are needed to define nrrd library API
-        hdrs.insert(idx, 'nrrdDefines.h')
-        hdrs.insert(idx, 'nrrdEnums.h')
-    return hdrs
-
-
-def check_hdr_path(hdr_path: str):
-    """
-    Sanity check on include path hdr_path.
-    Returns (exper, have_hdrs) where exper indicates if this was run on an "experimental"
-    Teem build, and have_hdrs is the list of .h headers to process.
-    """
-    itpath = hdr_path + '/teem'
-    if not os.path.isdir(itpath):
-        raise Exception(f'Need {itpath} to be directory')
-    base_lib_names = [L['name'] for L in filter(lambda L: not L['expr'], LIBS)]
-    base_hdrs = add_extra_nrrd_h([f'{LN}.h' for LN in base_lib_names])
-    expr_lib_names = [L['name'] for L in filter(lambda L: L['expr'], LIBS)]
-    expr_hdrs = [f'{LN}.h' for LN in expr_lib_names]
-    missing_hdrs = list(filter(lambda F: not os.path.isfile(f'{itpath}/{F}'), base_hdrs))
-    if missing_hdrs:
-        raise Exception(
-            f'Missing header(s) {" ".join(missing_hdrs)} in {itpath} '
-            'for one or more of the core Teem libs'
-        )
-    missing_expr_hdrs = list(filter(lambda F: not os.path.isfile(f'{itpath}/{F}'), expr_hdrs))
-    have_hdrs = base_hdrs
-    if missing_expr_hdrs:
-        # missing one or more of the non-core "Experimental" header files
-        if len(missing_expr_hdrs) < len(expr_hdrs):
-            raise Exception(
-                'Missing some (but not all) non-core header(s) '
-                f'{" ".join(missing_expr_hdrs)} in {itpath} for one or more of the '
-                'core Teem libs'
-            )
-        # else len(missing_expr_hdrs) == len(expr_hdrs)) aka all missing, ok, so
-        # not Experimental
-        if VERB:
-            print('(Teem build does *not* appear include "Experimental" libraries)')
-    else:
-        # it is Experimental; reform the header list in dependency order (above)
-        have_hdrs = add_extra_nrrd_h([f"{L['name']}.h" for L in LIBS])
-        if VERB:
-            print('(Teem build includes "Experimental" libraries)')
-    return (not missing_expr_hdrs, have_hdrs)
-
-
-def check_lib_path(lib_path: str) -> None:
-    """
-    Sanity checks on libaray lib_path.
-    May throw various exceptions but returns nothing.
-    """
-    if sys.platform == 'darwin':  # Mac
-        shext = 'dylib'
-    elif sys.platform == 'linux':
-        shext = 'so'
-    else:
-        raise Exception(
-            'Sorry, currently only know how work on Mac and Linux ' '(not {sys.platform})'
-        )
-    lib_fnames = os.listdir(lib_path)
-    if not lib_fnames:
-        raise Exception(f'Teem library dir {lib_path} seems empty')
-    ltname = f'libteem.{shext}'
-    if not ltname in lib_fnames:
-        raise Exception(
-            f'Teem library dir {lib_path} contents {lib_fnames} do not seem to include '
-            f'required {ltname} shared library, which means running '
-            'cffi.FFI().compile() later will not produce a working wrapper, even if '
-            'it finishes without error.'
-        )
 
 
 def proc_line(line: str) -> str:
@@ -294,7 +186,7 @@ def proc_hdr(fout, fin, hname: str) -> None:  # out, fin: files
         # air.h has a set of 15 lines around airLLong, airULLong typedefs
         # this currently signals it's start, though (HEY) seems fragile
         idx = drop_at(
-            '#if defined(_WIN32) && !defined(__CYGWIN__) ' '&& !defined(__MINGW32__)', 15, lines
+            '#if defined(_WIN32) && !defined(__CYGWIN__) && !defined(__MINGW32__)', 15, lines
         )
         # restore definitions that seem to work for mac/linux
         lines.insert(idx, 'typedef signed long long airLLong;')
@@ -379,76 +271,28 @@ def proc_hdr(fout, fin, hname: str) -> None:  # out, fin: files
         fout.write(f'{line}\n')
 
 
-def build(path: str):
+def cdef_write(cdef_path: str, hdr_path: str, libs: list[str]) -> None:
     """
-    Main function: creates cdef_teem.h then feeds it to cffi.FFI() to compile the
-    _teem Python module that links into the libteem shared library.
+    creates cdef/cdef_LIB.h for all LIB in this Teem installation
     """
-    path = path.rstrip('/')
-    hdr_path = path + '/include'
-    lib_path = path + '/lib'
-    if not os.path.isdir(hdr_path) or not os.path.isdir(lib_path):
-        raise Exception(f'Need both {hdr_path} and {lib_path} to be subdirs of teem install dir')
-    check_lib_path(lib_path)
-    (exper, hdrs) = check_hdr_path(hdr_path)
-    if VERB:
-        print('#################### writing cdef_teem.h ...')
-    with open('cdef_teem.h', 'w', encoding='utf-8') as out:
-        out.write(
-            """
-/* NOTE: This file (cdef_teem.h) is generated by build_teem.py.
- * It is NOT usable as a single "teem.h" header for all of Teem, because of
- * the many hacky transformations done to work with the limitations of the
- * CFFI C parser, specifically, lacking C pre-processor (e.g., all #include
- * directives have been removed, and lots of other #defines are gone).
- * The top-level header for all of Teem is teem/meet.h */
+    for lib in libs:
+        if VERB:
+            print(f'#################### writing cdef/cdef_{lib}.h ...')
+        with open(f'{cdef_path}/cdef_{lib}.h', 'w', encoding='utf-8') as out:
+            out.write(
+                f"""
+/* NOTE: This file is a *very* hacked up version of the original
+teem/{lib}.h, generated by build_teem.py to declare the {lib}
+library contents to CFFI, within its many limitations, specifically lacking
+a C pre-processor (so no #include directives, and only certain #defines). */
  """
-        )
-        for hdr in hdrs:
-            out.write(f'/* =========== {hdr} =========== */\n')
-            with open(f'{hdr_path}/teem/{hdr}', 'r', encoding='utf-8') as hfin:
-                proc_hdr(out, hfin, hdr)
-            out.write('\n\n')
-    ffibld = cffi.FFI()
-    # so that teem.py can call free() as part of biff error handling
-    ffibld.cdef('extern void free(void *);')
-    if VERB:
-        print('#################### reading cdef_teem.h ...')
-    with open('cdef_teem.h', 'r', encoding='utf-8') as file:
-        ffibld.cdef(file.read())
-    source_args = {
-        'libraries': ['teem'],
-        'include_dirs': [hdr_path],
-        'library_dirs': [lib_path],
-        'extra_compile_args': ['-DTEEM_BUILD_EXPERIMENTAL_LIBS'] if exper else None,
-        # On linux, path <dir> here is passed to -Wl,--enable-new-dtags,-R<dir>;
-        # "readelf -d ....so | grep PATH" should show <dir> and "ldd .....so" should show
-        # where dependencies were found.
-        # On Mac, path <dir> here is passed to -Wl,-rpath,<dir>, and you can see that
-        # from "otool -l ....so", in the LC_RPATH sections.
-        'runtime_library_dirs': [os.path.abspath(lib_path)],
-        # keep asserts()
-        # https://docs.python.org/3/distutils/apiref.html#distutils.core.Extension
-        'undef_macros': ['NDEBUG'],
-    }
-    if VERB:
-        print('#################### calling set_source with ...')
-        for key, val in source_args.items():
-            print(f'   {key} = {val}')
-    ffibld.set_source(
-        '_teem', '#include <teem/meet.h>', **source_args  # this is effectively teem.h
-    )
-    if VERB:
-        print('#################### compiling _teem (slow!) ...')
-    out_path = ffibld.compile(verbose=(VERB > 0))
-    if VERB:
-        print(f'#################### ... compiling _teem done; created:\n{out_path}')
-    # should have now created a new _teem.<platform>.so shared library
-    # so should be able to, on Mac, (e.g.) "otool -L _teem.cpython-39-darwin.so"
-    # or, on linux, (e.g.) "ldd _teem.cpython-38-x86_64-linux-gnu.so"
-    # to confirm that this want to dynamically link to the libteem shared library
-    # (something about the mac build process allows this to work even if -Wl,-rpath
-    # is not passed to set_source, but this seems necessary on linux)
+            )
+            for idx, hdr in enumerate(exult.lib_headers(lib)):
+                if idx:
+                    out.write('\n\n')
+                out.write(f'/* =========== {hdr} =========== */\n')
+                with open(f'{hdr_path}/teem/{hdr}', 'r', encoding='utf-8') as hfin:
+                    proc_hdr(out, hfin, hdr)
 
 
 def parse_args():
@@ -478,4 +322,26 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
     VERB = args.v
-    build(args.install_path)
+    (_hdr_path, _lib_path, _have_libs, _) = exult.check_path(args.install_path)
+    cdef_write('cdef', _hdr_path, _have_libs)
+    (ffibld, source_args) = exult.ffi_builder('cdef', _hdr_path, _lib_path, 'meet', VERB)
+    if VERB:
+        print('#################### calling set_source with ...')
+        for key, val in source_args.items():
+            print(f'   {key} = {val}')
+    ffibld.set_source(
+        '_teem',  # name of extension module
+        '#include <teem/meet.h>',  # this is effectively teem.h
+        **source_args,
+    )
+    if VERB:
+        print('#################### compiling _teem (slow!) ...')
+    out_path = ffibld.compile(verbose=(VERB > 0))
+    if VERB:
+        print(f'#################### ... compiling _teem done; created:\n{out_path}')
+    # should have now created a new _teem.<platform>.so shared library
+    # so should be able to, on Mac, (e.g.) "otool -L _teem.cpython-39-darwin.so"
+    # or, on linux, (e.g.) "ldd _teem.cpython-38-x86_64-linux-gnu.so"
+    # to confirm that this want to dynamically link to the libteem shared library
+    # (something about the mac build process allows this to work even if -Wl,-rpath
+    # is not passed to set_source, but this seems necessary on linux)
--- python/cffi/exult.py
+++ python/cffi/exult.py
@@ -18,26 +18,343 @@
 # this library; if not, write to Free Software Foundation, Inc., 51 Franklin Street,
 # Fifth Floor, Boston, MA 02110-1301 USA
 """
-exult.py: CFFI *EX*tension module *U*tilities for *L*ibraries depending on *T*eem.
-When creating CFFI extension modules for libraries that depend on Teem (and for
-Teem itself), some wrapping of the extension module helps it be more Pythonic,
-and more useful. The two main kinds of wrapping are:
+exult.py: CFFI *EX*tension module *U*tilities for *L*ibraries depending on *T*eem. Contains two
+kinds of things related to CFFI extension modules for libraries that depend on Teem (including
+Teem itself): creating extension modules, and using them.
+
+(1) Compiling extension modules involves checks on paths, and knowing which Teem libs
+are dependencies of a given single Teem lib, in order to know which cdef headers to read in.
+This functionality used to be in build_teem.py (which generates the _teem CFFI extension module),
+but it is useful for the building of other Teem-using extension modules.
+
+(2) Using extension modules that depend on Teem benefits from some wrapping:
 * for functions that use biff, wrap it in something that detects the biff error,
   and turn the biff error message into a Python exception
 * for airEnums, make a Python object with useful int<->string conversion methods
-This functionality used to be in teem.py (the wrapper around the _teem CFFI
-extension module), but then other Teem-based extension modules would have to
-copy-paste it. Now, anything wrapping a CFFI extension module with .lib elements
-that are biff-using functions or airEnums (including teem.py itself), can use
-exult.py to generate wrappers for those objects.
+This functionality used to be in teem.py (the wrapper around the _teem CFFI extension module),
+but then other Teem-based extension modules would have to copy-paste it. Now, anything wrapping
+a CFFI extension module with .lib elements that are biff-using functions or airEnums (including
+teem.py itself), can use exult.py to generate wrappers for those objects.
+
+The use cases for (1) and (2) above are in fact distinct: (1) is done once on a machine, (2) is
+repeatedly done with every program execution.  Still, for now it is nice to keep information
+about Teem localized to this single file.
 """
 
-import math as _math  # for isnan test that may appear in _BIFFDICT
+# import math as _math  # for isnan test that may appear in _BIFFDICT
 import sys as _sys
+import os as _os
+import cffi as _cffi
 
 # halt if python2; thanks to https://preview.tinyurl.com/44f2beza
 _x, *_y = 1, 2  # NOTE: A SyntaxError here means you need python3, not python2
 del _x, _y
+
+_tlibs = {
+    'air': {'expr': False, 'deps': []},  # (don't need airExistsConf.h)
+    'hest': {'expr': False, 'deps': ['air']},
+    'biff': {'expr': False, 'deps': ['air']},
+    'nrrd': {
+        'expr': False,
+        'deps': ['biff', 'hest', 'air'],
+        'hdrs': ['nrrdEnums.h', 'nrrdDefines.h'],
+    },
+    'ell': {
+        'expr': False,
+        'deps': ['nrrd', 'biff', 'air'],
+        # ellMacros.h does not add to ell API
+    },
+    'moss': {
+        'expr': False,
+        'deps': ['ell', 'nrrd', 'biff', 'hest', 'air'],
+    },
+    'unrrdu': {
+        'expr': False,
+        'deps': ['nrrd', 'hest', 'biff', 'air'],
+        # moss needed for linking (because of unu ilk) but not for declaring unrrdu API
+    },
+    'alan': {
+        'expr': True,
+        'deps': ['ell', 'nrrd', 'biff', 'air'],
+    },
+    'tijk': {
+        'expr': True,
+        'deps': ['ell', 'nrrd', 'air'],
+    },
+    'gage': {
+        'expr': False,
+        'deps': ['ell', 'nrrd', 'biff', 'air'],
+    },
+    'dye': {
+        'expr': False,
+        'deps': ['ell', 'biff', 'air'],
+        # may not actually need ell; implementation of dye needs ellMacros.h
+    },
+    'bane': {
+        'expr': True,
+        'deps': ['gage', 'unrrdu', 'nrrd', 'biff', 'air'],
+    },
+    'limn': {
+        'expr': False,
+        'deps': ['gage', 'ell', 'unrrdu', 'nrrd', 'biff', 'hest', 'air'],
+    },
+    'echo': {
+        'expr': False,
+        'deps': ['limn', 'ell', 'nrrd', 'biff', 'air'],
+    },
+    'hoover': {
+        'expr': False,
+        'deps': ['limn', 'ell', 'nrrd', 'biff', 'air'],
+    },
+    'seek': {
+        'expr': False,
+        'deps': ['gage', 'limn', 'ell', 'nrrd', 'biff', 'hest', 'air'],
+    },
+    'ten': {
+        'expr': False,
+        'deps': ['echo', 'limn', 'gage', 'dye', 'unrrdu', 'ell', 'nrrd', 'biff', 'air'],
+    },
+    'elf': {
+        'expr': True,
+        'deps': ['ten', 'tijk', 'limn', 'ell', 'nrrd', 'air'],
+    },
+    'pull': {
+        'expr': False,
+        'deps': ['ten', 'limn', 'gage', 'ell', 'nrrd', 'biff', 'hest', 'air'],
+    },
+    'coil': {
+        'expr': True,
+        'deps': ['ten', 'ell', 'nrrd', 'biff', 'air'],
+    },
+    'push': {
+        'expr': True,
+        'deps': ['ten', 'gage', 'ell', 'nrrd', 'biff', 'air'],
+    },
+    'mite': {
+        'expr': False,
+        'deps': ['ten', 'hoover', 'limn', 'gage', 'ell', 'nrrd', 'biff', 'air'],
+    },
+    'meet': {
+        'expr': False,
+        'deps': [
+            'air',
+            'hest',
+            'biff',
+            'nrrd',
+            'ell',
+            'moss',
+            'unrrdu',
+            'alan',
+            'tijk',
+            'gage',
+            'dye',
+            'bane',
+            'limn',
+            'echo',
+            'hoover',
+            'seek',
+            'ten',
+            'elf',
+            'pull',
+            'coil',
+            'push',
+            'mite',
+        ],
+    },
+}
+
+### (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1)
+### (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1) (1)
+###
+### Things useful for *compiling* Teem-based extension modules
+
+
+def lib_all() -> list[str]:
+    """
+    Returns list of all Teem libraries in dependency order
+    """
+    return list(_tlibs.keys())
+
+
+def lib_experimental(lib: str) -> bool:
+    """
+    Answers if a given Teem library is "experimental"
+    """
+    try:
+        info = _tlibs[lib]
+    except Exception as exc:
+        raise RuntimeError(f'{lib} is not a known Teem library') from exc
+    return info['expr']
+
+
+def lib_depends(lib: str) -> list[str]:
+    """
+    Computes dependency expansion of given Teem library
+    """
+    try:
+        info = _tlibs[lib]
+    except Exception as exc:
+        raise RuntimeError(f'{lib} is not a known Teem library') from exc
+    # iteratively find all dependencies and dependencies of dependencies, etc
+    oldd = set()   # all previously dependencies known
+    newd = set([lib]) | set(info['deps'])   # newly discovered dependencies
+    while oldd != newd:
+        # while new dependencies were just discovered
+        tmpd = set()
+        for nlb in newd:
+            tmpd = tmpd | set([lib]) | set(_tlibs[nlb]['deps'])
+        oldd = newd
+        newd = tmpd
+    tla = lib_all()   # linear array of all libs in dependency order
+    # return dependencies sorted in dependency order
+    return sorted(list(newd), key=tla.index)
+
+
+def lib_headers(lib: str) -> list[str]:
+    """
+    Returns list of headers (installed by CMake's "make install" the declares the API of given
+    Teem library. This is really only the business of things (like build_teem.py) that do the
+    one-time generation of cdef headers to be later consumed by CFFI, rather than things that
+    use Teem via its python wrappers, but it is nice to have the info about Teem libraries
+    centralized to this file.
+    """
+    try:
+        info = _tlibs[lib]
+    except Exception as exc:
+        raise RuntimeError(f'{lib} is not a known Teem library') from exc
+    ret = info['hdrs'].copy() if 'hdrs' in info else []
+    ret += [f'{lib}.h']
+    return ret
+
+
+def check_lib_path(lib_path: str) -> None:
+    """
+    Sanity checks on Teem install "lib" path lib_path.
+    May throw various exceptions but returns nothing.
+    """
+    if _sys.platform == 'darwin':  # Mac
+        shext = 'dylib'
+    elif _sys.platform == 'linux':
+        shext = 'so'
+    else:
+        raise Exception(
+            'Sorry, currently only know how work on Mac and Linux (not {_sys.platform})'
+        )
+    lib_fnames = _os.listdir(lib_path)
+    if not lib_fnames:
+        raise Exception(f'Teem library dir {lib_path} seems empty')
+    ltname = f'libteem.{shext}'
+    if not ltname in lib_fnames:
+        raise Exception(
+            f'Teem library dir {lib_path} contents {lib_fnames} do not seem to include '
+            f'required {ltname} shared library, which means running '
+            'cffi.FFI().compile() later will not produce a working wrapper, even if '
+            'it finishes without error.'
+        )
+
+
+def check_hdr_path(hdr_path: str):
+    """
+    Sanity check on include path hdr_path.
+    Returns (exper, have_libs) where exper indicates if this was run on an "experimental"
+    Teem build, and have_libs is the list of libraries for which the .h headers are present
+    """
+    itpath = hdr_path + '/teem'
+    if not _os.path.isdir(itpath):
+        raise Exception(f'Need {itpath} to be directory')
+    all_libs = lib_all()
+    base_libs = list(filter(lambda L: not lib_experimental(L), all_libs))
+    expr_libs = list(filter(lib_experimental, all_libs))
+    base_hdrs = sum([lib_headers(L) for L in base_libs], [])
+    expr_hdrs = sum([lib_headers(L) for L in expr_libs], [])
+    missing_hdrs = list(filter(lambda F: not _os.path.isfile(f'{itpath}/{F}'), base_hdrs))
+    if missing_hdrs:
+        raise Exception(
+            f'Missing header(s) {" ".join(missing_hdrs)} in {itpath} '
+            'for one or more of the core Teem libs'
+        )
+    have_libs = base_libs
+    missing_expr_hdrs = list(filter(lambda F: not _os.path.isfile(f'{itpath}/{F}'), expr_hdrs))
+    if missing_expr_hdrs:
+        # missing one or more of the non-core "Experimental" header files
+        if len(missing_expr_hdrs) < len(expr_hdrs):
+            raise Exception(
+                'Missing some (but not all) non-core header(s) '
+                f'{" ".join(missing_expr_hdrs)} in {itpath} for one or more of the '
+                'core Teem libs'
+            )
+        # else len(missing_expr_hdrs) == len(expr_hdrs)) aka all missing, ok, so
+        # not Experimental
+    else:
+        # it is Experimental; reform the header list in dependency order (above)
+        have_libs = all_libs
+    return (not missing_expr_hdrs, have_libs)
+
+
+def check_path(path: str):
+    """
+    Calls check_lib_path and check_hdr_path on given install path "path"
+    """
+    path = path.rstrip('/')
+    hdr_path = path + '/include'
+    lib_path = path + '/lib'
+    if not _os.path.isdir(hdr_path) or not _os.path.isdir(lib_path):
+        raise Exception(f'Need both {hdr_path} and {lib_path} to be subdirs of teem install dir')
+    check_lib_path(lib_path)
+    (exper, have_libs) = check_hdr_path(hdr_path)
+    return (hdr_path, lib_path, have_libs, exper)
+
+
+def ffi_builder(cdef_path: str, hdr_path: str, lib_path: str, top_lib: str, verb: bool):
+    """
+    Sets up a builder instance of cffi.FFI() by feeding it all the cdef headers needed for
+    library top_lib in Teem, and creates an args dict for builder.set_source. The last steps
+    for the caller to do are call builder.set_source and .compile.
+    """
+    # yes, this is probably redundant with something the caller has already done,
+    # but it would be more annoying to pass back (to here) this information, and
+    # we need to do error checking anyway
+    (exper, have_libs) = check_hdr_path(hdr_path)
+    if not top_lib in have_libs:
+        raise Exception(f'Requested lib {top_lib} not part of this Teem build: {have_libs}')
+    # if 'meet' == top_lib, we're probably being called from build_teem.py, so we use the
+    # learned value of exper as part of setting up source_args. Else:
+    if 'meet' != top_lib:
+        # we set exper according to whether requested library is "experimental"
+        exper = lib_experimental(top_lib)
+    ffibld = _cffi.FFI()
+    # so that teem.py can call free() as part of biff error handling
+    ffibld.cdef('extern void free(void *);')
+    for lib in lib_depends(top_lib):
+        if verb:
+            print(f'#################### reading cdef_{lib}.h ...')
+        with open(f'{cdef_path}/cdef_{lib}.h', 'r', encoding='utf-8') as file:
+            ffibld.cdef(file.read())
+    # NOTE that the caller (if not build_teem.py) will need to add to the arrays:
+    # 'libraries': with name of new teem-using library X
+    # 'runtime_library_dirs': with path to shared library of X.{so,dylib}
+    source_args = {
+        'libraries': ['teem'],
+        'include_dirs': [hdr_path],
+        'library_dirs': [lib_path],
+        'extra_compile_args': ['-DTEEM_BUILD_EXPERIMENTAL_LIBS'] if exper else None,
+        # On linux, path <dir> here is passed to -Wl,--enable-new-dtags,-R<dir>;
+        # "readelf -d ....so | grep PATH" should show <dir> and "ldd .....so" should show
+        # where dependencies were found.
+        # On Mac, path <dir> here is passed to -Wl,-rpath,<dir>, and you can see that
+        # from "otool -l ....so", in the LC_RPATH sections.
+        'runtime_library_dirs': [_os.path.abspath(lib_path)],
+        # keep asserts()
+        # https://docs.python.org/3/distutils/apiref.html#distutils.core.Extension
+        'undef_macros': ['NDEBUG'],
+    }
+    return (ffibld, source_args)
+
+
+### (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2)
+### (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2) (2)
+###
+### Things useful for *wrapping* Teem-using extension module
 
 
 class Tenum:
@@ -49,7 +366,7 @@ class Tenum:
 
     def __init__(self, aenm, _name, xmdl):
         """Constructor takes a Teem airEnum pointer (const airEnum *const)."""
-        # xmdl: what extension module is this enum coming from; it it is not _teem itself, then
+        # xmdl: what extension module is this enum coming from; if it is not _teem itself, then
         # we want to know the module so as to avoid errors (when calling .ffi.string) like:
         # "TypeError: initializer for ctype 'airEnum *' appears indeed to be 'airEnum *', but
         # the types are different (check that you are not e.g. mixing up different ffi instances)"
