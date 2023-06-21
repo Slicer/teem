7b08bef36e23654930e4ed96ce8d4c76c1d89ba6
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2023-06-21 15:49:39 +0000

    syncing with source
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@6970 3d70eeeb-363e-0410-a505-8a46323a89f2

--- python/cffi/cdef/cdef_hest.h
+++ python/cffi/cdef/cdef_hest.h
@@ -6,30 +6,27 @@ CFFI, within its many limitations, specifically lacking a C pre-processor
  /* =========== hest.h =========== */
 /*
   Teem: Tools to process and visualize scientific data and images
-  Copyright (C) 2009--2019  University of Chicago
-  Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
-  Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public License
-  (LGPL) as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-  The terms of redistributing and/or modifying this software also
-  include exceptions to the LGPL that facilitate static linking.
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  You should have received a copy of the GNU Lesser General Public License
-  along with this library; if not, write to Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  Copyright (C) 2009--2023  University of Chicago
+  Copyright (C) 2005--2008  Gordon Kindlmann
+  Copyright (C) 1998--2004  University of Utah
+  This library is free software; you can redistribute it and/or modify it under the terms
+  of the GNU Lesser General Public License (LGPL) as published by the Free Software
+  Foundation; either version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also include exceptions to
+  the LGPL that facilitate static linking.
+  This library is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License along with
+  this library; if not, write to Free Software Foundation, Inc., 51 Franklin Street,
+  Fifth Floor, Boston, MA 02110-1301 USA
 */
 /*
 ******** hestSource* enum
 **
-** records whether the info to satisfy a particular option came
-** from the default or from the user (command-line or response file).
-** Distinguishing command-line from response file would take a
-** much more significant code restructuring
+** records whether the info to satisfy a particular option came from the default or from
+** the user (command-line or response file). Distinguishing command-line from response
+** file would take a much more significant code restructuring
 */
 enum {
   hestSourceUnknown, /* 0 */
@@ -40,30 +37,26 @@ enum {
 /*
 ******** hestCB struct
 **
-** for when the thing you want to parse from the command-line is not a
-** simple boolean, number, airEnum, or string.  hestParse() will not
-** allocate anything to store individual things, though it may
-** allocate an array in the case of a multiple variable parameter
-** option.  If your things are actually pointers to things, then you
-** do the allocation in the parse() callback.  In this case, you set
-** destroy() to be your "destructor", and it will be called on the
-** result of derefencing the argument to parse().
+** for when the thing you want to parse from the command-line is not a simple boolean,
+** number, airEnum, or string.  hestParse() will not allocate anything to store
+** individual things, though it may allocate an array in the case of a multiple
+** variable parameter option.  If your things are actually pointers to things, then you
+** do the allocation in the parse() callback.  In this case, you set destroy() to be
+** your "destructor", and it will be called on the result of derefencing the argument
+** to parse().
 */
 typedef struct {
   size_t size;      /* sizeof() one thing */
   const char *type; /* used by hestGlossary() to describe the type */
   int (*parse)(void *ptr, const char *str, char err[AIR_STRLEN_HUGE]);
-  /* how to parse one thing from a string.  This will
-     be called multiple times for multiple parameter
-     options.  A non-zero return value is considered
-     an error.  Error message go in the err string */
+  /* how to parse one thing from a string.  This will be called multiple times for
+     multiple parameter options.  A non-zero return value is considered an error.  Error
+     message goes in the err string */
   void *(*destroy)(void *ptr);
-  /* if non-NULL, this is the destructor that will be
-     called by hestParseFree() (or by hestParse() if
-     there is an error midway through parsing).  The
-     argument is NOT the same as passed to parse():
-     it is the result of dereferencing the argument
-     to parse() */
+  /* if non-NULL, this is the destructor that will be called by hestParseFree() (or by
+     hestParse() if there is an error midway through parsing).  The argument is NOT the
+     same as passed to parse(): it is the result of dereferencing the argument to parse()
+   */
 } hestCB;
 /*
 ******** hestOpt struct
@@ -75,45 +68,38 @@ typedef struct {
     *name;            /* simple description of option's parameter(s) */
   int type;           /* type of option (from airType enum) */
   unsigned int min;   /* min # of parameters for option */
-  int max;            /* max # of parameters for option */
+  int max;            /* max # of parameters for option,
+                         or -1 for "there is no max" */
   void *valueP;       /* storage of parsed values */
   char *dflt,         /* default value written out as string */
     *info;            /* description to be printed with "glossary" info */
   unsigned int *sawP; /* used ONLY for multiple variable parameter options
-                         (min < max > 2): storage of # of parsed values */
+                         (min < max >= 2): storage of # of parsed values */
   airEnum *enm;       /* used ONLY for airTypeEnum options */
   hestCB *CB;         /* used ONLY for airTypeOther options */
-  /* --------------------- end of user-defined fields; the following fields
-     must be set by hestParse() as part of its operation. This does prevent
-     adding const to much usage of the hestOpt */
-  int kind, /* what kind of option is this, based on min and max,
-               set by hestParse() (actually _hestPanic()),
-               later used by hestFree():
-               1: min == max == 0
-                  stand-alone flag; no parameters
-               2: min == max == 1
-                  single fixed parameter
-               3: min == max >= 2
-                  multiple fixed parameters
-               4: min == 0; max == 1;
-                  single variable parameter
-               5: max - min >= 1; max >= 2
-                  multiple variable parameters */
-    alloc;  /* information about whether flag is non-NULL, and what
-               parameters were used, that determines whether or
-               not memory was allocated by hestParse(); info
+  /* --------------------- end of user-defined fields; the following fields must be set
+     by hestParse() as part of its operation. This does prevent adding const to much
+     usage of the hestOpt */
+  int kind, /* what kind of option is this, based on min and max,set by hestParse()
+               (actually _hestPanic()), later used by hestFree():
+               1: min == max == 0       stand-alone flag; no parameters
+               2: min == max == 1       single fixed parameter
+               3: min == max >= 2       multiple fixed parameters
+               4: min == 0; max == 1;   single variable parameter
+               5: min < max; max >= 2   multiple variable parameters */
+    alloc;  /* information about whether flag is non-NULL, and what parameters were used,
+               that determines whether or  not memory was allocated by hestParse(); info
                later used by hestParseFree():
                0: no free()ing needed
-               1: free(*valueP), either because it is a single
-                  string, or because was a dynamically allocated
-                  array of non-strings
-               2: free((*valueP)[i]), because they are elements
-                  of a fixed-length array of strings
-               3: free((*valueP)[i]) and free(*valueP), because
-                  it is a dynamically allocated array of strings */
+               1: free(*valueP), either because it is a single string, or because was a
+                  dynamically allocated array of non-strings
+               2: free((*valueP)[i]), because they are elements of a fixed-length
+                  array of strings
+               3: free((*valueP)[i]) and free(*valueP), because it is a dynamically
+                  allocated array of strings */
   /* --------------------- Output */
-  int source; /* from the hestSource* enum; from whence was this
-                 information set, else hestSourceUnknown if not */
+  int source; /* from the hestSource* enum; from whence was this information learned,
+                 else hestSourceUnknown if not */
 } hestOpt;
 /*
 ******** hestParm struct
@@ -123,71 +109,63 @@ typedef struct {
 ** GK: Don't even think about storing per-parse state in here.
 */
 typedef struct {
-  int verbosity,                     /* verbose diagnostic messages to stdout */
-    respFileEnable,                  /* whether or not to use response files */
-    elideSingleEnumType,             /* if type is airTypeEnum, and if its a single fixed
-                                        parameter option, then don't bother printing the
-                                        type information as part of hestGlossary() */
-    elideSingleOtherType,            /* like above, but for airTypeOther */
-    elideSingleOtherDefault,         /* don't display default for single fixed
-                                        airTypeOther parameter */
-    elideSingleNonExistFloatDefault, /* if default for a single fixed floating
-                                        point (float or double) parameter
-                                        doesn't AIR_EXIST, then don't display
-                                        the default */
+  int verbosity,          /* verbose diagnostic messages to stdout */
+    respFileEnable,       /* whether or not to use response files */
+    elideSingleEnumType,  /* if type is airTypeEnum, and if it's a single fixed parameter
+                             option, then don't bother printing the  type information as
+                             part of hestGlossary() */
+    elideSingleOtherType, /* like above, but for airTypeOther */
+    elideSingleOtherDefault, /* don't display default for single fixed airTypeOther
+                                parameter */
+    elideSingleNonExistFloatDefault, /* if default for a single fixed floating point
+                                        (float or double) parameter doesn't AIR_EXIST,
+                                        then don't display the default */
     elideMultipleNonExistFloatDefault,
     elideSingleEmptyStringDefault, /* if default for a single string is empty
                                       (""), then don't display default */
     elideMultipleEmptyStringDefault,
-    noArgsIsNoProblem,      /* if non-zero, having no arguments to parse is not in
-                               and of itself a problem; this means that if all
-                               options have defaults, it would be *ok* to invoke
-                               the problem without any further command-line
-                               options. This is counter to pre-Teem-1.11
-                               behavior (for which no arguments *always* meant
-                               "show me usage info"). */
-    greedySingleString,     /* when parsing a single string, whether or not
-                               to be greedy (as per airParseStrS) */
-    cleverPluralizeOtherY,  /* when printing the type for airTypeOther, when
-                               the min number of items is > 1, and the type
-                               string ends with "y", then pluralize with
-                               "ies" instead of "ys" */
-    dieLessVerbose,         /* on parse failure, hestParseOrDie prints less
-                               than it otherwise might: only print info and
-                               glossary when they "ask for it" */
+    noArgsIsNoProblem,     /* if non-zero, having no arguments to parse is not in and
+                              of itself a problem; this means that if all options have
+                              defaults, it would be *ok* to invoke the problem without
+                              any further command-line options. This is counter to
+                              pre-Teem-1.11 behavior (for which no arguments *always*
+                              meant "show me usage info"). */
+    greedySingleString,    /* when parsing a single string, whether or not to be greedy
+                              (as per airParseStrS) */
+    cleverPluralizeOtherY, /* when printing the type for airTypeOther, when the min
+                              number of items is > 1, and the type string ends with "y",
+                              then pluralize with "ies" instead of "ys" */
+    dieLessVerbose, /* on parse failure, hestParseOrDie prints less than it otherwise
+                       might: only print info and glossary when they "ask for it" */
     noBlankLineBeforeUsage; /* like it says */
   unsigned int columns;     /* number of printable columns in output */
   char respFileFlag,        /* the character at the beginning of an argument
                                indicating that this is a response file name */
-    respFileComment,        /* comment character for the repose files */
-    varParamStopFlag,       /* prefixed by '-' to form the flag which signals
-                               the end of a flagged variable parameter option
-                               (single or multiple) */
-    multiFlagSep;           /* character in flag which signifies that there is
-                               a long and short version, and which separates
-                               the two.  Or, can be set to '\0' to disable this
-                               behavior entirely. */
+    respFileComment,        /* comment character for the response files */
+    varParamStopFlag, /* prefixed by '-' to form the flag which signals the end of a
+                         flagged variable parameter option (single or multiple) */
+    multiFlagSep;     /* character in flag which signifies that there is a long and short
+                         version, and which separates the two.  Or, can be set to '\0' to
+                         disable this behavior entirely. */
 } hestParm;
 /* defaultsHest.c */
-/* HEY: recent Teem conventions say these should all start with
-   "hestDef" not just "hest" */
-extern int hestVerbosity;
-extern int hestRespFileEnable;
-extern int hestElideSingleEnumType;
-extern int hestElideSingleOtherType;
-extern int hestElideSingleOtherDefault;
-extern int hestElideSingleNonExistFloatDefault;
-extern int hestElideMultipleNonExistFloatDefault;
-extern int hestElideSingleEmptyStringDefault;
-extern int hestElideMultipleEmptyStringDefault;
-extern int hestNoArgsIsNoProblem;
-extern int hestGreedySingleString;
-extern int hestCleverPluralizeOtherY;
-extern unsigned int hestColumns;
-extern char hestRespFileFlag;
-extern char hestRespFileComment;
-extern char hestVarParamStopFlag;
-extern char hestMultiFlagSep;
+extern int hestDefaultVerbosity;
+extern int hestDefaultRespFileEnable;
+extern int hestDefaultElideSingleEnumType;
+extern int hestDefaultElideSingleOtherType;
+extern int hestDefaultElideSingleOtherDefault;
+extern int hestDefaultElideSingleNonExistFloatDefault;
+extern int hestDefaultElideMultipleNonExistFloatDefault;
+extern int hestDefaultElideSingleEmptyStringDefault;
+extern int hestDefaultElideMultipleEmptyStringDefault;
+extern int hestDefaultNoArgsIsNoProblem;
+extern int hestDefaultGreedySingleString;
+extern int hestDefaultCleverPluralizeOtherY;
+extern unsigned int hestDefaultColumns;
+extern char hestDefaultRespFileFlag;
+extern char hestDefaultRespFileComment;
+extern char hestDefaultVarParamStopFlag;
+extern char hestDefaultMultiFlagSep;
 /* methodsHest.c */
 extern const int hestPresent;
 extern hestParm *hestParmNew(void);
--- python/cffi/cdef/cdef_nrrd.h
+++ python/cffi/cdef/cdef_nrrd.h
@@ -6,22 +6,20 @@ CFFI, within its many limitations, specifically lacking a C pre-processor
  /* =========== nrrdEnums.h =========== */
 /*
   Teem: Tools to process and visualize scientific data and images
-  Copyright (C) 2009--2021  University of Chicago
-  Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
-  Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public License
-  (LGPL) as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-  The terms of redistributing and/or modifying this software also
-  include exceptions to the LGPL that facilitate static linking.
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  You should have received a copy of the GNU Lesser General Public License
-  along with this library; if not, write to Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  Copyright (C) 2009--2023  University of Chicago
+  Copyright (C) 2005--2008  Gordon Kindlmann
+  Copyright (C) 1998--2004  University of Utah
+  This library is free software; you can redistribute it and/or modify it under the terms
+  of the GNU Lesser General Public License (LGPL) as published by the Free Software
+  Foundation; either version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also include exceptions to
+  the LGPL that facilitate static linking.
+  This library is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License along with
+  this library; if not, write to Free Software Foundation, Inc., 51 Franklin Street,
+  Fifth Floor, Boston, MA 02110-1301 USA
 */
 /*******
 ******** NONE of these enums should have values set explicitly in their
@@ -719,22 +717,20 @@ enum {
 /* =========== nrrdDefines.h =========== */
 /*
   Teem: Tools to process and visualize scientific data and images
-  Copyright (C) 2009--2022  University of Chicago
-  Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
-  Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public License
-  (LGPL) as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-  The terms of redistributing and/or modifying this software also
-  include exceptions to the LGPL that facilitate static linking.
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  You should have received a copy of the GNU Lesser General Public License
-  along with this library; if not, write to Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  Copyright (C) 2009--2023  University of Chicago
+  Copyright (C) 2005--2008  Gordon Kindlmann
+  Copyright (C) 1998--2004  University of Utah
+  This library is free software; you can redistribute it and/or modify it under the terms
+  of the GNU Lesser General Public License (LGPL) as published by the Free Software
+  Foundation; either version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also include exceptions to
+  the LGPL that facilitate static linking.
+  This library is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License along with
+  this library; if not, write to Free Software Foundation, Inc., 51 Franklin Street,
+  Fifth Floor, Boston, MA 02110-1301 USA
 */
 /* feel free to set these to higher values and recompile */
 #define NRRD_DIM_MAX       16
@@ -787,22 +783,20 @@ enum {
 /* =========== nrrd.h =========== */
 /*
   Teem: Tools to process and visualize scientific data and images
-  Copyright (C) 2009--2022  University of Chicago
-  Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
-  Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public License
-  (LGPL) as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-  The terms of redistributing and/or modifying this software also
-  include exceptions to the LGPL that facilitate static linking.
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  You should have received a copy of the GNU Lesser General Public License
-  along with this library; if not, write to Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  Copyright (C) 2009--2023  University of Chicago
+  Copyright (C) 2005--2008  Gordon Kindlmann
+  Copyright (C) 1998--2004  University of Utah
+  This library is free software; you can redistribute it and/or modify it under the terms
+  of the GNU Lesser General Public License (LGPL) as published by the Free Software
+  Foundation; either version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also include exceptions to
+  the LGPL that facilitate static linking.
+  This library is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License along with
+  this library; if not, write to Free Software Foundation, Inc., 51 Franklin Street,
+  Fifth Floor, Boston, MA 02110-1301 USA
 */
 /* ---- BEGIN non-NrrdIO */
 /* ---- END non-NrrdIO */
@@ -2200,7 +2194,7 @@ extern int nrrdKernelSpecSprint(char str[AIR_STRLEN_LARGE],
                                      const NrrdKernelSpec *ksp);
 extern int nrrdKernelSprint(char str[AIR_STRLEN_LARGE],
                                  const NrrdKernel *kernel,
-                                 const double *kparm);
+                                 const double kparm[NRRD_KERNEL_PARMS_NUM]);
 extern int nrrdKernelCompare(const NrrdKernel *kernA,
                                   const double parmA[NRRD_KERNEL_PARMS_NUM],
                                   const NrrdKernel *kernB,
