d6812255255cc6dc0040545edbcd2bd9da32fba1
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2023-07-22 05:11:12 +0000

    synching source
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@7140 3d70eeeb-363e-0410-a505-8a46323a89f2

--- python/cffi/cdef/cdef_seek.h
+++ python/cffi/cdef/cdef_seek.h
@@ -155,9 +155,9 @@ typedef struct {
                                             vertex strength seen (from probing slabs) */
   double time;                           /* time for extraction */
   double gradMagAvg; /* in case per-vertex gradients were computed (such as:
-                        isocontouring without a gageContext), then the average gradient
-                        magnitude over all the verts (useful for some computations
-                        involving signed distance functions) */
+                        isocontouring without a gageContext but with normalsFind on),
+                        then the average gradient magnitude over all the verts (useful
+                        for some computations involving signed distance functions) */
 } seekContext;
 /* enumsSeek.c */
 extern const char *const seekBiffKey;
