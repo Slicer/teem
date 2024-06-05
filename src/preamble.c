91cde81997c6a996e877c99b6699c67fca81a173
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2024-06-05 03:08:24 +0000

    happy new year
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@7161 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/preamble.c
+++ src/preamble.c
@@ -1,6 +1,6 @@
 /*
   Teem: Tools to process and visualize scientific data and images
-  Copyright (C) 2009--2023  University of Chicago
+  Copyright (C) 2009--2024  University of Chicago
   Copyright (C) 2005--2008  Gordon Kindlmann
   Copyright (C) 1998--2004  University of Utah
 
