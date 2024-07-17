f34067926d638a1818bfed65b67064fffd300195
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2024-07-17 17:54:47 +0000

    fixing uses of AIR_STRLEN_x plus clang-format
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@7217 3d70eeeb-363e-0410-a505-8a46323a89f2

--- Testing/gage/tsbp.c
+++ Testing/gage/tsbp.c
@@ -37,16 +37,17 @@ char *extraParm;
 gageStackBlurParm *sbp, *sbq;
 airArray *mop;
 char *err;
-char buff[AIR_STRLEN_LARGE];
+char buff[AIR_STRLEN_LARGE + 1];
 
 void
 parseFailOrDie(const char *str) {
-  static const char me[]="parseFailOrDie";
+  static const char me[] = "parseFailOrDie";
 
   fprintf(stderr, "%s(\"%s\"): ", me, str);
   if (!gageStackBlurParmParse(sbp, extraFlag, &extraParm, str)) {
     fprintf(stderr, "problem did NOT fail!\n");
-    airMopError(mop); exit(1);
+    airMopError(mop);
+    exit(1);
   }
   /* else good, we got an error */
   airMopAdd(mop, err = biffGetDone(GAGE), airFree, airMopAlways);
@@ -56,21 +57,22 @@ parseFailOrDie(const char *str) {
 
 void
 parseOrDie(const char *str) {
-  static const char me[]="parseOrDie";
+  static const char me[] = "parseOrDie";
   int differ;
-  char explain[AIR_STRLEN_LARGE]="!explain_not_set!";
+  char explain[AIR_STRLEN_LARGE + 1] = "!explain_not_set!";
 
   if (gageStackBlurParmParse(sbp, extraFlag, &extraParm, str)
       || gageStackBlurParmSprint(buff, sbp, extraFlag, extraParm)) {
     airMopAdd(mop, err = biffGetDone(GAGE), airFree, airMopAlways);
-    fprintf(stderr, "%s: failed to parse \"%s\" or then sprint: %s\n",
-            me, str, err);
-    airMopError(mop); exit(1);
+    fprintf(stderr, "%s: failed to parse \"%s\" or then sprint: %s\n", me, str, err);
+    airMopError(mop);
+    exit(1);
   }
   if (gageStackBlurParmParse(sbq, extraFlag, &extraParm, str)) {
     airMopAdd(mop, err = biffGetDone(GAGE), airFree, airMopAlways);
     fprintf(stderr, "%s: failed to parse \"%s\": %s\n", me, str, err);
-    airMopError(mop); exit(1);
+    airMopError(mop);
+    exit(1);
   }
   /* can tweak sbq here to make sure compare is working */
   /*
@@ -85,16 +87,16 @@ parseOrDie(const char *str) {
   nrrdBoundarySpecParse(sbp->bspec, "pad:3");
   nrrdBoundarySpecParse(sbq->bspec, "pad:2");
   */
-  if (gageStackBlurParmCompare(sbp, "first", sbq, "second",
-                               &differ, explain)) {
+  if (gageStackBlurParmCompare(sbp, "first", sbq, "second", &differ, explain)) {
     airMopAdd(mop, err = biffGetDone(GAGE), airFree, airMopAlways);
     fprintf(stderr, "%s: failed to compare: %s", me, err);
-    airMopError(mop); exit(1);
+    airMopError(mop);
+    exit(1);
   }
   if (differ) {
-    fprintf(stderr, "%s: two sbps from same string differ: %s\n",
-            me, explain);
-    airMopError(mop); exit(1);
+    fprintf(stderr, "%s: two sbps from same string differ: %s\n", me, explain);
+    airMopError(mop);
+    exit(1);
   }
   printf("%s: \"%s\" -> \"%s\"\n", me, str, buff);
   gageStackBlurParmInit(sbp);
@@ -102,13 +104,12 @@ parseOrDie(const char *str) {
   return;
 }
 
-static const char *sbpInfo =
-  "for testing handling of stack blur parms";
+static const char *sbpInfo = "for testing handling of stack blur parms";
 int
 main(int argc, const char **argv) {
   /* stock variables */
   const char *me;
-  hestOpt *hopt=NULL;
+  hestOpt *hopt = NULL;
   hestParm *hparm;
   /* variables specific to this program */
   gageStackBlurParm **sbpv;
@@ -118,19 +119,20 @@ main(int argc, const char **argv) {
   mop = airMopNew();
   hparm = hestParmNew();
   airMopAdd(mop, hparm, (airMopper)hestParmFree, airMopAlways);
-  hestOptAdd(&hopt, "sbp", "sbp0", airTypeOther, 1, -1, &sbpv, NULL,
-             "stack blur parms", &sbpNum, NULL, gageHestStackBlurParm);
-  hestParseOrDie(hopt, argc-1, argv+1, hparm, me, sbpInfo,
-                 AIR_TRUE, AIR_TRUE, AIR_TRUE);
+  hestOptAdd(&hopt, "sbp", "sbp0", airTypeOther, 1, -1, &sbpv, NULL, "stack blur parms",
+             &sbpNum, NULL, gageHestStackBlurParm);
+  hestParseOrDie(hopt, argc - 1, argv + 1, hparm, me, sbpInfo, AIR_TRUE, AIR_TRUE,
+                 AIR_TRUE);
   airMopAdd(mop, hopt, (airMopper)hestOptFree, airMopAlways);
   airMopAdd(mop, hopt, (airMopper)hestParseFree, airMopAlways);
 
   printf("%s: command line options: ---------- \n", me);
-  for (sbpIdx=0; sbpIdx<sbpNum; sbpIdx++) {
+  for (sbpIdx = 0; sbpIdx < sbpNum; sbpIdx++) {
     if (gageStackBlurParmSprint(buff, sbpv[sbpIdx], NULL, NULL)) {
       airMopAdd(mop, err = biffGetDone(GAGE), airFree, airMopAlways);
       fprintf(stderr, "%s: problem with sbpv[%u]:%s", me, sbpIdx, err);
-      airMopError(mop); return 1;
+      airMopError(mop);
+      return 1;
     }
     printf("%s: sbp[%u] = \"%s\"\n", me, sbpIdx, buff);
   }
