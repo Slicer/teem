fadca7126ef85624dcb482483b643d78f710af65
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2025-08-18 18:10:56 +0000

    with simplification of non-CMake build, much less error checking needed
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@7301 3d70eeeb-363e-0410-a505-8a46323a89f2

--- src/make/errorCheck.mk
+++ src/make/errorCheck.mk
@@ -1,6 +1,6 @@
 #
 # Teem: Tools to process and visualize scientific data and images
-# Copyright (C) 2009--2023  University of Chicago
+# Copyright (C) 2009--2025  University of Chicago
 # Copyright (C) 2005--2008  Gordon Kindlmann
 # Copyright (C) 1998--2004  University of Utah
 #
@@ -22,78 +22,6 @@
 #### errorCheck.mk: checks on validity of the variables that make uses
 ####
 
-# all the architectures currently supported
-KNOWN_ARCH = irix6.n32 irix6.64 linux.32 linux.ia64 linux.amd64 \
-  darwin.32 darwin.64 cygwin solaris aix \
-  netbsd.32 netbsd.ia64 netbsd.amd64 \
-
-# there is no default architecture
-checkArchSet = $(if $(TEEM_ARCH),,\
-$(warning *)\
-$(warning *)\
-$(warning * Environment variable TEEM_ARCH not set.)\
-$(warning * Possible settings currently supported:)\
-$(warning * $(KNOWN_ARCH))\
-$(warning *)\
-$(warning *)\
-$(error Make quitting))
-
-# see if they were looking for linux
-checkArchLinux = $(if $(findstring $(TEEM_ARCH),linux),\
-$(warning *)\
-$(warning *)\
-$(warning * Sorry: the $(TEEM_ARCH) TEEM_ARCH has split into:)\
-$(warning * "linux.32" for 32-bit)\
-$(warning * "linux.ia64" for 64-bit Itanium (Intel))\
-$(warning * "linux.amd64" for 64-bit Athlon (AMD))\
-$(warning *)\
-$(warning *)\
-$(error Make quitting))
-
-# see if they were looking for linux.64
-checkArchLinux64 = $(if $(findstring $(TEEM_ARCH),linux.64),\
-$(warning *)\
-$(warning *)\
-$(warning * Sorry: the $(TEEM_ARCH) TEEM_ARCH has split into:)\
-$(warning * "linux.ia64" for 64-bit Itanium (Intel))\
-$(warning * "linux.amd64" for 64-bit Athlon (AMD))\
-$(warning *)\
-$(warning *)\
-$(error Make quitting))
-
-# see if they were looking for netbsd.64
-checkArchNetbsd64 = $(if $(findstring $(TEEM_ARCH),netbsd.64),\
-$(warning *)\
-$(warning *)\
-$(warning * Sorry: the $(TEEM_ARCH) TEEM_ARCH has split into:)\
-$(warning * "netbsd.ia64" for 64-bit Itanium (Intel))\
-$(warning * "netbsd.amd64" for 64-bit Athlon (AMD))\
-$(warning *)\
-$(warning *)\
-$(error Make quitting))
-
-# see if they were looking for darwin
-checkArchDarwin = $(if $(findstring $(TEEM_ARCH),darwin),\
-$(warning *)\
-$(warning *)\
-$(warning * Sorry: the "darwin" TEEM_ARCH has split into:)\
-$(warning * "darwin.32" for 32-bit Macs (G4))\
-$(warning * "darwin.64" for 64-bit Macs (G5))\
-$(warning *)\
-$(warning *)\
-$(error Make quitting))
-
-# verify that we can recognize the architecture setting
-checkArchValid = $(if $(strip $(findstring $(TEEM_ARCH),$(KNOWN_ARCH))),,\
-$(warning *)\
-$(warning *)\
-$(warning * Environment variable TEEM_ARCH="$(TEEM_ARCH)" unknown)\
-$(warning * Possible settings currently supported:)\
-$(warning * $(KNOWN_ARCH))\
-$(warning *)\
-$(warning *)\
-$(error Make quitting))
-
 ## Other error checking ...
 ##
 checkShext = $(if $(filter undefined,$(origin TEEM_LINK_SHARED)),,\
@@ -101,7 +29,7 @@ $(if $(TEEM_SHEXT),,\
 $(warning *)\
 $(warning *)\
 $(warning * Cannot do shared library linking with TEEM_SHEXT unset)\
-$(warning * Set it in teem/make/$(ARCH).mk or as environment var)\
+$(warning * Set it in teem/make/arch.mk or as environment var)\
 $(warning *)\
 $(warning *)\
 $(error Make quitting)))
