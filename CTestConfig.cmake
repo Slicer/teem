7135331de3a61a189faf1c4a8233a0112e3aebd7
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2025-09-06 16:20:02 +0000

    tweaks
    git-svn-id: https://svn.code.sf.net/p/teem/code/teem/trunk@7367 3d70eeeb-363e-0410-a505-8a46323a89f2

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,34 +1,23 @@
-#
-# Teem: Tools to process and visualize scientific data and images
-# Copyright (C) 2009--2019  University of Chicago
-# Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
-# Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public License
-# (LGPL) as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-# The terms of redistributing and/or modifying this software also
-# include exceptions to the LGPL that facilitate static linking.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-# You should have received a copy of the GNU Lesser General Public License
-# along with this library; if not, see <https://www.gnu.org/licenses/>.
-#
+# CTestConfig.cmake - Dashboard submission settings for Teem
+# Copyright (C) 1998--2025 Teem contributors
+# Licensed under the GNU LGPL v2.1 or later (see LICENSE.txt for details)
 
-## This file should be placed in the root directory of your project.
-## Then modify the CMakeLists.txt file in the root directory of your
-## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
-##   enable_testing()
-##   include(Dart)
+# This file is used only when running CTest in "dashboard mode":
+#   ctest -D Experimental
+#   ctest -D Nightly
+#   ctest -D Continuous
+#
+# Local developer runs (just `ctest`) do NOT use this file.
+
+# Project name shown on CDash
 set(CTEST_PROJECT_NAME "Teem")
+
+# Define when the "nightly" dashboard day starts
 set(CTEST_NIGHTLY_START_TIME "05:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
+# Dashboard server information
+# If you don’t use CDash yet, you can leave these as-is or comment them out.
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Teem")
 set(CTEST_DROP_SITE_CDASH TRUE)
